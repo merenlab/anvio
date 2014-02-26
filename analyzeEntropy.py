01a7751b593d624a81a6b9d5cf32984d048f6706
A. Murat Eren <a.murat.eren@gmail.com>
2014-02-25 14:52:21 -1000

    executable bit is on

--- analyzeEntropy.py
+++ analyzeEntropy.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+# Copyright (C) 2010 - 2012, A. Murat Eren
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
 import os
 import sys
 import numpy
