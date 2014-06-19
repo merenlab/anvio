56efa323aa9d7ba1fff1b371d08a1c465049c289
A. Murat Eren <a.murat.eren@gmail.com>
2014-06-19 16:12:22 -0400

    even more structure

---
+++ PaPi/constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014, A. Murat Eren
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
+pretty_names = {}
