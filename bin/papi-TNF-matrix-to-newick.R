5b954f18876f439e6bd140866e61744056139e7d
A. Murat Eren <a.murat.eren@gmail.com>
2014-06-27 13:49:30 -0400

    better.

--- bin/papi-TNF-matrix-to-newick.R
+++ bin/papi-TNF-matrix-to-newick.R
@@ -16,8 +16,8 @@ option_list <- list(
 				help = 'Agglomeration method for clustering. Default is "%default". Available
 						methods are "ward", "single", "complete", "average", "mcquitty", "median"
 						and "centroid"'),
-		make_option(c("-o", "--output_file_prefix"), default="unknown",
-				help = "Output file prefix for output files [default \"%default\"]")
+		make_option(c("-o", "--output_file_prefix"), default="unknown-tree.txt",
+				help = "Output file [default \"%default\"]")
 )
 
 parser <- OptionParser(usage = "TNF_matrix_to_newick.R [options] input_matrix", option_list=option_list,
@@ -42,4 +42,4 @@ raw_data <- t(data.matrix(read.table(matrix_file, header = TRUE, row.names = 1,s
 dcols<-vegdist(t(raw_data), method=options$distance, na.rm=TRUE)
 hc <- hclust(dcols, method=options$method)
 phy <- hclust2phylog(hc, add.tools = TRUE)
-write.tree(as.phylo(phy), file=paste(options$output_file_prefix, '.txt', sep=''))
+write.tree(as.phylo(phy), file=options$output_file_prefix)
