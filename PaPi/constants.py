4549305f5532d9376468c3bd25925ce3f15bc3a7
A. Murat Eren <a.murat.eren@gmail.com>
2014-11-02 15:21:53 -0500

    provide access to filtering of auxiliary and essential fields. kinda embarrassing the way I do it, and it would be great to fix that function at some point, but at the moment I am excited about other things

--- PaPi/constants.py
+++ PaPi/constants.py
@@ -9,4 +9,7 @@
 #
 # Please read the COPYING file.
 
+IS_ESSENTIAL_FIELD = lambda f: (not f.startswith('__')) and (f not in ["contigs", "GC_content", "length"])
+IS_AUXILIARY_FIELD = lambda f: f.startswith('__')
+
 pretty_names = {}
