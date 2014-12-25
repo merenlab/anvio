74b90993a3302432c47918c1ec5a9bcaedc93cfd
A. Murat Eren <a.murat.eren@gmail.com>
2014-12-25 15:45:39 -0500

    bit more information about what is going on for people who would like to try themselves

--- tests/mini_test.sh
+++ tests/mini_test.sh
@@ -1,14 +1,34 @@
-cd 00-full-mini
-rm -rf tmp
-mkdir tmp
+# change directory
+cd mini_test
 
+# clean the old mess if exists
+rm -rf test-output
+mkdir test-output
+
+# init raw bam files.
 for f in 6M 7M 9M
 do
-    papi-init-bam 204_3contigs_"$f".bam -o tmp/204-$f
-    papi-profile -i tmp/204-$f.bam -o tmp/204-$f
+    papi-init-bam 204_3contigs_"$f".bam -o test-output/204-$f
 done
 
-papi-merge-multiple-runs tmp/204*/RUNINFO.cp -o tmp/204-MERGED
-papi-interactive-binning -r tmp/204-MERGED/RUNINFO-mean_coverage.cp
-rm -rf tmp
-cd ..
+
+# generate an annotation db using files obtained from myrast_gui using contigs.fa (contigs.fa
+# is the original file all samples were mapped to) using split size 1000 (the default split
+# size is better for most projects, small split size here is for testing purposes)
+papi-gen-annotation --contigs contigs.fa -p myrast_gui myrast_gui/* -o test-output/ -L 1000 
+
+
+# for each sample, run the profiling using the same split size used for the annotation database.
+# profiling generates individual directiorues uner test-output directory for each sample.
+for f in 6M 7M 9M
+do
+    papi-profile -i test-output/204-$f.bam -o test-output/204-$f -a test-output/ANNOTATION.db -L 1000
+done
+
+
+# merge samples
+papi-merge-multiple-runs test-output/204*/RUNINFO.cp -o test-output/204-MERGED
+
+
+# fire up the browser to show how does the merged samples look like.
+papi-interactive-binning -r test-output/204-MERGED/RUNINFO-mean_coverage.cp -a test-output/ANNOTATION.db
