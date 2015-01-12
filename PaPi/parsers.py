2ffee5f3018631c8b95caba09c575911b0c09dd5
A. Murat Eren <a.murat.eren@gmail.com>
2015-01-12 13:15:22 -0500

    Joe found out a better use of myrast svr scripts, so the cmdline parser is updated

--- PaPi/parsers.py
+++ PaPi/parsers.py
@@ -43,9 +43,9 @@ class Parser(object):
                 missing_files.append(f)
         if missing_files:
             if sorted(missing_files) == sorted(self.files_expected.values()):
-                raise ConfigError, "%s parser requires these files: %s"\
-                                     % (self.annotation_source,
-                                        ', '.join(self.files_expected))
+                raise ConfigError, "%s parser requires these file(s): %s. Please refer to the documentation if you\
+                                    don't know how to generate them" % (self.annotation_source,
+                                                                        ', '.join(self.files_expected.values()))
 
             raise ConfigError, "%s parser requires %d files (%s). %s missing from your input: %s"\
                                      % (self.annotation_source,
@@ -86,14 +86,12 @@ class Parser(object):
 
 class MyRastCMDLine(Parser):
     def __init__(self, contigs_fasta, input_file_paths, output_file_prefix, split_length = 20000):
-        files_expected = {'functions': 'functions.tbl', 'genes': 'genes.peg'}
+        files_expected = {'svr_output': 'svr_output.tbl'}
 
-        files_structure = {'functions': 
-                                {'col_names': ['t_species', 'field2', 'prot', 'function'],
-                                 'col_mapping': [str, int, str, str],
-                                 'indexing_field': 2},
-                           'genes': 
-                                {'type': 'fasta'},}
+        files_structure = {'svr_output': 
+                                {'col_names': ['contig', 'field1', 'prot', 'function', 't_species'],
+                                 'col_mapping': [str, int, str, str, str],
+                                 'indexing_field': 2}}
 
         Parser.__init__(self, 'MyRastCMDLine', input_file_paths, files_expected, files_structure)
 
@@ -102,47 +100,29 @@ class MyRastCMDLine(Parser):
 
 
     def get_annotations_dict(self):
-        # start with the fasta dict to identify start and stop. fasta file looked like this, as a reminder:
-        #
-        #    >prot_00001 D23-1_contig_1_127_1215
-        #    MTDCSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        #    >prot_00002 D23-1_contig_1_1281_1499
-        #    MKDNAERKAKRRIFLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        #    >prot_00003 D23-1_contig_1_2630_1626
-        #    MAKQKIRIRLKAYDHRVIDQSAEKIVETAKRSGADVSGPIPLPTE
-        #    >prot_00004 D23-1_contig_1_2987_3376
-        #    MKNGPKLSLALIGIFLILCEFFYGIPFLGATFILSFGWQPLIFNA
-        #    >prot_00005 D23-1_contig_1_4901_3567
-        #    MKNYFQFDKYGTNFKREILGGITTFLSMAYILAVNPQVLSLAGVK
-        #    >prot_00006 D23-1_contig_1_7149_5014
-        #    MKSLILAEKPSVARDIADALQINQKRNGYFENNQYIVTWALGHLV
-        #    >prot_00007 D23-1_contig_1_7266_8231
-        #    MLISLLTFISVEILYNKSNKKYGGNDMSIVQLYDITQIKSFIEHS
-
         annotations_dict = {}
 
-        for key in self.dicts['genes']:
+        counter = 1
+        for key in self.dicts['svr_output']:
             entry = {}
+            prot = 'prot_%.12d' % counter
+            counter += 1
+
             for field in annotation.annotation_table_structure:
                 entry[field] = None
 
-            prot, remainder = key.split()
-            contig = '_'.join(remainder.split('_')[:-2])
-            start, stop = [t for t in remainder.split('_')[-2:]]
+            start, stop = [t for t in key.split('_')[-2:]]
             start, stop = int(start), int(stop)
             entry['start'], entry['stop'], entry['direction'] = (start, stop, 'f') if start < stop else (stop, start, 'r')
-            entry['contig'] = contig
+            entry['contig'] = self.dicts['svr_output'][key]['contig']
+            entry['function'] = self.dicts['svr_output'][key]['function']
 
-            annotations_dict[prot] = entry
-
-        for prot in self.dicts['functions']:
-            d = self.dicts['functions'][prot]
-
-            t_species_str = d['t_species']
+            t_species_str = self.dicts['svr_output'][key]['t_species']
             if len(t_species_str.split()) > 2:
                 t_species_str = ' '.join(t_species_str.split()[0:2])
-            annotations_dict[prot]['t_species'] = t_species_str
-            annotations_dict[prot]['function'] = d['function']
+            entry['t_species'] = t_species_str
+
+            annotations_dict[prot] = entry
 
         return annotations_dict
 
---
+++ tests/mini_test/myrast_cmdline/README.txt
@@ -0,0 +1,3 @@
+Generated using:
+
+svr_assign_to_dna_using_figfams < ../contigs.fa > svr_output.tbl
