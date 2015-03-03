3a19551cc7bc38c18f1d5f59ec52a8711a9525c5
A. Murat Eren <a.murat.eren@gmail.com>
2015-03-03 17:23:56 -0500

    sandbox item

---
+++ sandbox/papi-script-get-collections-as-tab-delimited-matrix.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+"""
+Goes through the collections_* tables and creates a TAB delimited matrix based on
+all available collections in a given annotation database.
+"""
+
+
+import os
+import sys
+import argparse
+
+import PaPi.utils as utils
+import PaPi.terminal as terminal 
+import PaPi.annotation as annotation
+
+from PaPi.utils import ConfigError
+from PaPi.filesnpaths import FilesNPathsError
+
+run = terminal.Run()
+progress = terminal.Progress()
+
+parser = argparse.ArgumentParser(description='A simple script to generate info from search tables')
+parser.add_argument('annotation_db', metavar = 'ANNOTATION_DB',
+                    help = 'Annotation database to read from.')
+parser.add_argument('-o', '--output', metavar = "OUTPUT_FILE.txt", default="COLLECTIONS.txt",
+                    help = 'Output file name.')
+
+args = parser.parse_args()
+
+contigs = set([])
+contig_lengths = {}
+
+db = annotation.AnnotationDatabase(args.annotation_db, quiet=False)
+collections_splits_table = db.db.get_table_as_dict(annotation.collections_splits_table_name)
+collections_info_table = db.db.get_table_as_dict(annotation.collections_info_table_name)
+contig_lengths_table = db.db.get_table_as_dict(annotation.contig_lengths_table_name)
+contig_lengths = dict([(c, contig_lengths_table[c]['length']) for c in contig_lengths_table])
+db.disconnect()
+
+sources = collections_info_table.keys()
+
+splits = {}
+for entry in collections_splits_table.values():
+    split = entry['split']
+    source = entry['source']
+    cluster_id = entry['cluster_id']
+
+    if splits.has_key(split):
+        splits[split][source] = cluster_id
+    else:
+        splits[split] = {source: cluster_id}
+
+
+output = open(args.output, 'w')
+output.write('split\t%s\n' % '\t'.join(sources))
+for split in splits.keys():
+    line = [split]
+    for source in sources:
+        if splits[split].has_key(source):
+            line.append(splits[split][source])
+        else:
+            line.append(None)
+    output.write('\t'.join(line) + '\n')
+output.close()
+
+run.info('Collections matrix', args.output)
