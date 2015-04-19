19f33503b19c63c0dd56bd41634e58e1cd8d45e1
A. Murat Eren <a.murat.eren@gmail.com>
2015-04-19 19:02:10 -0400

    initial libraries for summary. related #95.

---
+++ anvio/summarizer.py
@@ -0,0 +1,269 @@
+# coding: utf-8
+"""Summarizes information stored in a profile database using an annotation database for
+   a given list of splits."""
+
+import os
+import sys
+import textwrap
+
+import anvio.tables as t
+import anvio.terminal as terminal
+import anvio.filesnpaths as filesnpaths
+import anvio.ccollections as ccollections
+
+from anvio.errors import ConfigError
+from anvio.dbops import DatabasesMetaclass
+from anvio.summaryhtml import SummaryHTMLOutput
+
+
+__author__ = "A. Murat Eren"
+__copyright__ = "Copyright 2015, The anvio Project"
+__credits__ = []
+__license__ = "GPL 3.0"
+__version__ = t.profile_db_version
+__maintainer__ = "A. Murat Eren"
+__email__ = "a.murat.eren@gmail.com"
+__status__ = "Development"
+
+
+pp = terminal.pretty_print
+run = terminal.Run()
+progress = terminal.Progress()
+
+
+class Summarizer(DatabasesMetaclass):
+    """Creates an über dictionary of 'summary'."""
+    def __init__(self, args = None, r = run, p = progress):
+        self.summary = {}
+
+        self.profile_db_path = None
+        self.annotation_db_path = None
+        self.output_directory = None
+
+        self.run = run
+        self.progress = progress
+
+        DatabasesMetaclass.__init__(self, args, self.run, self.progress)
+
+        self.collections = ccollections.Collections()
+        self.collections.populate_sources_dict(self.annotation_db_path, t.annotation_db_version)
+        self.collections.populate_sources_dict(self.profile_db_path, t.profile_db_version)
+
+        self.collection_id = None
+
+        if args:
+            if args.list_collections:
+                self.collections.list_collections()
+                sys.exit()
+
+            self.collection_id = args.collection_id
+            self.output_directory = args.output_directory
+
+        self.sanity_check()
+
+        filesnpaths.gen_output_directory(self.output_directory)
+
+        # set samples
+        self.samples = sorted([s.strip() for s in self.p_meta['samples'].split(',')])
+
+
+    def sanity_check(self):
+        if not self.collection_id:
+            raise ConfigError, "You must specify a collection id :/"
+
+        if self.collection_id not in self.collections.sources_dict:
+            raise ConfigError, "%s is not a valid collection ID. See a list of available ones with '--list-collections' flag" % self.collection_id
+
+        self.output_directory = filesnpaths.check_output_directory(self.output_directory)
+
+
+    def process(self):
+        # learn who you are:
+        collection_dict = self.collections.get_collection_dict(self.collection_id)
+        collection_colors = self.collections.get_collection_colors(self.collection_id)
+
+        # set up the initial summary dictionary
+        self.summary['meta'] = {'output_directory': self.output_directory,
+                                'groups': collection_dict.keys(),
+                                'collection_id': self.collection_id}
+        self.summary['groups'] = {}
+
+        for group_id in collection_dict: 
+            group = Group(self, group_id, collection_dict[group_id], self.run, self.progress)
+            group.output_directory = os.path.join(self.output_directory, 'collections', group_id)
+
+            self.summary['groups'][group_id] = group.create()
+            self.summary['groups'][group_id]['color'] = collection_colors[group_id] or '#212121'
+
+        SummaryHTMLOutput(self.summary, r = self.run, p = self.progress).generate()
+
+
+class Group:
+    def __init__(self, summary, group_id, split_ids, r = run, p = progress):
+        self.summary = summary
+        self.group_id = group_id
+        self.split_ids = split_ids
+        self.progress = p
+        self.run = r
+
+        self.group_info_dict = {'files': {}}
+
+        self.output_directory = None
+
+        # make sure all split_ids in the collection is actually in the annotation database.
+        # in collections stored in the annotation database, split_ids that are not in the
+        # oritinal contigs used to generate annotation database *may* end up in the
+        # collections table. we gotta make sure we deal with them properly:
+        missing_ids = [split_id for split_id in self.split_ids if split_id not in self.summary.split_sequences]
+        if len(missing_ids):
+            for missing_id in missing_ids:
+                self.split_ids.remove(missing_id)
+
+            self.run.warning('%d split id(s) in group "%s" reported by collection "%s" is not found in the\
+                              annotation database and removed from the group summary. If this does not make\
+                              any sense, you may need make sure everything is in order. The thing is,\
+                              sometimes external clustering results that are added to the annotation via\
+                              `anvi-populate-collections-table` may include split names that are not used\
+                              while the annotation database was generated.'\
+                                                % (len(missing_ids), group_id, self.summary.collection_id))
+
+
+    def create(self):
+        self.progress.new('[Collection "%s"] Creating the output directory' % self.group_id)
+        self.create_group_dir()
+
+        self.progress.new('[Collection "%s"] Creating the FASTA file' % self.group_id)
+        self.store_contigs_fasta()
+
+        return self.group_info_dict
+
+
+    def create_group_dir(self):
+        self.progress.update('...')
+
+        if not self.output_directory:
+            self.progress.end()
+            raise ConfigError, 'You caled Group.create before setting an output directory. Anvio says "nope, thanks".'
+
+        filesnpaths.gen_output_directory(self.output_directory)
+
+        self.progress.end()
+
+
+    def get_output_file_handle(self, prefix = 'output.txt', overwrite = False):
+        file_path = os.path.join(self.output_directory, '%s-%s' % (self.group_id, prefix))
+        if os.path.exists(file_path) and not overwrite:
+            raise ConfigError, 'get_output_file_handle: well, this file already exists: "%s"' % file_path
+
+
+        key = prefix.split('.')[0].replace('-', '_')
+        self.group_info_dict['files'][key] = file_path[len(self.summary.output_directory):].strip('/')
+
+        return open(file_path, 'w')
+
+
+    def store_contigs_fasta(self):
+        """Storing contig sequences.
+        
+           This is not an easy problem. We split contigs into smaller sequences at the beginning. Only
+           a part of a given contig may be used during the binning process. On the other hand we can't
+           simply store sequences of splits, whenever possible, we must store the entire sequence of
+           the contig (only if all splits are selected from a contig in to the same bin). So, this
+           function first identifies all splits coming from the same parent, then identifies sequential
+           blocks of splits (see `SequentialBlocks` class), then checks whether all splits of a given
+           contig is included in the group. If that is the case, it puts the contig as a single entry,
+           witht he identical FASTA id to the original contigs in the assembly file. Otherwise it appends
+           `_partial_X_Y` to the FASTA id, X and Y being the start and stop positions.
+        """
+
+        fasta_file = self.get_output_file_handle('contigs.fa')
+
+        # this dict will keep all the contig ids found in this group:
+        contigs_represented = {}
+
+        # go through all splits in this group, and populate `contigs_represented`
+        self.progress.update('Identifying contigs involved ...')
+        for split_id in self.split_ids:
+            s = self.summary.splits_basic_info[split_id]
+            if s['parent'] in contigs_represented:
+                contigs_represented[s['parent']][s['order_in_parent']] = split_id
+            else:
+                contigs_represented[s['parent']] = {s['order_in_parent']: split_id}
+
+        # now it is time to go through each contig found in contigs_represented to
+        # figure out how much of the contig is in fact in this group
+        for contig_id in contigs_represented:
+            splits_order = contigs_represented[contig_id].keys()
+
+            # this is critical: `sequential_blocks` is a list of one ore more lists,
+            # each describes splits that follow each other to represent a coherent
+            # chunk of the parent sequence:
+            self.progress.update('Identifying sequential blocks ...')
+            sequential_blocks = SequentialBlocks(splits_order).process()
+
+            for sequential_block in sequential_blocks:
+                self.progress.update('Identifying the portion of contig represented ...')
+                first_split = contigs_represented[contig_id][sequential_block[0]]
+                last_split = contigs_represented[contig_id][sequential_block[-1]]
+
+                contig_sequence_start_in_splits = self.summary.splits_basic_info[first_split]['start']
+                contig_sequence_end_in_splits = self.summary.splits_basic_info[last_split]['end']
+
+                # so this much of the contig is represented by its splits:
+                total_contig_length_in_splits = contig_sequence_end_in_splits - contig_sequence_start_in_splits
+
+                # and this is is actual length:
+                contig_sequence_length = self.summary.contigs_basic_info[contig_id]['length']
+
+                if contig_sequence_length == total_contig_length_in_splits:
+                    # the entireity of the contig is represented!
+                    appendix = ''
+                else:
+                    appendix = '_partial_%d_%d' % (contig_sequence_start_in_splits, contig_sequence_end_in_splits)
+
+                sequence = ''
+                self.progress.update('Reconstructing contig sequence from splits ...')
+                for split_order in sequential_block:
+                    sequence += self.summary.split_sequences[contigs_represented[contig_id][split_order]]
+
+                fasta_id = contig_id + appendix
+
+                self.progress.update('Writing contig sequence into file ...')
+                fasta_file.write('>%s\n' % fasta_id)
+                fasta_file.write('%s\n' % textwrap.fill(sequence, 80, break_on_hyphens = False))
+
+        fasta_file.close()
+        self.progress.end()
+
+
+class SequentialBlocks:
+    """Gets a list that goes like this: [1, 2, 3, 5, 6, 9], and returns another list
+       that goes like this: [[1, 2, 3], [5, 6], [9]]"""
+    def __init__(self, l):
+        self.l = sorted(list(set(l)))
+        self.blocks = []
+        self.current_block = []
+
+
+    def finalize_block(self):
+        self.blocks.append(self.current_block)
+        self.current_block = []
+
+
+    def process(self):
+        while 1:
+            if not self.l:
+                break
+
+            current = self.l.pop(0)
+
+            if not len(self.current_block) or current == self.current_block[-1] + 1:
+                self.current_block.append(current)
+            else:
+                self.finalize_block()
+                self.current_block.append(current)
+
+        self.finalize_block()
+
+        return self.blocks
+
---
+++ anvio/summaryhtml.py
@@ -0,0 +1,92 @@
+# coding: utf-8
+"""Creates an HTML output to act as a front-end for the static summary directory."""
+
+import os
+import shutil
+
+import anvio.tables as t
+import anvio.terminal as terminal
+
+from anvio.errors import ConfigError
+
+# get django imported
+try:
+    from django.conf import settings
+    absolute = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+    template_dir = os.path.join(absolute, 'data/static/template')
+    html_content_dir = os.path.join(absolute, 'data/static/content')
+    settings.configure(DEBUG=True, TEMPLATE_DEBUG=True, DEFAULT_CHARSET='utf-8', TEMPLATE_DIRS = (template_dir,))
+
+    try:
+        import django
+        django.setup()
+    except:
+        pass
+
+    from django.template.loader import render_to_string
+    from django.template.defaultfilters import register
+except ImportError:
+    raise ConfigError, 'You need to have Django module (http://djangoproject.com) installed on your system to generate HTML output.'
+
+
+__author__ = "A. Murat Eren"
+__copyright__ = "Copyright 2015, The anvio Project"
+__credits__ = []
+__license__ = "GPL 3.0"
+__version__ = t.profile_db_version
+__maintainer__ = "A. Murat Eren"
+__email__ = "a.murat.eren@gmail.com"
+__status__ = "Development"
+
+
+pp = terminal.pretty_print
+run = terminal.Run()
+progress = terminal.Progress()
+
+
+class SummaryHTMLOutput:
+    def __init__(self, summary_dict = {}, r = run, p = progress):
+        self.run = r
+        self.progress = p
+        self.summary_dict = summary_dict
+        print self.summary_dict
+
+
+
+    def generate(self):
+        self.progress.new('Copying static files')
+        self.copy_files()
+
+        self.progress.new('Rendering')
+        index_html = self.render()
+
+        self.run.info('HTML Output', index_html)
+
+
+    def copy_files(self):
+        self.progress.update('...')
+        shutil.copytree(html_content_dir, os.path.join(self.summary_dict['meta']['output_directory'], '.html'))
+        self.progress.end()
+
+
+    def render(self):
+        self.progress.update('Processing the template ...')
+        rendered = render_to_string('index.tmpl', self.summary_dict)
+
+        index_html = os.path.join(self.summary_dict['meta']['output_directory'], 'index.html')
+        self.progress.update('Writing the index file ...')
+        open(index_html, 'w').write(rendered.encode('utf-8'))
+
+        self.progress.end()
+        return index_html
+
+
+@register.filter(name='lookup')
+def lookup(d, index):
+    if index in d:
+        return d[index]
+    return ''
+
+@register.filter(name='humanize')
+def lookup(s):
+    return s.replace('_', ' ')
