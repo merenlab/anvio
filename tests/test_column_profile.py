0c6df0af4f8d3652cc0fa56e649fd77c8d7e86c2
A. Murat Eren <a.murat.eren@gmail.com>
2015-04-25 17:20:16 -0400

    simple script to measure the runtime performances of column profilers implemented in C and Python. related #102.

---
+++ tests/test_column_profile.py
@@ -0,0 +1,55 @@
+import time
+
+from anvio.columnprofile import ColumnProfile as ColumnProfile_in_C
+from anvio.variability import ColumnProfile as ColumnProfile_in_Python
+
+# setup the test factory:
+from anvio.variability import VariablityTestFactory
+variability_test_class = VariablityTestFactory()
+
+# pretty output
+from anvio.summaryhtml import pretty
+from anvio.terminal import Run
+run = Run(width=55)
+
+# available profiles dict:
+ColumnProfile = {'Python': ColumnProfile_in_Python,
+                 'C'     : ColumnProfile_in_C}
+
+
+# test function:
+def test(column, quiet = False, test_class = variability_test_class):
+    coverage = len(column)
+
+    results = {}
+
+    if not quiet:
+        run.warning('', 'Profiling results for %s nts [TestFactory = %s]' % (pretty(len(column)), 'True' if test_class else 'False'))
+
+    for method in ['C', 'Python']:
+        results[method] = {}
+
+        start = time.time()
+        results[method]['profile'] = ColumnProfile[method](column, coverage = coverage, pos = 0, test_class = test_class).profile
+        end = time.time()
+    
+        results[method]['delta_time'] = end - start
+    
+        if not quiet:
+            run.info('%s profile' % method, results[method]['profile'])
+            run.info('%s response time' % method, results[method]['delta_time'])
+
+    run.info('Result', 'C is ~%.2f times faster' % (results['Python']['delta_time'] / results['C']['delta_time']), mc = 'green')
+
+
+# tests:
+
+# with and without test class:
+column = ('A' * 1000) + ('T' * 1000000)
+test(column)
+
+column = ('A' * 1000) + ('T' * 1000000)
+test(column, test_class = None)
+
+column = ('A' * 10000) + ('T' * 10000)
+test(column)
