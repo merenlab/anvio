cc747b018509c8f0b525de05110b6a231d516822
A. Murat Eren <a.murat.eren@gmail.com>
2015-04-27 17:32:59 -0400

    pretty names for our pretty interface.. related #108.

--- anvio/data/interactive/js/constants.js
+++ anvio/data/interactive/js/constants.js
@@ -1,70 +1,89 @@
 var named_layers = {
+	'__parent__': {
+		'pretty_name': 'Parent',
+	},
+	'taxonomy': {
+		'pretty_name': 'Taxonomy',
+	},
 	'num_genes': {
 		'height': 80,
 		'color': '#414141',
 		'norm': 'sqrt',
+        'pretty_name': 'Number of genes',
 	},
 	'avg_gene_length': {
 		'height': 80,
 		'color': '#414141',
 		'norm': 'sqrt',
+		'pretty_name': 'Avg. gene length',
 	},
 	'ratio_coding': {
 		'height': 80,
 		'color': '#414141',
 		'norm': 'none',
+		'pretty_name': 'Ratio coding region',
 	},
 	'ratio_hypothetical': {
 		'height': 80,
 		'color': '#072c59',
 		'norm': 'none',
+		'pretty_name': 'Ratio hypothetical',
 	},
 	'ratio_with_tax': {
 		'height': 80,
 		'color': '#555907',
 		'norm': 'none',
+		'pretty_name': 'Ratio w/ taxonomy',
 	},
 	'tax_accuracy': {
 		'height': 80,
 		'color': '#40012e',
 		'norm': 'none',
+		'pretty_name': 'Accuracy of taxonomy',
 	},
 	'length': {
 		'height': 80,
 		'color': '#414141',
 		'norm': 'none',
+		'pretty_name': 'Length',
 	},
 	'gc_content': {
 		'height': 180,
 		'color': '#004a0f',
 		'norm': 'none',
+		'pretty_name': 'GC-content',
 	},
 
 	'normalized_coverage': {
 		'height': 120,
 		'color': '#414141',
 		'norm': 'log',
+		'pretty_name': 'Normalized coverage',
 	},
 	'std_coverage': {
 		'height': 60,
 		'color': '#414141',
 		'norm': 'log',
+		'pretty_name': 'Coverage STD',
 	},
 	'portion_covered': {
 		'height': 60,
 		'color': '#616161',
 		'norm': 'none',
+		'pretty_name': 'Percent covered',
 	},
 	'abundance': {
 		'height': 60,
 		'color': '#818181',
 		'norm': 'none',
+		'pretty_name': 'Abundance',
 	},
 
 	'variability': {
 		'height': 90,
 		'color': '#4a000f',
 		'norm': 'none',
+		'pretty_name': 'Variablity',
 	},
 };
 
