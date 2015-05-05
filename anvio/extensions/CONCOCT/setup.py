9171ddd941dd85f03beb5248880abfa553cd63a5
Christopher Quince <u1375038@004851.home>
2015-05-05 22:29:01 +0100

    Concoct extension

---
+++ anvio/extensions/CONCOCT/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("vbgmm",
+                             sources=["vbgmm.pyx", "c_vbgmm_fit.c"],
+			     libraries =['gsl',  'gslcblas'],
+                             include_dirs=[numpy.get_include()])],
+)
