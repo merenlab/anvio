aef052c8835debdbcfb0d6f8e34c036bd8d20efb
A. Murat Eren <a.murat.eren@gmail.com>
2015-05-11 11:57:37 -0400

    now it does update the database! closes #132.

--- anvio/refine.py
+++ anvio/refine.py
@@ -16,7 +16,7 @@ import anvio.filesnpaths as filesnpaths
 import anvio.interactive as interactive
 import anvio.ccollections as ccollections
 
-from anvio.errors import ConfigError
+from anvio.errors import ConfigError, RefineError
 from anvio.clusteringconfuguration import ClusteringConfiguration
 
 
@@ -47,6 +47,7 @@ class RefineBins(dbops.DatabasesMetaclass):
             self.collection_id = args.collection_id
             self.annotation_db_path = args.annotation_db
             self.profile_db_path = args.profile_db
+            self.debug = args.debug
         except:
             raise ConfigError, "RefineBins :: missing argument..."
 
@@ -63,6 +64,16 @@ class RefineBins(dbops.DatabasesMetaclass):
         if self.bins_file_path and not len(self.bins):
             filesnpaths.is_file_exists(self.bins_file_path)
             self.bins = set([b.strip() for b in open(self.bins_file_path).readlines()])
+
+
+        # if the user updates the refinement of a single bin or bins, there shouldn't be multiple copies
+        # of that stored in the database. so everytime 'store_refined_bins' function is called,
+        # it will check this varlable and, (1) if empty, continue updating stuff in db store updates
+        # in it, (2) if not empty, remove items stored in this variable from collections dict, and continue
+        # with step (1). the starting point is of course self.bins. when the store_refined_bins function is
+        # called the first time, it will read collection data for collection_id, and remove the bin(s) in
+        # analysis from it before it stores the data:
+        self.ids_for_already_refined_bins = self.bins
 
         self.input_directory = os.path.dirname(os.path.abspath(self.profile_db_path))
 
@@ -120,6 +131,58 @@ class RefineBins(dbops.DatabasesMetaclass):
         d.title = textwrap.fill(title)
 
         return d
+
+
+    def store_refined_bins(self, refined_bin_data, refined_bin_colors):
+        self.progress.new('Storing refined bins')
+        self.progress.update('accessing to collection "%s" ...' % self.collection_id)
+        collection_dict = self.collections.get_collection_dict(self.collection_id)
+        colors_dict = self.collections.get_collection_colors(self.collection_id)
+        self.progress.end()
+
+        bad_bin_names = [b for b in collection_dict if (b in refined_bin_data and b not in self.ids_for_already_refined_bins)]
+        if len(bad_bin_names):
+            raise RefineError, '%s of your bin names %s NOT unique, and already exist%s in the database. You must rename\
+                                %s to something else: %s' % (
+                                                              'One' if len(bad_bin_names) == 1 else len(bad_bin_names),
+                                                              'is' if len(bad_bin_names) == 1 else 'are',
+                                                              's' if len(bad_bin_names) == 1 else '',
+                                                              'this one' if len(bad_bin_names) == 1 else 'these',
+                                                              ', '.join(bad_bin_names)
+                                                             )
+
+        # remove bins that should be updated in the database:
+        for bin_id in self.ids_for_already_refined_bins:
+            collection_dict.pop(bin_id)
+            colors_dict.pop(bin_id)
+
+        # zero it out
+        self.ids_for_already_refined_bins = set([])
+
+        if self.debug:
+            self.run.info('collection from db', collection_dict)
+            self.run.info('colors from db', colors_dict)
+            self.run.info_single('')
+
+            self.run.info('incoming collection data', refined_bin_data)
+            self.run.info('incoming collection colors', refined_bin_colors)
+            self.run.info_single('')
+
+        for bin_id in refined_bin_data:
+            collection_dict[bin_id] = refined_bin_data[bin_id]
+            colors_dict[bin_id] = refined_bin_colors[bin_id]
+            self.ids_for_already_refined_bins.add(bin_id)
+
+
+        if self.debug:
+            self.run.info('resulting collection', collection_dict)
+            self.run.info('resulting collection colors', colors_dict)
+            self.run.info_single('')
+
+        collections = dbops.TablesForCollections(self.profile_db_path, anvio.__profile__version__)
+        collections.append(self.collection_id, collection_dict, colors_dict)
+
+        self.run.info_single('"%s" collection is updated!' % self.collection_id)
 
 
     def cluster_splits_of_interest(self):
