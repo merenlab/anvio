db38e451fd062ec2376931a0518ce33f3a681c63
A. Murat Eren <a.murat.eren@gmail.com>
2015-06-08 16:30:13 -0400

    basic license information

--- anvio/data/interactive/js/charts.js
+++ anvio/data/interactive/js/charts.js
@@ -1,3 +1,23 @@
+/**
+ * Javascript library to visualize anvi'o charts
+ *
+ *  Author: A. Murat Eren <a.murat.eren@gmail.com>
+ *  Credits: Özcan Esen, Gökmen Göksel
+ *  Copyright 2015, The anvio Project
+ *
+ * This file is part of anvi'o (<https://github.com/meren/anvio>).
+ *
+ * Anvi'o is a free software. You can redistribute this program
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with anvi'o. If not, see <http://opensource.org/licenses/GPL-3.0>.
+ *
+ * @license GPL-3.0+ <http://opensource.org/licenses/GPL-3.0>
+ */
+
 var VIEWER_WIDTH = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
 
 var layers;
--- anvio/data/interactive/js/utils.js
+++ anvio/data/interactive/js/utils.js
@@ -1,3 +1,23 @@
+/**
+ * Utility functions for anvi'o interactive interface
+ *
+ *  Author: Özcan Esen <ozcanesen@gmail.com>
+ *  Copyright 2015, The anvio Project
+ *
+ * This file is part of anvi'o (<https://github.com/meren/anvio>).
+ *
+ * Anvi'o is a free software. You can redistribute this program
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with anvi'o. If not, see <http://opensource.org/licenses/GPL-3.0>.
+ *
+ * @license GPL-3.0+ <http://opensource.org/licenses/GPL-3.0>
+ */
+
+
 //--------------------------------------------------------------------------------------------------
 // http://stackoverflow.com/questions/3019278/any-way-to-specify-the-base-of-math-log-in-javascript
 function log10(val) {
