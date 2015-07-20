7d69803b8159c53736da207380d4289ff4dbcfe9
A. Murat Eren <a.murat.eren@gmail.com>
2015-07-20 15:53:36 -0400

    fixies.

--- sandbox/anvi-script-import-state.py
+++ sandbox/anvi-script-import-state.py
@@ -2,63 +2,86 @@
 # -*- coding: utf-8
 
 import os
+import sys
 import glob
 import argparse
 
-import anvio
 import anvio.tables as t
 import anvio.terminal as terminal 
 import anvio.dbops as dbops
 import anvio.db as db
+
 from anvio.errors import ConfigError
-from sqlite3 import OperationalError
+
 
 run = terminal.Run()
+progress = terminal.Progress()
 
-parser = argparse.ArgumentParser(description='A simple script to upgrade profile database to version 5 and import all state files into database')
-parser.add_argument('-p', '--profile-db', metavar = 'PROFILE_DB',
-                    help = 'Profile database')
 
-args = parser.parse_args()
+def update_profile_db_from_v4_to_v5(profile_db_path):
+    if profile_db_path is None:
+        raise ConfigError, "No profile database is given."
 
-if args.profile_db is None:
-	raise ConfigError, "No profile database."
+    progress.new("Trying to upgrade the profile database")
+    progress.update('...')
 
-run.info_single("Trying to upgrade profile database...")
+    profile_db = db.DB(profile_db_path, '4')
 
-try:
-    profile_db = db.DB(args.profile_db, '4')
+    if t.states_table_name in profile_db.get_table_names():
+        progress.end()
+        raise ConfigError, "It seems '%s' table already exists in your profile database." % t.states_table_name
+
+    # create the table
     profile_db.create_table(t.states_table_name, t.states_table_structure, t.states_table_types)   
+
+    # set the version
     profile_db.remove_meta_key_value_pair('version')
     profile_db.set_version('5')
+
+    # bye
     profile_db.disconnect()
+    progress.end()
+    run.info_single("An empty states table is generated.")
+
+    # next: states
+    progress.new("Importing state files")
+    progress.update('...')
+
+    profile_db_full_path = os.path.join(os.getcwd(), profile_db_path)
+    dir_name = os.path.dirname(profile_db_full_path)
+    
+    search_pattern = os.path.join(dir_name, 'state*.json')
+    state_files = glob.glob(search_pattern)
+ 
+    for state_file_path in state_files:
+        progress.update("Importing '%s' ..." % state_file_path)
+
+        path, filename = os.path.split(state_file_path)
+    
+        #replace .json and use as key.
+        state_id = filename.replace('.json', '')
+     
+        # get json file content
+        with open(state_file_path) as f: content = f.read()
+    
+        states = dbops.TablesForStates(profile_db_path, '5')
+        states.store_state(state_id, content)
+
+    progress.end()
+    run.info_single("%d state files added to the database." % len(state_files)) if len(state_files) else None
+
     run.info_single("Database successfully upgraded.")
-except OperationalError:
-    run.warning("It seems '%s' table already exists in your profile database." % t.states_table_name)
-except ConfigError:
-    run.warning("It seems your profile database already in version '5'.")
 
-run.info_single("Trying to import all state files in same path with profile databse.")
 
-profile_db_full_path = os.path.join(os.getcwd(), args.profile_db)
-dir_name = os.path.dirname(profile_db_full_path)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A simple script to upgrade profile database to version 5 and import all state files into database')
+    parser.add_argument('profile_db', metavar = 'PROFILE_DB', help = 'Profile database')
+    args = parser.parse_args()
 
-search_pattern = os.path.join(dir_name, 'state*.json')
-state_files = glob.glob(search_pattern)
-
-run.info("State files found in the path", len(state_files))
-
-for state_file_path in state_files:
-    path, filename = os.path.split(state_file_path)
-
-    #replace .json and use as key.
-    state_id = filename.replace('.json', '')
-
-    run.info_single("Importing '%s'..." % state_id)
-
-    # get json file content
-    with open(state_file_path) as f: content = f.read()
-
-    states = dbops.TablesForStates(args.profile_db, '5')
-    states.store_state(state_id, content)
-
+    update_profile_db_from_v4_to_v5(args.profile_db)
+    sys.exit()
+    try:
+        update_profile_db_from_v4_to_v5(args.profile_db)
+    except ConfigError, e:
+        print e
+        sys.exit(-1)
