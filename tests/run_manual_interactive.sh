dc9e1f2642f390868f05fc7b717cacf2a6b77307
A. Murat Eren <a.murat.eren@gmail.com>
2015-08-23 12:12:12 -0400

    update the test with the correct file names and params. related #194

--- tests/run_manual_interactive.sh
+++ tests/run_manual_interactive.sh
@@ -12,7 +12,7 @@ INFO "Anvo'o version ..."
 anvi-profile --version
 
 INFO "Generating a newick file from the data ..."
-anvi-matrix-to-newick data.txt -o test-output/tree.txt
+anvi-matrix-to-newick view_data.txt -o test-output/tree.txt
 
 INFO "Running the interactive interface on files"
-anvi-interactive -f fasta.fa -m data.txt -A metadata.txt -t test-output/tree.txt -o test-output/output-dir --title 'Interactive Tree For User Provided Files'
+anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt -t test-output/tree.txt -o test-output/output-dir --title 'Interactive Tree For User Provided Files'
