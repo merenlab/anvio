615563955512494cb83c71a1502ebbb69a0006d7
A. Murat Eren <a.murat.eren@gmail.com>
2015-11-13 09:44:29 -0600

    generate and use samples database. related to #222.

--- tests/run_manual_interactive.sh
+++ tests/run_manual_interactive.sh
@@ -14,5 +14,9 @@ anvi-profile --version
 INFO "Generating a newick file from the data ..."
 anvi-matrix-to-newick view_data.txt -o test-output/tree.txt
 
+INFO "Generating a samples database ..."
+anvi-gen-samples-info-database -R samples-order.txt -D samples-information.txt -o test-output/samples.db
+
 INFO "Running the interactive interface on files"
-anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt -t test-output/tree.txt --manual-mode -p test.db --title 'Interactive Tree For User Provided Files'
+anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt -t test-output/tree.txt --manual-mode -p test-output/test.db -s test-output/samples.db --title 'Interactive Tree For User Provided Files'
+
