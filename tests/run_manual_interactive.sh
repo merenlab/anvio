38288c2c88c703a056748dddb8b62e88abb7564e
Tobias Paczian <paczian@mcs.anl.gov>
2015-12-02 09:41:57 -0600

    adjusted home page

--- tests/run_manual_interactive.sh
+++ tests/run_manual_interactive.sh
@@ -8,11 +8,11 @@ cd sandbox/files_for_manual_interactive
 rm -rf test-output
 mkdir test-output
 
-INFO "Anvo'o version ..."
+INFO "Anvi'o version ..."
 anvi-profile --version
 
 INFO "Generating a newick file from the data ..."
 anvi-matrix-to-newick view_data.txt -o test-output/tree.txt
 
 INFO "Running the interactive interface on files"
-anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt -t test-output/tree.txt --manual-mode -p test.db --title 'Interactive Tree For User Provided Files'
+anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt  --manual-mode --title 'Manual Interactive' -p test.db -t test-output/tree.txt
