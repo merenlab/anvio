a6381ef4cf95fee4de762e8ad4ad1613bf4a5ffe
Tobias Paczian <paczian@mcs.anl.gov>
2015-12-03 08:07:31 -0600

    added server script for multiuser

---
+++ tests/run_multi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source 00.sh
+set -e
+cd sandbox/files_for_manual_interactive
+anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt  --manual-mode --title 'Manual Interactive' -p test.db -t test-output/tree.txt --port-number 80 --server-only
