4ed8ea597714e8a45dcd71217ffb60c1cff69ec1
Tobias Paczian <paczian@mcs.anl.gov>
2015-12-04 03:37:09 -0600

    added user directory creation if it does not exist

--- tests/run_multi.sh
+++ tests/run_multi.sh
@@ -2,4 +2,7 @@
 source 00.sh
 set -e
 cd sandbox/files_for_manual_interactive
+if [ ! -d "userdata" ]; then
+  mkdir userdata
+fi
 anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt  --manual-mode --title 'Manual Interactive' -p test.db -t test-output/tree.txt --port-number 80 --server-only
