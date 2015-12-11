7d4761efb69de481a85e0daed7ec957906c08536
Tobias Paczian <paczian@mcs.anl.gov>
2015-12-11 03:56:37 -0600

    updated to use anvi-server vs anvi-interactive

--- tests/run_multi.sh
+++ tests/run_multi.sh
@@ -5,4 +5,4 @@ cd sandbox/files_for_manual_interactive
 if [ ! -d "userdata" ]; then
   mkdir userdata
 fi
-anvi-interactive -f fasta.fa -d view_data.txt -A additional_view_data.txt --manual-mode --title 'Manual Interactive' -p test.db -t test-output/tree.txt --port-number 80 --server-only --multiuser
+anvi-server -f fasta.fa -d view_data.txt -A additional_view_data.txt --manual-mode --title "anvi'o Server" -p test.db -t test-output/tree.txt --port-number 80
