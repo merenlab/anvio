26be4ddeaab49b7db698aa9771004100eac498a0
A. Murat Eren <a.murat.eren@gmail.com>
2015-12-14 11:44:14 -0600

    a new one is on the way

--- tests/run_multi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source 00.sh
-set -e
-cd sandbox/files_for_manual_interactive
-if [ ! -d "userdata" ]; then
-  mkdir userdata
-fi
-anvi-server -f fasta.fa -d view_data.txt -A additional_view_data.txt --manual-mode --title "anvi'o Server" -p test.db -t test-output/tree.txt --port-number 80
