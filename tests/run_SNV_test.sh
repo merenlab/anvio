7af8e4608fec6beec35c6b21eb24bc127ab814bb
A. Murat Eren <a.murat.eren@gmail.com>
2015-12-17 09:42:42 -0600

    better

---
+++ tests/run_SNV_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+source 00.sh
+set -e
+
+INFO "Creating the output directory ..."
+# change directory and clean the old mess if it exists
+cd sandbox
+rm -rf test-output
+mkdir test-output
+
+INFO "Anvo'o version ..."
+anvi-profile --version
+
+INFO "Generating short reads for SNV testing ..."
+anvi-script-gen-short-reads short_reads_for_SNV_testing.ini
+
+INFO "Generating a Bowtie2 ref ..."
+bowtie2-build single_contig.fa test-output/single_contig
+
+INFO "Mapping short reads to the ref ..."
+../misc/bowtie_batch_single_fasta.sh test-output/short_reads.fa test-output/SNV-testing test-output/single_contig
+
+INFO "Generating anvi'o contigs database ..."
+anvi-gen-contigs-database -f single_contig.fa -o test-output/single_contig.db -L 500
+
+INFO "Profiling the BAM file ..."
+anvi-profile -i test-output/SNV-testing.bam -c test-output/single_contig.db -o test-output/SNV-testing -M 0 --cluster
+
+INFO "Calling the interactive interface ..."
+anvi-interactive -p test-output/SNV-testing/PROFILE.db -c test-output/single_contig.db
