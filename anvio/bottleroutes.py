49f3b3d1a8790852dda8b5ae88d157a529faed36
A. Murat Eren <a.murat.eren@gmail.com>
2016-01-16 15:19:47 -0600

    once everything's uploaded, perform a sanity check
    related to #268.

--- anvio/bottleroutes.py
+++ anvio/bottleroutes.py
@@ -279,6 +279,7 @@ def get_tree(args, d, request, response, tree_id):
 
     return json.dumps("")
 
+
 def get_split_info(args, d, request, response, split_name):
     set_default_headers(response)
     return json.dumps(d.split_sequences[split_name])
