a36c66cc28776565124f4ce633ad2af0f6b9562f
A. Murat Eren <a.murat.eren@gmail.com>
2016-01-17 17:16:53 -0600

    A new function to learn about each nt
    This function now allows us to say whether a given nucleotide position
    in a partial gene call (a beginning or the end of which is missing from
    the contig), a complete gene call, or that position is a third base in a
    codon given the gene context. This will have pretty sweet implications
    on our analysis results going forward. Related to #274. Closes #206.
    Closes #163.

--- anvio/tables.py
+++ anvio/tables.py
@@ -95,8 +95,8 @@ states_table_structure               = ['name', 'content', 'last_modified']
 states_table_types                   = ['text',  'text'  ,      'text'    ]
 
 variable_positions_table_name        = 'variable_positions'
-variable_positions_table_structure   = ['entry_id', 'sample_id', 'split_name',   'pos'  , 'pos_in_contig', 'coverage', 'departure_from_consensus', 'competing_nts', 'consensus',    'A'   ,    'T'   ,    'C'   ,    'G'   ,    'N'   ]
-variable_positions_table_types       = [ 'numeric',    'text'  ,    'text'   , 'numeric',    'numeric'   , 'numeric' ,          'numeric'        ,      'text'    ,    'text'  , 'numeric', 'numeric', 'numeric', 'numeric', 'numeric']
+variable_positions_table_structure   = ['entry_id', 'sample_id', 'split_name',   'pos'  , 'pos_in_contig', 'in_partial_gene_call', 'in_complete_gene_call', 'is_third_base', 'coverage', 'departure_from_consensus', 'competing_nts', 'consensus',    'A'   ,    'T'   ,    'C'   ,    'G'   ,    'N'   ]
+variable_positions_table_types       = [ 'numeric',    'text'  ,    'text'   , 'numeric',    'numeric'   ,        'numeric'      ,       'numeric'        ,     'numeric'  , 'numeric' ,          'numeric'        ,      'text'    ,    'text'  , 'numeric', 'numeric', 'numeric', 'numeric', 'numeric']
 
 gene_coverages_table_name            = 'gene_coverages'
 gene_coverages_table_structure       = ['entry_id', 'gene_callers_id', 'sample_id', 'mean_coverage']
