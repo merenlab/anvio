87cb4c2a8cd0d63b2488cd35cd82a0383cc63642
A. Murat Eren <a.murat.eren@gmail.com>
2016-01-18 21:58:23 -0600

    they will be useful soon. you have no idea!

--- anvio/constants.py
+++ anvio/constants.py
@@ -46,6 +46,23 @@ complements = string.maketrans('acgtrymkbdhvACGTRYMKBDHV',\
 
 nucleotides = 'ATCGN'
 
+codon_to_AA = {'ATA':'Ile', 'ATC':'Ile', 'ATT':'Ile', 'ATG':'Met',
+               'ACA':'Thr', 'ACC':'Thr', 'ACG':'Thr', 'ACT':'Thr',
+               'AAC':'Asn', 'AAT':'Asn', 'AAA':'Lys', 'AAG':'Lys',
+               'AGC':'Ser', 'AGT':'Ser', 'AGA':'Arg', 'AGG':'Arg',
+               'CTA':'Leu', 'CTC':'Leu', 'CTG':'Leu', 'CTT':'Leu',
+               'CCA':'Pro', 'CCC':'Pro', 'CCG':'Pro', 'CCT':'Pro',
+               'CAC':'His', 'CAT':'His', 'CAA':'Gln', 'CAG':'Gln',
+               'CGA':'Arg', 'CGC':'Arg', 'CGG':'Arg', 'CGT':'Arg',
+               'GTA':'Val', 'GTC':'Val', 'GTG':'Val', 'GTT':'Val',
+               'GCA':'Ala', 'GCC':'Ala', 'GCG':'Ala', 'GCT':'Ala',
+               'GAC':'Asp', 'GAT':'Asp', 'GAA':'Glu', 'GAG':'Glu',
+               'GGA':'Gly', 'GGC':'Gly', 'GGG':'Gly', 'GGT':'Gly',
+               'TCA':'Ser', 'TCC':'Ser', 'TCG':'Ser', 'TCT':'Ser',
+               'TTC':'Phe', 'TTT':'Phe', 'TTA':'Leu', 'TTG':'Leu',
+               'TAC':'Tyr', 'TAT':'Tyr', 'TAA':'STP', 'TAG':'STP',
+               'TGC':'Cys', 'TGT':'Cys', 'TGA':'STP', 'TGG':'Trp'}
+
 pretty_names = {}
 
 def get_pretty_name(key):
