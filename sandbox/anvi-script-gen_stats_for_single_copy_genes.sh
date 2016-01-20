fc37743736b8e4d2c95053221a083ffcdb6cdea1
A. Murat Eren <a.murat.eren@gmail.com>
2016-01-20 15:02:24 -0600

    well. params.

--- sandbox/anvi-script-gen_stats_for_single_copy_genes.sh
+++ sandbox/anvi-script-gen_stats_for_single_copy_genes.sh
@@ -12,7 +12,7 @@ echo "############################################################"
 echo "# Working on $1" 
 echo "############################################################"
 anvi-gen-contigs-database -f $1 -o $1.db
-anvi-populate-search-table $1.db
+anvi-populate-search-table -c $1.db
 anvi-script-gen_stats_for_single_copy_genes.py $1.db
 anvi-script-gen_stats_for_single_copy_genes.R $1.db.hits $1.db.genes
 echo ""
