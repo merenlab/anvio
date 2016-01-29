f3521c004f6bce8d7437fbdaa5c7840edb2d1b7f
A. Murat Eren <a.murat.eren@gmail.com>
2016-01-28 19:36:12 -0600

    pfft

--- anvio/tables.py
+++ anvio/tables.py
@@ -11,7 +11,7 @@ __email__ = "a.murat.eren@gmail.com"
 
 
 contigs_db_version = "4"
-profile_db_version = "7"
+profile_db_version = "8"
 samples_info_db_version = "2"
 auxiliary_hdf5_db_version = "1"
 
