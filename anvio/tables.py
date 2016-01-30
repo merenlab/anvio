ae75dc74487a47f57853a6b36bea7b75381cf795
A. Murat Eren <a.murat.eren@gmail.com>
2016-01-30 13:38:51 -0600

    incrase the profile db version

--- anvio/tables.py
+++ anvio/tables.py
@@ -11,7 +11,7 @@ __email__ = "a.murat.eren@gmail.com"
 
 
 contigs_db_version = "4"
-profile_db_version = "8"
+profile_db_version = "9"
 samples_info_db_version = "2"
 auxiliary_hdf5_db_version = "1"
 
