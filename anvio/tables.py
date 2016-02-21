912e0b6a2e56e7dcf45995f28dcb91ae0284f8bc
A. Murat Eren <a.murat.eren@gmail.com>
2016-02-20 18:08:10 -0600

    New table: variable_aas_table
    New, fancy tables in the profile database to store all AA frequency
    information. Related to #332._

--- anvio/tables.py
+++ anvio/tables.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8
 """ Table schemas for databases."""
 
+from anvio.constants import codon_to_AA
 
 __author__ = "A. Murat Eren"
 __copyright__ = "Copyright 2015, The anvio Project"
@@ -11,7 +12,7 @@ __email__ = "a.murat.eren@gmail.com"
 
 
 contigs_db_version = "5"
-profile_db_version = "12"
+profile_db_version = "13"
 samples_info_db_version = "2"
 auxiliary_hdf5_db_version = "1"
 users_db_version = "1"
@@ -95,9 +96,13 @@ states_table_name                    = 'states'
 states_table_structure               = ['name', 'content', 'last_modified']
 states_table_types                   = ['text',  'text'  ,      'text'    ]
 
+variable_aas_table_name              = 'variable_amino_acid_frequencies'
+variable_aas_table_structure         = ['entry_id', 'sample_id', 'corresponding_gene_call', 'codon_order_in_gene', 'consensus', 'coverage'] + sorted(list(set(codon_to_AA.values())))
+variable_aas_table_types             = [ 'numeric',    'text'  ,        'numeric'         ,       'numeric'      ,    'text'  , 'numeric' ] + ['numeric'] * len(list(set(codon_to_AA.values())))
+
 variable_nts_table_name              = 'variable_nucleotide_positions'
-variable_nts_table_structure         = ['entry_id', 'sample_id', 'split_name',   'pos'  , 'pos_in_contig', 'corresponding_gene_call', 'in_partial_gene_call', 'in_complete_gene_call', 'pos_in_codon', 'coverage', 'cov_outlier_in_split', 'cov_outlier_in_contig', 'departure_from_consensus', 'competing_nts', 'consensus',    'A'   ,    'T'   ,    'C'   ,    'G'   ,    'N'   ]
-variable_nts_table_types             = [ 'numeric',    'text'  ,    'text'   , 'numeric',    'numeric'   ,        'numeric'         ,       'numeric'       ,       'numeric'        ,    'numeric'  , 'numeric' ,          'bool'       ,          'bool'        ,          'numeric'        ,      'text'    ,    'text'  , 'numeric', 'numeric', 'numeric', 'numeric', 'numeric']
+variable_nts_table_structure         = ['entry_id', 'sample_id', 'split_name',   'pos'  , 'pos_in_contig', 'corresponding_gene_call', 'in_partial_gene_call', 'in_complete_gene_call', 'base_pos_in_codon', 'codon_order_in_gene', 'coverage', 'cov_outlier_in_split', 'cov_outlier_in_contig', 'departure_from_consensus', 'competing_nts', 'consensus',    'A'   ,    'T'   ,    'C'   ,    'G'   ,    'N'   ]
+variable_nts_table_types             = [ 'numeric',    'text'  ,    'text'   , 'numeric',    'numeric'   ,        'numeric'         ,       'numeric'       ,       'numeric'        ,       'numeric'    ,       'numeric'      , 'numeric' ,          'bool'       ,          'bool'        ,          'numeric'        ,      'text'    ,    'text'  , 'numeric', 'numeric', 'numeric', 'numeric', 'numeric']
 
 gene_coverages_table_name            = 'gene_coverages'
 gene_coverages_table_structure       = ['entry_id', 'gene_callers_id', 'sample_id', 'mean_coverage']
