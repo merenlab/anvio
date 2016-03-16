38cd75c3f2c92250654bd45470db60e0586e4399
A. Murat Eren <a.murat.eren@gmail.com>
2016-03-16 18:52:19 -0500

    some sample files for pan stuff

---
+++ tests/sandbox/anvi_pangenome_files/00_test_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ../../00.sh
+
+set -e
+
+INFO "Anvo'o version ..."
+anvi-profile --version
+
+INFO "Running anvi-pan-genome ..."
+anvi-pan-genome -i test-output/contig_dbs.txt -o test-output/pan-output
