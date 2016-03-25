b2da532ddf67f96076155a5de90a3d212b1e1c20
A. Murat Eren <a.murat.eren@gmail.com>
2016-03-24 19:36:36 -0500

    New drivers module.
    We will put every anvi'o driver for a third party software in here.

---
+++ anvio/drivers/diamond.py
@@ -0,0 +1,174 @@
+# coding: utf-8
+"""Interface to Diamond."""
+
+import os
+import shutil
+import tempfile
+
+import anvio
+import anvio.utils as utils
+import anvio.terminal as terminal
+
+from anvio.errors import ConfigError
+
+
+__author__ = "A. Murat Eren"
+__copyright__ = "Copyright 2016, The anvio Project"
+__credits__ = []
+__license__ = "GPL 3.0"
+__version__ = anvio.__version__
+__maintainer__ = "A. Murat Eren"
+__email__ = "a.murat.eren@gmail.com"
+
+
+run = terminal.Run()
+progress = terminal.Progress()
+pp = terminal.pretty_print
+
+
+class Diamond:
+    def __init__(self, query_fasta, run = run, progress = progress, num_threads = 1, overwrite_output_destinations = False):
+        self.run = run
+        self.progress = progress
+
+        self.num_threads = num_threads
+        self.overwrite_output_destinations = overwrite_output_destinations
+
+        utils.is_program_exists('diamond')
+
+        self.tmp_dir = tempfile.gettempdir()
+
+        self.query_fasta = query_fasta
+        self.log_file_path = 'diamond-log-file.txt'
+        self.target_db_path = 'diamond-target'
+        self.search_output_path = 'diamond-search-resuults'
+        self.tabular_output_path = 'diamond-search-results.txt'
+
+        self.sensitive = False
+
+        # if names_dict is None, all fine. if not, the query_fasta is assumed to be uniqued, and names_dict is
+        # the dictionary that connects the ids in the fasta file, to ids that were identical to it.
+        self.names_dict = None
+
+
+    def get_blastall_results(self):
+        force_makedb, force_blastp, force_view = False, False, False
+
+        if self.overwrite_output_destinations:
+            force_makedb = True
+
+        if os.path.exists(self.target_db_path + '.dmnd') and not force_makedb:
+            self.run.warning("Notice: A diamond database is found in the output directory, and will be used!")
+        else:
+            self.makedb()
+            force_blastp, forrce_view = True, True
+
+        if os.path.exists(self.search_output_path + '.daa') and not force_blastp:
+            self.run.warning("Notice: A DIAMOND search result is found in the output directory: skipping BLASTP!")
+        else:
+            self.blastp()
+            force_view = True
+
+        if os.path.exists(self.tabular_output_path) and not force_view:
+            self.run.warning("Notice: A DIAMOND tabular output is found in the output directory. Anvi'o will not generate another one!")
+        else:
+            self.view()
+
+        return self.tabular_output_path
+
+
+    def check_output(self, expected_output, process = 'diamond'):
+        if not os.path.exists(expected_output):
+            self.progress.end()
+            raise ConfigError, "Pfft. Something probably went wrong with Diamond's '%s' since one of the expected output files are missing.\
+                                Please check the log file here: '%s." % (process, self.log_file_path)
+
+
+    def makedb(self):
+        self.progress.new('DIAMOND')
+        self.progress.update('creating the search database (using %d thread(s)) ...' % self.num_threads)
+        cmd_line = ('diamond makedb --in %s -d %s -p %d >> "%s" 2>&1' % (self.query_fasta,
+                                                                         self.target_db_path,
+                                                                         self.num_threads,
+                                                                         self.log_file_path))
+
+        with open(self.log_file_path, "a") as log: log.write('CMD: ' + cmd_line + '\n')
+
+        utils.run_command(cmd_line)
+
+        self.progress.end()
+
+        expected_output = self.target_db_path + '.dmnd'
+        self.check_output(expected_output, 'makedb')
+
+        self.run.info('Diamond temp search db', expected_output)
+
+
+    def blastp(self):
+        self.run.info('DIAMOND is set to be', 'Sensitive' if self.sensitive else 'Fast')
+
+        self.progress.new('DIAMOND')
+        self.progress.update('running blastp (using %d thread(s)) ...' % self.num_threads)
+        cmd_line = ('diamond blastp -q %s -d %s -a %s -t %s -p %d %s -k 1000000 >> "%s" 2>&1' % (self.query_fasta,
+                                                                                   self.target_db_path,
+                                                                                   self.search_output_path,
+                                                                                   self.tmp_dir,
+                                                                                   self.num_threads,
+                                                                                   '--sensitive' if self.sensitive else '',
+                                                                                   self.log_file_path))
+        with open(self.log_file_path, "a") as log: log.write('BLASTP CMD: ' + cmd_line + '\n')
+
+        utils.run_command(cmd_line)
+
+        self.progress.end()
+
+        expected_output = self.search_output_path + '.daa'
+        self.check_output(expected_output, 'blastp')
+
+        self.run.info('Diamond blastp results', expected_output)
+
+
+    def view(self):
+        self.progress.new('DIAMOND')
+        self.progress.update('generating tabular output (using %d thread(s)) ...' % self.num_threads)
+        cmd_line = ('diamond view -a %s -o %s -p %d -k 1000000 >> "%s" 2>&1' % (self.search_output_path + '.daa',
+                                                                     self.tabular_output_path,
+                                                                     self.num_threads,
+                                                                     self.log_file_path))
+        with open(self.log_file_path, "a") as log: log.write('VIEW CMD: ' + cmd_line + '\n')
+
+        utils.run_command(cmd_line)
+
+        self.check_output(self.tabular_output_path, 'view')
+
+        if self.names_dict:
+            self.progress.update('Un-uniqueing the tabular output ...')
+            with open(self.log_file_path, "a") as log: log.write('self.names_dict is found. Un-uniqueing the tabular output.\n')
+            # if we are here, this means the self.tabular_output_path contains information only about unique
+            # entries. We will expand it here so downstream analyses do not need to pay attention to this
+            # detail.
+            new_tabular_output_path = self.tabular_output_path + '.ununiqued'
+            new_tabular_output = open(new_tabular_output_path, 'w')
+            saved_comparisons = set([])
+
+            for line in open(self.tabular_output_path):
+                fields = line.strip().split('\t')
+                for query_id in self.names_dict[fields[0]]:
+                    for subject_id in self.names_dict[fields[1]]:
+                        comparison_id = '_'.join(sorted([query_id, subject_id]))
+
+                        if comparison_id in saved_comparisons:
+                            continue
+
+                        new_tabular_output.write('%s\t%s\t%s\n' % (query_id, subject_id, '\t'.join(fields[2:])))
+                        saved_comparisons.add(comparison_id)
+
+
+            new_tabular_output.close()
+
+        shutil.move(self.tabular_output_path, self.tabular_output_path + '.unique')
+        shutil.move(new_tabular_output_path, self.tabular_output_path)
+
+        self.progress.end()
+
+        self.run.info('Diamond %stabular output file' % ('un-uniqued' if len(self.names_dict) else ''), self.tabular_output_path)
---
+++ anvio/drivers/hmmer.py
@@ -0,0 +1,132 @@
+# coding: utf-8
+"""Interface to HMMer."""
+
+import os
+import gzip
+import shutil
+
+import anvio
+import anvio.utils as utils
+import anvio.terminal as terminal
+import anvio.filesnpaths as filesnpaths
+
+from anvio.errors import ConfigError
+
+
+__author__ = "A. Murat Eren"
+__copyright__ = "Copyright 2016, The anvio Project"
+__credits__ = []
+__license__ = "GPL 3.0"
+__version__ = anvio.__version__
+__maintainer__ = "A. Murat Eren"
+__email__ = "a.murat.eren@gmail.com"
+
+
+run = terminal.Run()
+progress = terminal.Progress()
+pp = terminal.pretty_print
+
+
+class HMMer:
+    def __init__(self, protein_sequences_fasta, num_threads_to_use = 1, progress = progress, run = run):
+        self.num_threads_to_use = num_threads_to_use
+        self.progress = progress
+        self.run = run
+
+        filesnpaths.is_file_fasta_formatted(protein_sequences_fasta)
+
+        self.protein_sequences_fasta = protein_sequences_fasta
+
+        # hmm_scan_hits is the file to access later on for parsing:
+        self.hmm_scan_output = None
+        self.hmm_scan_hits = None
+        self.genes_in_contigs = None
+
+        self.tmp_dirs = []
+
+
+    def run_hmmscan(self, source, genes_in_model, hmm, ref, cut_off_flag = "--cut_ga"):
+        self.run.warning('', header = 'HMM Profiling for %s' % source, lc = 'green')
+        self.run.info('Reference', ref if ref else 'unknown')
+        self.run.info('Pfam model', hmm)
+        self.run.info('Number of genes', len(genes_in_model))
+        self.run.info('Number of CPUs will be used for search', self.num_threads_to_use)
+
+        tmp_dir = filesnpaths.get_temp_directory_path()
+        self.tmp_dirs.append(tmp_dir)
+
+        self.hmm_scan_output = os.path.join(tmp_dir, 'hmm.output')
+        self.hmm_scan_hits = os.path.join(tmp_dir, 'hmm.hits')
+        self.hmm_scan_hits_shitty = os.path.join(tmp_dir, 'hmm.hits.shitty')
+        log_file_path = os.path.join(tmp_dir, '00_log.txt')
+
+        self.run.info('Temporary work dir', tmp_dir)
+        self.run.info('HMM scan output', self.hmm_scan_output)
+        self.run.info('HMM scan hits', self.hmm_scan_hits)
+        self.run.info('Log file', log_file_path)
+
+        self.progress.new('Unpacking the model into temporary work directory')
+        self.progress.update('...')
+        hmm_file_path = os.path.join(tmp_dir, 'hmm.txt')
+        hmm_file = open(hmm_file_path, 'w')
+        hmm_file.write(gzip.open(hmm, 'rb').read())
+        hmm_file.close()
+        self.progress.end()
+
+        self.progress.new('Processing')
+        self.progress.update('Compressing the pfam model')
+        cmd_line = ('hmmpress "%s" >> "%s" 2>&1' % (hmm_file_path, log_file_path))
+        with open(log_file_path, "a") as myfile: myfile.write('CMD: ' + cmd_line + '\n')
+        ret_val = utils.run_command(cmd_line)
+        if ret_val:
+            raise ConfigError, "The last call did not work quite well. Most probably the version of HMMER\
+                                you have installed is not up-to-date enough. Just to make sure what went\
+                                wrong please take a look at the log file ('%s'). Please visit %s to see what\
+                                is the latest version availalbe. You can learn which version of HMMER you have\
+                                on your system by typing 'hmmpress -h'"\
+                                        % (log_file_path, 'http://hmmer.janelia.org/download.html')
+        self.progress.end()
+
+        self.progress.new('Processing')
+        self.progress.update('Performing HMM scan ...')
+
+        cmd_line = ('hmmscan -o "%s" %s --cpu %d --tblout "%s" "%s" "%s" >> "%s" 2>&1' \
+                                        % (self.hmm_scan_output,
+                                           cut_off_flag,
+                                           self.num_threads_to_use,
+                                           self.hmm_scan_hits_shitty,
+                                           hmm_file_path,
+                                           self.protein_sequences_fasta,
+                                           log_file_path))
+
+        with open(log_file_path, "a") as myfile: myfile.write('CMD: ' + cmd_line + '\n')
+        utils.run_command(cmd_line)
+
+        if not os.path.exists(self.hmm_scan_hits_shitty):
+            raise ConfigError, "Something went wrong with hmmscan, and it failed to generate the\
+                                expected output :/ Fortunately, this log file should tell you what\
+                                might be the problem: '%s'. Please do not forget to include this\
+                                file if you were to ask for help." % log_file_path
+
+        self.progress.end()
+
+        # thank you, hmmscan, for not generating a simple TAB-delimited, because we programmers
+        # love to write little hacks like this into our code:
+        parseable_output = open(self.hmm_scan_hits, 'w')
+        for line in open(self.hmm_scan_hits_shitty).readlines():
+            if line.startswith('#'):
+                continue
+            parseable_output.write('\t'.join(line.split()[0:18]) + '\n')
+        parseable_output.close()
+
+        num_raw_hits = filesnpaths.get_num_lines_in_file(self.hmm_scan_hits)
+        self.run.info('Number of raw hits', num_raw_hits)
+
+        return self.hmm_scan_hits if num_raw_hits else None
+
+
+    def clean_tmp_dirs(self):
+        for tmp_dir in self.tmp_dirs:
+            shutil.rmtree(tmp_dir)
+
+
