05996a68aa6cb67d135c123393828e757ecf3b8c
A. Murat Eren <a.murat.eren@gmail.com>
2016-06-17 18:31:32 -0500

    adapt to the new taxonomy design

--- anvio/dbops.py
+++ anvio/dbops.py
@@ -130,10 +130,6 @@ class ContigsSuperclass(object):
         self.genes_in_splits_summary_dict = contigs_db.db.get_table_as_dict(t.genes_in_splits_summary_table_name)
         self.genes_in_splits_summary_headers = contigs_db.db.get_table_structure(t.genes_in_splits_summary_table_name)
 
-        self.progress.update('Reading splits taxonomy')
-        self.splits_taxonomy_dict = contigs_db.db.get_table_as_dict(t.splits_taxonomy_table_name)
-
-        self.genes_in_splits_summary_headers = contigs_db.db.get_table_structure(t.genes_in_splits_summary_table_name)
         self.progress.update('Identifying HMM searches for single-copy genes and others')
         self.hmm_sources_info = contigs_db.db.get_table_as_dict(t.hmm_hits_info_table_name)
         for hmm_source in self.hmm_sources_info:
@@ -175,6 +171,24 @@ class ContigsSuperclass(object):
             self.run.info('Auxiliary Data', 'Found: %s (v. %s)' % (auxiliary_contigs_data_path, anvio.__hdf5__version__))
 
         self.run.info('Contigs DB', 'Initialized: %s (v. %s)' % (self.contigs_db_path, anvio.__contigs__version__))
+
+
+    def init_splits_taxonomy(self, t_level = 't_species'):
+        self.progress.new('Initializing splits taxonomy')
+        self.progress.update('...')
+
+        contigs_db = ContigsDatabase(self.contigs_db_path)
+        splits_taxonomy_table = contigs_db.db.get_table_as_dict(t.splits_taxonomy_table_name)
+        taxon_names_table = contigs_db.db.get_table_as_dict(t.taxon_names_table_name)
+
+        for split_name in splits_taxonomy_table:
+            taxon_id = splits_taxonomy_table[split_name]['taxon_id']
+            if taxon_id:
+                if t_level in taxon_names_table[taxon_id] and taxon_names_table[taxon_id][t_level]:
+                    self.splits_taxonomy_dict[split_name] = taxon_names_table[taxon_id][t_level]
+
+        contigs_db.disconnect()
+        self.progress.end()
 
 
     def init_contig_sequences(self, min_contig_length=0):
@@ -1033,6 +1047,8 @@ class ContigsDatabase:
         self.db.create_table(t.genes_in_contigs_table_name, t.genes_in_contigs_table_structure, t.genes_in_contigs_table_types)
         self.db.create_table(t.genes_in_splits_table_name, t.genes_in_splits_table_structure, t.genes_in_splits_table_types)
         self.db.create_table(t.splits_taxonomy_table_name, t.splits_taxonomy_table_structure, t.splits_taxonomy_table_types)
+        self.db.create_table(t.taxon_names_table_name, t.taxon_names_table_structure, t.taxon_names_table_types)
+        self.db.create_table(t.genes_taxonomy_table_name, t.genes_taxonomy_table_structure, t.genes_taxonomy_table_types)
         self.db.create_table(t.contig_sequences_table_name, t.contig_sequences_table_structure, t.contig_sequences_table_types)
         self.db.create_table(t.gene_function_calls_table_name, t.gene_function_calls_table_structure, t.gene_function_calls_table_types)
         self.db.create_table(t.gene_protein_sequences_table_name, t.gene_protein_sequences_table_structure, t.gene_protein_sequences_table_types)
@@ -2197,26 +2213,36 @@ class TablesForStates(Table):
 
 
 
-class TableForSplitsTaxonomy(Table):
+class TablesForTaxonomy(Table):
+    """Populate all tables with taxonomy information.
+
+       Essentially takes in a dictionary of genes and taxon calls, populates three tables: taxon_names,
+       splits_taxonomy, and gene_taxonomy when 'create' is called."""
     def __init__(self, db_path, run=run, progress=progress):
         self.db_path = db_path
+        self.run = run
+        self.progress = progress
 
-        Table.__init__(self, self.db_path, anvio.__contigs__version__, run, progress)
+        Table.__init__(self, self.db_path, anvio.__contigs__version__, self.run, self.progress)
 
         # this class keeps track of genes that occur in splits, and responsible
         # for generating the necessary table in the contigs database
         self.genes_in_splits = GenesInSplits()
 
 
-    def create(self, genes_dict, parser):
-        self.genes_dict = genes_dict
+    def create(self, genes_taxonomy_dict, taxon_names_dict, source='unkown source'):
+        self.source = source
 
-        if parser:
-            self.parser = parser
-        else:
-            self.parser = "user_input"
+        if not self.genes_are_called:
+            raise ConfigError, "Something is wrong. The contigs database says that genes were now called, and here\
+                                you are trying to populate taxonomy tables for genes. No, thanks."
 
-        self.sanity_check()
+        self.init_gene_calls_dict()
+
+        self.genes_taxonomy_dict = genes_taxonomy_dict
+        self.taxon_names_dict = taxon_names_dict
+
+        self.sanity_check() 
 
         # oepn connection
         contigs_db = ContigsDatabase(self.db_path)
@@ -2226,46 +2252,110 @@ class TableForSplitsTaxonomy(Table):
         # test whether there are already genes tables populated
         taxonomy_source = contigs_db.meta['taxonomy_source']
         if taxonomy_source:
-            self.run.warning('Previous taxonomy information from "%s" will be replaced with the incoming data\
-                              through "%s"' % (taxonomy_source, self.parser))
+            self.run.warning('Previous taxonomy information from "%s" is being replaced with the incoming data\
+                              through "%s".' % (taxonomy_source, self.source))
             contigs_db.db._exec('''DELETE FROM %s''' % (t.splits_taxonomy_table_name))
+            contigs_db.db._exec('''DELETE FROM %s''' % (t.taxon_names_table_name))
+            contigs_db.db._exec('''DELETE FROM %s''' % (t.genes_taxonomy_table_name))
+
+        # populate taxon mames table
+        self.populate_taxon_names_table()
+
+        # populate genes taxonomy table
+        self.populate_genes_taxonomy_table()
 
         # compute and push split taxonomy information.
-        self.populate_split_taxonomy_information()
+        self.populate_splits_taxonomy_table()
 
-        # set the parser
+        # set the source
         contigs_db.db.remove_meta_key_value_pair('taxonomy_source')
-        contigs_db.db.set_meta_value('taxonomy_source', self.parser)
+        contigs_db.db.set_meta_value('taxonomy_source', self.source)
 
         # disconnect like a pro.
         contigs_db.disconnect()
 
 
+    def populate_genes_taxonomy_table(self):
+        # open connection
+        contigs_db = ContigsDatabase(self.db_path)
+
+        # push taxonomy data
+        db_entries = [(gene_callers_id, self.genes_taxonomy_dict[gene_callers_id]) for gene_callers_id in self.genes_taxonomy_dict]
+        contigs_db.db._exec_many('''INSERT INTO %s VALUES (?,?)''' % t.genes_taxonomy_table_name, db_entries)
+
+        # disconnect
+        contigs_db.disconnect()
+
+        self.run.info('Genes taxonomy table', 'Taxonomy stored for %d gene calls' % len(db_entries))
+
+
+    def populate_taxon_names_table(self):
+        # open connection
+        contigs_db = ContigsDatabase(self.db_path)
+
+        db_entries = [tuple([t_name_id] + [self.taxon_names_dict[t_name_id][t_level] for t_level in t.taxon_names_table_structure[1:]]) for t_name_id in self.taxon_names_dict]
+        contigs_db.db._exec_many('''INSERT INTO %s VALUES (?,?,?,?,?,?,?)''' % t.taxon_names_table_name, db_entries)
+
+        contigs_db.disconnect()
+        self.run.info('Taxon names table', 'Updated with %d unique taxon names' % len(db_entries))
+
+
     def sanity_check(self):
+        """Basic checks to make sure things are in order at least to a minimum extent."""
+
+        self.progress.new('Sanity checking ...')
+        self.progress.update('Comparing gene caller ids in the incoming data and in the contigs database ..')
+        gene_caller_ids_in_database = set(self.gene_calls_dict.keys())
+        gene_caller_ids_in_taxonomy_dict = set(self.genes_taxonomy_dict.keys())
+        gene_caller_ids_missing_in_db = gene_caller_ids_in_taxonomy_dict.difference(gene_caller_ids_in_database)
+        self.progress.end()
+
+        run.info("Num gene caller ids in the db", pp(len(gene_caller_ids_in_database)))
+        run.info("Num gene caller ids in the incoming data", pp(len(gene_caller_ids_in_taxonomy_dict)))
+
+        if gene_caller_ids_missing_in_db:
+            raise ConfigError, "Taxonomy information for genes you are trying to import into the database contains\
+                                %s gene caller ids that do not appear to be in the database. This is a step you must\
+                                be very careful to make sure you are not importing annotations for genes that have\
+                                nothing to do with your contigs database. To make sure of that, you should always work\
+                                with `anvi-get-sequences-for-gene-calls` or `anvi-export-protein-sequences` programs\
+                                to get the data to annotate. For instance one of the gene caller ids you have in your\
+                                input data that does not appear in the database is this one: '%s'. Anvi'o hopes it makes\
+                                sense to you, because it definitely does not make any sense to anvi'o :("\
+                                                        % (len(gene_caller_ids_missing_in_db), str(gene_caller_ids_missing_in_db.pop()))
+
         # check whether input matrix dict
-        keys_found = ['prot'] + self.genes_dict.values()[0].keys()
-        missing_keys = [key for key in t.splits_taxonomy_table_structure[1:] if key not in keys_found]
+        keys_found =  self.taxon_names_dict.values()[0].keys()
+        missing_keys = [key for key in t.taxon_names_table_structure[1:] if key not in keys_found]
         if len(missing_keys):
-            raise ConfigError, "Your dictionary is missing one or more keys that are necessary to populate the\
-                                taxonomy table. Here is a list of missing keys: %s. The complete list of input\
-                                keys should contain these: %s." % (', '.join(missing_keys),
-                                                                   ', '.join(t.splits_taxonomy_table_structure))
+            raise ConfigError, "Anvi'o is trying to get ready to create tables for taxonomy, but there is something\
+                                wrong :( The taxonomy names dict (one of the required input dictionaries to the class\
+                                seems to be missing a one or more keys that are necessary to finish the job. Here is \
+                                a list of missing keys: %s. The complete list of input keys should contain these: %s."\
+                                        % (', '.join(missing_keys), ', '.join(t.taxon_names_table_structure[1:]))
+
+        if not len(self.taxon_names_dict):
+            raise ConfigError, "Anvi'o is trying to get ready to create tables for taxonomy, but taxonomy names dict\
+                                (one of the required input dictionaries to the class responsible for this task) seems\
+                                to be empty."
 
 
-    def populate_split_taxonomy_information(self):
+    def populate_splits_taxonomy_table(self):
+        """Populate the taxonomy information per split"""
+
         # build a dictionary for fast access to all proteins identified within a contig
-        prots_in_contig = {}
-        for prot in self.genes_dict:
-            contig = self.genes_dict[prot]['contig']
-            if contig in prots_in_contig:
-                prots_in_contig[contig].add(prot)
+        gene_caller_ids_in_contigs = {}
+        for gene_callers_id in self.genes_taxonomy_dict:
+            contig = self.gene_calls_dict[gene_callers_id]['contig']
+            if contig in gene_caller_ids_in_contigs:
+                gene_caller_ids_in_contigs[contig].add(gene_callers_id)
             else:
-                prots_in_contig[contig] = set([prot])
+                gene_caller_ids_in_contigs[contig] = set([gene_callers_id])
 
-        contigs_without_annotation = list(set(self.contigs_info.keys()) - set(prots_in_contig.keys()))
+        contigs_without_annotation = list(set(self.contigs_info.keys()) - set(gene_caller_ids_in_contigs.keys()))
 
         for contig in contigs_without_annotation:
-            prots_in_contig[contig] = set([])
+            gene_caller_ids_in_contigs[contig] = set([])
 
         splits_dict = {}
 
@@ -2275,30 +2365,28 @@ class TableForSplitsTaxonomy(Table):
         for contig in self.contigs_info:
             for split_name in self.contig_name_to_splits[contig]:
                 num_splits_processed += 1
+
+                splits_dict[split_name] = None
                 start = self.splits_info[split_name]['start']
                 stop = self.splits_info[split_name]['end']
 
-                taxa = []
-                for prot in prots_in_contig[contig]:
-                    if self.genes_dict[prot]['stop'] > start and self.genes_dict[prot]['start'] < stop:
-                        taxa.append(self.genes_dict[prot]['t_species'])
+                taxon_name_ids = []
+                for gene_callers_id in gene_caller_ids_in_contigs[contig]:
+                    if self.gene_calls_dict[gene_callers_id]['stop'] > start and self.gene_calls_dict[gene_callers_id]['start'] < stop:
+                        taxon_name_ids.append(self.genes_taxonomy_dict[gene_callers_id])
 
-                taxonomy_strings = [tt for tt in taxa if tt]
-
-                splits_dict[split_name] = dict([(f, None) for f in t.splits_taxonomy_table_structure])
-                distinct_taxa = set(taxonomy_strings)
-
-                if not len(distinct_taxa):
+                if not taxon_name_ids:
                     continue
 
-                if len(distinct_taxa) == 1:
-                    splits_dict[split_name]['t_species'] = distinct_taxa.pop()
+                if len(set(taxon_name_ids)) == 1:
+                    splits_dict[split_name] = taxon_name_ids[0]
                 else:
                     d = Counter()
-                    for taxon in taxonomy_strings:
-                        d[taxon] += 1
-                    most_frequent_taxon_name, occurrence = sorted(d.items(), key=operator.itemgetter(1))[-1]
-                    splits_dict[split_name]['t_species'] = most_frequent_taxon_name
+                    for taxon_name_id in taxon_name_ids:
+                        d[taxon_name_id] += 1
+
+                    most_frequent_taxon_name_id, occurrence = d.most_common[0]
+                    splits_dict[split_name] = most_frequent_taxon_name_id
 
                 num_splits_with_taxonomy += 1
 
@@ -2306,14 +2394,14 @@ class TableForSplitsTaxonomy(Table):
         contigs_db = ContigsDatabase(self.db_path)
 
         # push taxonomy data
-        db_entries = [tuple([split] + [splits_dict[split][h] for h in t.splits_taxonomy_table_structure[1:]]) for split in splits_dict]
-        contigs_db.db._exec_many('''INSERT INTO %s VALUES (?,?,?,?,?,?,?)''' % t.splits_taxonomy_table_name, db_entries)
+        db_entries = [(split, splits_dict[split]) for split in splits_dict]
+        contigs_db.db._exec_many('''INSERT INTO %s VALUES (?,?)''' % t.splits_taxonomy_table_name, db_entries)
 
         # disconnect
         contigs_db.disconnect()
 
-        self.run.info('Splits taxonomy', 'Input data from parser "%s" annotated %d of %d splits (%.1f%%) with taxonomy.'\
-                                            % (self.parser, num_splits_with_taxonomy, num_splits_processed,
+        self.run.info('Splits taxonomy', 'Input data from "%s" annotated %d of %d splits (%.1f%%) with taxonomy.'\
+                                            % (self.source, num_splits_with_taxonomy, num_splits_processed,
                                                num_splits_with_taxonomy * 100.0 / num_splits_processed))
 
 class TableForGeneFunctions(Table):
