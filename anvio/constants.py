3ae7b6299decacb49e1e533e1e7510eb2b12c928
A. Murat Eren <a.murat.eren@gmail.com>
2016-07-28 09:30:09 -1000

    the default port number is in the constants module

--- anvio/constants.py
+++ anvio/constants.py
@@ -22,6 +22,8 @@ __status__ = "Development"
 clustering_configs_dir = os.path.join(os.path.dirname(anvio.__file__), 'data/clusterconfigs')
 clustering_configs = {}
 
+default_port_number = 8080
+
 blank_default = "tnf-splits"
 single_default = "tnf"
 merged_default = "tnf-cov"
