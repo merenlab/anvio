8bb756471e95fa4d4fb82b32cf92ff52ab6671a2
A. Murat Eren <a.murat.eren@gmail.com>
2016-09-05 18:38:12 -0500

    more structure

--- anvio/auxiliarydataops.py
+++ anvio/auxiliarydataops.py
@@ -7,6 +7,7 @@ import numpy as np
 
 import anvio
 import anvio.tables as t
+import anvio.utils as utils
 import anvio.terminal as terminal
 import anvio.constants as constants
 import anvio.filesnpaths as filesnpaths
@@ -24,6 +25,7 @@ __email__ = "a.murat.eren@gmail.com"
 
 run = terminal.Run()
 progress = terminal.Progress()
+pp = terminal.pretty_print
 
 
 class HDF5_IO(object):
@@ -45,10 +47,6 @@ class HDF5_IO(object):
             filesnpaths.is_file_exists(self.file_path)
             self.fp = h5py.File(self.file_path, 'r')
 
-            if [h not in self.fp.attrs for h in ['hash', 'version']].count(True):
-                raise HDF5Error, "The database at '%s' is missing one or more essential headers that\
-                                  should appear in every anvi'o generated HDF5 file. Sorry!" % self.file_path
-
             if self.fp.attrs['version'] != anvio.__hdf5__version__:
                 raise HDF5Error, "The database at '%s' is at version '%s', however your cliend is at\
                                   version '%s'. Bad news." % (self.file_path, self.fp.attrs['version'], anvio.__hdf5__version__)
@@ -56,6 +54,8 @@ class HDF5_IO(object):
             if not ignore_hash and self.fp.attrs['hash'] != unique_hash:
                 raise HDF5Error, "The database at '%s' does not seem to be compatible with the client :/\
                                   (i.e., the hash values do not match)." % self.file_path
+
+            self.unique_hash = self.fp.attrs['hash']
 
 
     def add_integer_list(self, path, l, data_type = 'uint16'):
@@ -162,7 +162,10 @@ class GenomesDataStorage(HDF5_IO):
         self.progress = progress
         self.quiet = quiet
 
-        self.essential_genome_info = constants.essential_genome_info + ['genome_hash']
+        self.essential_genome_info = constants.essential_genome_info + ['genome_hash', 'external_genome']
+
+        if not create_new:
+            self.num_genomes = len(self.fp['/info/genomes'])
 
 
     def is_known_genome(self, genome_name, throw_exception=True):
@@ -180,7 +183,8 @@ class GenomesDataStorage(HDF5_IO):
 
     def add_gene_call_data(self, genome_name, gene_caller_id, sequence, partial=0, functions = [], taxonomy_dict = None):
         """Add a gene call in a genome into the database"""
-        self.fp['/data/genomes/%s/%d/sequence' % (genome_name, int(gene_caller_id))] = sequence
+        self.fp['/data/genomes/%s/%d/sequence' % (genome_name, gene_caller_id)] = sequence
+        self.fp['/data/genomes/%s/%d/partial' % (genome_name, gene_caller_id)] = partial
 
         if taxonomy_dict:
             for t_level in t.taxon_names_table_structure[1:]:
@@ -214,3 +218,63 @@ class GenomesDataStorage(HDF5_IO):
 
         for key in self.essential_genome_info:
             self.fp['/info/genomes/%s/%s' % (genome_name, key)] = info_dict[key]
+
+
+    def get_genomes_dict(self):
+        genomes_dict = {}
+
+        for d in self.fp['/info/genomes']:
+            genomes_dict[d] = {}
+
+        for genome_name in genomes_dict:
+            for key in self.fp['/info/genomes/%s' % genome_name]:
+                genomes_dict[genome_name][key] = self.fp['/info/genomes/%s/%s' % (genome_name, key)].value
+
+        return genomes_dict
+
+    def gen_combined_protein_sequences_FASTA(self, output_file_path, exclude_partial_gene_calls=False):
+        self.run.info('Exclude partial gene calls', exclude_partial_gene_calls, nl_after=1)
+
+        genomes = self.get_genomes_dict()
+
+        total_num_protein_sequences = 0
+        total_num_excluded_protein_sequences = 0
+
+        output_file = open(output_file_path, 'w')
+
+        D = lambda genome_name: self.fp['/data/genomes/%s' % genome_name]
+        G = lambda gene_callers_id: genome_data['%d' % gene_callers_id]
+
+        for genome_name in genomes:
+            self.progress.new('Storing protein sequences')
+            self.progress.update('%s ...' % genome_name)
+
+            genome_data = D(genome_name)
+            gene_caller_ids = sorted([int(i[0]) for i in D(genome_name).items()])
+
+            for gene_caller_id in gene_caller_ids:
+                partial = G(gene_caller_id)['partial'].value
+
+                if exclude_partial_gene_calls and partial:
+                    total_num_excluded_protein_sequences += 1
+                    continue
+
+                sequence = G(gene_caller_id)['sequence'].value
+
+                output_file.write('>%s_%d\n' % (genomes[genome_name]['genome_hash'], int(gene_caller_id)))
+                output_file.write('%s\n' % sequence)
+
+                total_num_protein_sequences += 1
+
+            self.progress.end()
+
+        self.progress.new('Uniquing the output FASTA file')
+        self.progress.update('...')
+        unique_proteins_FASTA_path, unique_proteins_names_file_path, unique_proteins_names_dict = utils.unique_FASTA_file(output_file_path, store_frequencies_in_deflines=False)
+        self.progress.end()
+
+        self.run.info('Unique protein sequences FASTA', output_file_path)
+        self.run.info('Num protein sequences reported', '%s' % pp(total_num_protein_sequences), nl_before=1)
+        self.run.info('Num excluded gene calls', '%s' % pp(total_num_excluded_protein_sequences))
+
+        return unique_proteins_FASTA_path, unique_proteins_names_dict
