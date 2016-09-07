aa09aa93cc4ad19e8c336c1cbee68caa2ee65e83
A. Murat Eren <a.murat.eren@gmail.com>
2016-09-07 14:20:49 -0500

    drop table function

--- anvio/db.py
+++ anvio/db.py
@@ -68,6 +68,10 @@ class DB:
         self._exec('''CREATE TABLE self (key text, value text)''')
 
 
+    def drop_table(self, table_name):
+        self._exec('''DROP TABLE IF EXISTS %s;''' % table_name)
+
+
     def create_table(self, table_name, fields, types):
         if len(fields) != len(types):
             raise ConfigError, "create_table: The number of fields and types has to match."
