fae0eabc6bb298decf5f7ace02d15c934c14d96f
A. Murat Eren <a.murat.eren@gmail.com>
2016-09-30 18:18:19 +0200

    example pangenomic analysis w/ ext genomes

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -253,3 +253,25 @@ anvi-interactive -p $output_dir/SAMPLES-MERGED/PROFILE.db -c $output_dir/CONTIGS
 
 INFO "Firing up the interactive interface to refine a bin"
 anvi-refine -p $output_dir/SAMPLES-MERGED/PROFILE.db -c $output_dir/CONTIGS.db -s $output_dir/SAMPLES.db -C CONCOCT -b Bin_1
+
+INFO "Setting up the pan analysis directory"
+mkdir $output_dir/pan_test
+cp mock_data_for_pangenomics/*.fa mock_data_for_pangenomics/external-genomes.txt mock_data_for_pangenomics/default-state.json $output_dir/pan_test/
+cd $output_dir/pan_test
+
+INFO "Generating contigs databases for external genomes"
+anvi-script-FASTA-to-contigs-db 01.fa
+anvi-script-FASTA-to-contigs-db 02.fa
+anvi-script-FASTA-to-contigs-db 03.fa
+
+INFO "Generating an anvi'o genomes storage"
+anvi-gen-genomes-storage -e external-genomes.txt -o TEST-GENOMES.h5
+
+INFO "Running the pangenome anaysis with default parameters"
+anvi-pan-genome -g TEST-GENOMES.h5 -o TEST/ -J TEST
+
+INFO "Importing the default state for pretty outputs"
+anvi-import-state -p TEST/TEST-PAN.db -s default-state.json -n default
+
+INFO "Displaying the pangenome analysis results"
+anvi-display-pan -p TEST/TEST-PAN.db -s TEST/TEST-SAMPLES.db -g TEST-GENOMES.h5 --title "A mock pangenome analysis"
