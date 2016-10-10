35e94cbca164234b912e01e88a23a93f031609ce
A. Murat Eren <a.murat.eren@gmail.com>
2016-10-10 18:24:54 +0200

    common operations into 00.sh

--- tests/00.sh
+++ tests/00.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 C() {
     echo -e "\033[1;30m\033[47m$1\033[0m"
 }
@@ -9,4 +11,25 @@ INFO() {
     echo
     C ":: $1 ..."
     echo
-} 
+}
+
+SETUP_WITH_OUTPUT_DIR() {
+    if [ -z "$1"  ]
+    then
+        output_dir="`pwd`/test-output"
+        rm -rf $output_dir
+    else
+        output_dir="$1/test-output"
+    fi
+
+    mkdir -p $output_dir
+
+    files="sandbox"
+
+    INFO "Output directory"
+    echo "$output_dir"
+    echo
+
+    INFO "Anvo'o version"
+    anvi-profile --version
+}
