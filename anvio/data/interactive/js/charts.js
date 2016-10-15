162b16a74fd90bfe5858a728d1ea7f39ec7271a2
A. Murat Eren <a.murat.eren@gmail.com>
2016-10-15 07:48:58 -0500

    common functions go into a shared lib

--- anvio/data/interactive/js/charts.js
+++ anvio/data/interactive/js/charts.js
@@ -27,37 +27,6 @@ var maxVariability = 0;
 var geneParser;
 var contextSvg;
 
-GeneParser = (function() {
-  function GeneParser(data) {
-    this.data = data;
-  }
-
-  GeneParser.prototype.filterData = function(start, stop) {
-    var gene, _data, _i, _len, _ref;
-    _data = [];
-    _ref = this.data;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      gene = _ref[_i];
-      
-      if (gene.start_in_split > start && gene.start_in_split < stop || gene.stop_in_split > start && gene.stop_in_split < stop || gene.start_in_split <= start && gene.stop_in_split >= stop) {
-        _data.push(gene);
-      }
-    }
-    return _data;
-  };
-
-  return GeneParser;
-
-})();
-
-function getUrlVars() {
-    var map = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        map[key] = value;
-    });
-    return map;
-}
-
 
 function loadAll() {
     contig_id = getUrlVars()["contig"];
@@ -72,23 +41,25 @@ function loadAll() {
 
             layers = contig_data.layers;
             coverage = contig_data.coverage;
-	    variability = [];
-	    for (var i=0; i<coverage.length; i++) {
-		variability[i] = [];
-		for (var l=0; l<4; l++) {
-		    variability[i][l] = [];
-		    for (var h=0; h<coverage[i].length; h++) {
-			if (contig_data.variability[i][l].hasOwnProperty(h)) {
-			    variability[i][l].push(contig_data.variability[i][l][h]);
-			    if (contig_data.variability[i][l][h] > maxVariability) {
-				maxVariability = contig_data.variability[i][l][h];
-			    }
-			} else {
-			    variability[i][l].push(0);
-			}
-		    }
-		}
-	    }
+            variability = [];
+
+            for (var i=0; i<coverage.length; i++) {
+                variability[i] = [];
+                for (var l=0; l<4; l++) {
+                    variability[i][l] = [];
+                    for (var h=0; h<coverage[i].length; h++) {
+                        if (contig_data.variability[i][l].hasOwnProperty(h)) {
+                            variability[i][l].push(contig_data.variability[i][l][h]);
+                            if (contig_data.variability[i][l][h] > maxVariability) {
+                                maxVariability = contig_data.variability[i][l][h];
+                            }
+                        } else {
+                            variability[i][l].push(0);
+                        }
+                    }
+                }
+            }
+
             competing_nucleotides = contig_data.competing_nucleotides;
             previous_contig_name = contig_data.previous_contig_name;
             next_contig_name = contig_data.next_contig_name;
@@ -282,170 +253,6 @@ function createCharts(state){
     drawArrows(0, charts[0].xScale.domain()[1]);
 }
 
-function removeGeneChart() {
-  var node = document.getElementById("gene-arrow-chart");
-  if (node && node.parentNode) {
-    node.parentNode.removeChild(node);
-  }
-}
-
-function get_gene_functions_table_html(gene){
-    functions_table_html = '<h2>Gene Call</h2>';
-    functions_table_html += '<table class="table table-striped" style="width: 600px; text-align: center;">';
-    functions_table_html += '<thead><th>ID</th><th>Source</th><th>Length</th><th>Direction</th><th>Start</th><th>Stop</th><th>Complete</th><th>% in split</th></thead>';
-    functions_table_html += '<tbody>';
-    functions_table_html += '<tr><td>' + gene.gene_callers_id
-                          + '</td><td>' + gene.source
-                          + '</td><td>' + gene.length
-                          + '</td><td>' + gene.direction
-                          + '</td><td>' + gene.start_in_contig
-                          + '</td><td>' + gene.stop_in_contig
-                          + '</td><td>' + gene.complete_gene_call
-                          + '</td><td>' + gene.percentage_in_split.toFixed(2) + '%'
-                          + '</td></tr></tbody></table>';
-
-    functions_table_html += '<button type="button" class="btn btn-default btn-sm" onClick="show_sequence(' + gene.gene_callers_id + ');">Get sequence</button> ';
-    functions_table_html += '<button type="button" class="btn btn-default btn-sm" onClick="fire_up_ncbi_blast(' + gene.gene_callers_id + ', \'blastn\', \'nr\', \'gene\');">blastn @ nr</button> ';
-    functions_table_html += '<button type="button" class="btn btn-default btn-sm" onClick="fire_up_ncbi_blast(' + gene.gene_callers_id + ', \'blastn\', \'refseq_genomic\', \'gene\');">blastn @ refseq_genomic</button> ';
-    functions_table_html += '<button type="button" class="btn btn-default btn-sm" onClick="fire_up_ncbi_blast(' + gene.gene_callers_id + ', \'blastx\', \'nr\', \'gene\');">blastx @ nr</button> ';
-    functions_table_html += '<button type="button" class="btn btn-default btn-sm" onClick="fire_up_ncbi_blast(' + gene.gene_callers_id + ', \'blastn\', \'refseq_genomic\', \'gene\');">blastx @ refseq_genomic</button> ';
-
-    if(!gene.functions)
-        return functions_table_html;
-
-    functions_table_html += '<h2>Annotation</h2>';
-    functions_table_html += '<table class="table table-striped">';
-    functions_table_html += '<thead><th>Source</th><th>Hit</th><th>Score</th></thead>';
-    functions_table_html += '<tbody>';
-
-    for (function_source in gene.functions){
-        functions_table_html += '<tr>';
-
-        functions_table_html += '<td><b>' + function_source + '</b></td>';
-        if (gene.functions[function_source]) {
-            functions_table_html += '<td>' + gene.functions[function_source][0] + '</td>';
-            functions_table_html += '<td><em>' + gene.functions[function_source][1] + '</em></td>';
-        } else {
-            functions_table_html += '<td>&nbsp;</td>';
-            functions_table_html += '<td>&nbsp;</td>';
-        }
-
-        functions_table_html += '</tr>';
-    }
-
-    functions_table_html += '</tbody></table>';
-
-    return functions_table_html;
-}
-
-function show_sequence(gene_id) {
-    $.ajax({
-        type: 'GET',
-        cache: false,
-        url: '/data/gene/' + gene_id + '?timestamp=' + new Date().getTime(),
-        success: function(data) {
-            $('body').append('<div class="modal modal-sequence"> \
-                <div class="modal-dialog"> \
-                    <div class="modal-content"> \
-                        <div class="modal-header"> \
-                            <button class="close" data-dismiss="modal" type="button"><span>&times;</span></button> \
-                            <h4 class="modal-title">Split Sequence</h4> \
-                        </div> \
-                        <div class="modal-body"> \
-                            <div class="col-md-12"> \
-                                <textarea class="form-control" rows="16" onclick="$(this).select();" readonly>&gt;' + data['header'] + '\n' + data['sequence'] + '</textarea> \
-                            </div> \
-                        </div> \
-                        <div class="modal-footer"> \
-                            <button class="btn btn-default" data-dismiss="modal" type="button">Close</button> \
-                        </div> \
-                    </div> \
-                </div> \
-            </div>');
-            $('[data-toggle="popover"]').popover('hide');
-            $('.modal-sequence').modal('show');
-        }
-    });
-}
-
-function drawArrows(_start, _stop) {
-
-    width = VIEWER_WIDTH * 0.80;
-    genes = geneParser.filterData(_start, _stop);
-
-    removeGeneChart();
-
-    paths = contextSvg.append('svg:g')
-      .attr('id', 'gene-arrow-chart')
-      .attr('transform', 'translate(50, -10)');
-
-    paths.selectAll('path');
-
-    // Find the max_split
-    max_split = _stop - _start;
-
-    // Find the ratio based on screen width and max_split
-    ratio = width / max_split;
-
-    // Draw arrows
-    genes.forEach(function(gene) {
-
-      diff = 0;
-
-      if (gene.start_in_split < _start) {
-        start = 0;
-        diff  = _start - gene.start_in_split;
-      }
-
-      start = gene.start_in_split < _start ? 0 : (gene.start_in_split - _start) * ratio;
-      // start = ratio * (start - gene.start_in_split);
-      __stop= (gene.stop_in_split > _stop ? _stop : gene.stop_in_split);
-      stop  = (__stop - gene.start_in_split - diff) * ratio;
-
-      var y = 10 + (gene.level * 20);
-
-      color = (gene.functions !== null ? 'green' : 'gray');
-
-      // M10 15 l20 0
-      path = paths.append('svg:path')
-           .attr('d', 'M' + start +' '+ y +' l'+ stop +' 0')
-           .attr('stroke', color)
-           .attr('stroke-width', 6)
-           .attr('marker-end', function() {
-
-             if ((gene.direction == 'r' && gene.start_in_split > _start) ||
-                 (gene.direction == 'f' && gene.stop_in_split  < _stop)) {
-                   return 'url(#arrow_' + color + ')';
-                 }
-
-              return '';
-           })
-           .attr('transform', function() {
-               return gene.direction == 'r' ? "translate(" + (2*start+stop) + ", 0), scale(-1, 1)" : "";
-             })
-           .attr('data-content', get_gene_functions_table_html(gene) + '')
-	    .attr('data-toggle', 'popover');
-    });
-    $('[data-toggle="popover"]').popover({"html": true, "trigger": "click", "container": "body", "placement": "top"});
-}
-
-
-var base_colors = ['#CCB48F', '#727EA3', '#65567A', '#CCC68F', '#648F7D', '#CC9B8F', '#A37297', '#708059'];
-
-function get_comp_nt_color(nts){
-    if(nts == "CT" || nts == "TC")
-        return "red";
-    if(nts == "GA" || nts == "AG")
-        return "green";
-    if(nts == "AT" || nts == "TA")
-        return "blue";
-    if(nts == "CA" || nts == "AC")
-        return "purple";
-    if(nts == "GT" || nts == "TG")
-        return "orange";
-    else
-        return "black";
-}
 
 function Chart(options){
     this.coverage = options.coverage;
