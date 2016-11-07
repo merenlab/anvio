7b4f667fd5efa04ebc8162b4aaff64b6c26586ab
A. Murat Eren <a.murat.eren@gmail.com>
2016-11-07 10:23:20 -0600

    an interface for muscle

---
+++ anvio/drivers/muscle.py
@@ -0,0 +1,84 @@
+# coding: utf-8
+"""Interface to muscle."""
+
+import os
+import shutil
+
+import anvio
+import anvio.utils as utils
+import anvio.terminal as terminal
+import anvio.filesnpaths as filesnpaths
+
+from anvio.errors import ConfigError
+
+
+__author__ = "A. Murat Eren"
+__copyright__ = "Copyright 2016, The anvio Project"
+__credits__ = []
+__license__ = "GPL 3.0"
+__version__ = anvio.__version__
+__maintainer__ = "A. Murat Eren"
+__email__ = "a.murat.eren@gmail.com"
+
+
+run = terminal.Run()
+progress = terminal.Progress()
+pp = terminal.pretty_print
+
+
+class Muscle:
+    def __init__(self, progress=progress, run=run, program_name = 'muscle'):
+        """A class to streamline HMM runs."""
+        self.progress = progress
+        self.run = run
+
+        self.program_name = program_name
+
+        utils.is_program_exists(self.program_name)
+
+
+    def run_muscle_stdin(self, sequences_list, debug=True):
+        """Takes a list of tuples for sequences, and performs MSA using muscle.
+
+            >>> from anvio.drivers.muscle import Muscle
+            >>> m = Muscle()
+            >>> m.run_muscle_stdin([('seq1', 'ATCATCATCGA'), ('seq2', 'ATCGAGTCGAT')])
+            [(u'>seq1', u'ATCATCATCGA-'), (u'>seq2', u'ATCG-AGTCGAT')]
+
+        """
+
+        tmp_dir = filesnpaths.get_temp_directory_path()
+        log_file_path = os.path.join(tmp_dir, '00_log.txt')
+
+        self.run.info('Running %s' % self.program_name, '%d seqeunces will be aligned' % len(sequences_list))
+        self.run.info('Log file path', log_file_path)
+
+        sequences_data = ''.join(['>%s\n%s\n' % (t[0], t[1]) for t in sequences_list])
+        cmd_line = [self.program_name, '-quiet']
+
+        output = utils.run_command_STDIN(cmd_line, log_file_path, sequences_data)
+
+        if not output[0] == '>':
+            with open(log_file_path, "a") as log_file: log_file.write('# THIS IS THE OUTPUT YOU ARE LOOKING FOR:\n\n%s\n' % (output))
+            raise ConfigError, "Drivers::Muscle: Something went wrong with this run :/ The output does not\
+                                look alright. You can find the output in this log file."
+
+        alignments = []
+
+        # parse the output, and fill alignments
+        defline, seq = None, None
+        for line in [o for o in output.split('\n') if len(o)] + ['>']:
+            if line.startswith('>'):
+                if defline:
+                    alignments.append((defline, seq),)
+                defline, seq = line, None
+            else:
+                if not seq:
+                    seq = line
+                else:
+                    seq += line
+
+        if not debug:
+            shutil.rmtree(tmp_dir)
+
+        return alignments
