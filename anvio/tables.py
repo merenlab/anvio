019f0d554576c4847072b1df9f8aeba1ee0e5f87
ShaiberAlon <alon.shaiber@gmail.com>
2017-02-27 11:58:55 -0600

    profile DB version 20
    Added the table gene_detection_table to the profile database to
    upgrading from v19 to v20.

--- anvio/tables.py
+++ anvio/tables.py
@@ -13,7 +13,7 @@ __email__ = "a.murat.eren@gmail.com"
 
 
 contigs_db_version = "8"
-profile_db_version = "19"
+profile_db_version = "20"
 pan_db_version = "5"
 samples_info_db_version = "2"
 auxiliary_hdf5_db_version = "1"
