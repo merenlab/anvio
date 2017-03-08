7542b8763ddae440e12cfc0f25dd97ad4750e051
A. Murat Eren <a.murat.eren@gmail.com>
2017-03-07 22:58:39 -0600

    sad face.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 import glob
 
-anvio_version='2.2.1'
+anvio_version='2.2.2'
 
 requirements = [req.strip() for req in open('requirements.txt', 'rU').readlines() if not req.startswith('#')]
 
