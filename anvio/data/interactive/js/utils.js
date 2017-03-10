8d5387e69c35ac9a0b2db86483437bc473b7114e
Ozcan Esen <ozcanesen@gmail.com>
2017-03-10 14:57:35 -0600

    setup $.ajaxPrefilter using request prefix, add csrf cookie

--- anvio/data/interactive/js/utils.js
+++ anvio/data/interactive/js/utils.js
@@ -158,6 +158,33 @@ function fire_up_ncbi_blast(item_name, program, database, target)
         }
     });
 }
+//--------------------------------------------------------------------------------------------------
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+//--------------------------------------------------------------------------------------------------
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 //--------------------------------------------------------------------------------------------------
 
