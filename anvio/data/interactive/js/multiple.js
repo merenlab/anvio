745005f45f7bfd5f602445eaf6fbdbf53cf8e624
Ozcan Esen <ozcanesen@gmail.com>
2017-03-16 14:57:14 -0500

    add new default values for multiple select boxes

--- anvio/data/interactive/js/multiple.js
+++ anvio/data/interactive/js/multiple.js
@@ -68,6 +68,8 @@ $(document).ready(function() {
 
     $('.normalization_multiple, .type_multiple').on('change', function() {
         var new_val = this.value;
+        if (new_val == "")
+            return;
         var target_selector = '.' + this.getAttribute('class').replace('_multiple', '');
         var table = $(this).closest('table');
 
