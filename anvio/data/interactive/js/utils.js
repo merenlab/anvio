978f82f87d5327b4219e8bb2452ac0000b324f50
Ozcan Esen <ozcanesen@gmail.com>
2017-05-11 17:15:19 -0500

    set cookie function

--- anvio/data/interactive/js/utils.js
+++ anvio/data/interactive/js/utils.js
@@ -187,6 +187,18 @@ function getCookie(name) {
 }
 
 //--------------------------------------------------------------------------------------------------
+// https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+//--------------------------------------------------------------------------------------------------
 
 function showDraggableDialog(title, content, updateOnly)
 {
