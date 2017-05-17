06129df9bf83734aa6ed7e5f4ef36264fd86de18
A. Murat Eren <a.murat.eren@gmail.com>
2017-05-16 23:01:08 -0500

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 import glob
 
-anvio_version='2.3.2'
+anvio_version='2.3.2-master'
 
 requirements = [req.strip() for req in open('requirements.txt', 'rU').readlines() if not req.startswith('#')]
 
