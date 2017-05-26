e84618293abfd8b71ab36ec999557c915503edc2
Ozcan Esen <ozcanesen@gmail.com>
2017-05-26 09:52:30 -0500

    oops, fix wrong cookie

--- anvio/data/interactive/js/utils.js
+++ anvio/data/interactive/js/utils.js
@@ -190,13 +190,13 @@ function getCookie(name) {
 // https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
 function createCookie(name,value,days) {
     var expires = "";
+    var date = new Date();
     if (days == -1) {
-        expires = "; expires=" + Number.MAX_SAFE_INTEGER;
+        date.setTime(date.getTime() + (10*365*24*60*60*1000));
     } else if (days) {
-        var date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
     }
+    expires = "; expires=" + date.toUTCString();
     document.cookie = name + "=" + value + expires + "; path=/";
 }
 
