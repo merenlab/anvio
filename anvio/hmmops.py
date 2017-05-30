8b9eb2b98996090990effea479da30fffcac9fd9
meren <a.murat.eren@gmail.com>
2017-05-30 13:00:51 -0500

    a little more structure. even more will be needed since this module will likely play a critical role for phylogenomic efforts

--- anvio/hmmops.py
+++ anvio/hmmops.py
@@ -26,6 +26,9 @@ progress = terminal.Progress()
 
 class SequencesForHMMHits:
     def __init__(self, contigs_db_path, sources=set([]), run=run, progress=progress):
+        self.run = run
+        self.progress = progress
+
         if not isinstance(sources, type(set([]))):
             raise ConfigError("'sources' variable has to be a set instance.")
 
@@ -227,116 +230,145 @@ class SequencesForHMMHits:
         return (header, sequence)
 
 
-    def store_hmm_sequences_into_FASTA(self, hmm_sequences_dict_for_splits, output_file_path, wrap=120, concatenate_genes=False, separator = 'XXX', genes_order=None):
+    def __store_concatenated_hmm_sequences_into_FASTA(self, hmm_sequences_dict_for_splits, output_file_path, wrap=120, concatenate_genes=False, separator = 'XXX', genes_order=None):
+        """Generates concatenated sequences from `hmm_sequences_dict_for_splits` dict.
+
+           Please do NOT directly access to this function, and use `store_hmm_sequences_into_FASTA`
+           instead.
+        """
+
+        if len(self.sources) != 1:
+            raise ConfigError("If you want your genes to be concatenated, you should be requesting a single HMM source. Why?\
+                               In fact we are not exactly sure why. But when we think of it, we couldn't come up with a \
+                               scenario where the user might truly be interested in concatenating genes from multiple HMM\
+                               sources, and we wanted to add a control in case they are making a mistake w/o realizing. If you\
+                               are sure this is what you must do for the question you are interested in, please send an\
+                               e-mail to the anvi'o discussion group, and convince us .. or you can just delete this if block\
+                               to avoid this check if you are not in the mood. We know the feeling.")
+
+        hmm_source = self.sources.pop()
+        gene_names_in_source = [g.strip() for g in self.hmm_hits_info[hmm_source]['genes'].split(',')]
+
+        # the user wants to play rough. FINE. we will concatenate genes for phylogenomic analyses.
+        gene_names = None
+
+        # lets learn about what we have in this dictionary first.
+        bin_names_in_dict = list(set([x['bin_id'] for x in hmm_sequences_dict_for_splits.values()]))
+        gene_names_in_dict = sorted(list(set([x['gene_name'] for x in hmm_sequences_dict_for_splits.values()])))
+
+        # if the function is called with a particular set and order of genes, use those, otherwise
+        # stick with the gene names / order we found in the dictionary.
+        if genes_order:
+            genes_in_genes_order_but_missing_in_hmm_source = [g for g in genes_order if g not in gene_names_in_source]
+            if len(genes_in_genes_order_but_missing_in_hmm_source):
+                raise ConfigError("One or more gene names in the genes order list does seem to appear among the genes described\
+                                   by the HMM source %s (which translates to 'terrible news'). Here are the genes that cause this\
+                                   issue if you want to fix this: '%s'" \
+                                              % (hmm_source, ', '.join(genes_in_genes_order_but_missing_in_hmm_source)))
+            gene_names = genes_order
+        else:
+            self.run.warning("You did not define any gene names. Bold move. Now anvi'o will attempt to report a file with all\
+                              genes defined in the HMM source '%s'." % hmm_source)
+
+            gene_names = gene_names_in_dict
+
+        # gene lenghts are especially important to accommodate missing genes with proper number of
+        # gap characters
+        gene_lengths = {}
+
+        # buld a simpler dict that keeps genes sequences for each bin for a given gene name
+        genes_in_bins_dict = {}
+        for entry in hmm_sequences_dict_for_splits.values():
+            gene_name = entry['gene_name']
+            bin_name = entry['bin_id']
+            sequence = entry['sequence']
+            if gene_name in genes_in_bins_dict:
+                genes_in_bins_dict[gene_name][bin_name] = sequence
+            else:
+                genes_in_bins_dict[gene_name] = {bin_name: sequence}
+
+        # align homolog sequences across bins
+        self.progress.new('Aligning homolog gene sequences pre-concatenation')
+        m = Muscle(run=terminal.Run(verbose=False))
+        all_gene_names = list(genes_in_bins_dict.keys())
+        num_genes = len(all_gene_names)
+        for i in range(0, num_genes):
+            gene_name = all_gene_names[i]
+            self.progress.update('working on %s (%d of %d) ...' % (gene_name, i + 1, num_genes))
+            genes_list = [(bin_name, genes_in_bins_dict[gene_name][bin_name]) \
+                                                        for bin_name in genes_in_bins_dict[gene_name] \
+                                                                           if bin_name in genes_in_bins_dict[gene_name]]
+            genes_in_bins_dict[gene_name] = m.run_muscle_stdin(genes_list)
+            gene_lengths[gene_name] = len(list(genes_in_bins_dict[gene_name].values())[0])
+
+        # concatenate all of them and write them in a file
+        f = open(output_file_path, 'w')
+        gene_names_missing_from_everywhere = []
+        for bin_name in bin_names_in_dict:
+            sequences_list = []
+
+            for gene_name in gene_names:
+                if gene_name in genes_in_bins_dict:
+                    if bin_name in genes_in_bins_dict[gene_name]:
+                        sequences_list.append(genes_in_bins_dict[gene_name][bin_name])
+                    else:
+                        sequences_list.append('-' * gene_lengths[gene_name])
+                else:
+                    # if we are here, it means this is a gene that has been missing form the hmm hits dict, since it
+                    # was not in any of the bins the dict described, but the user requested to have it in the
+                    # alignment anyway. This can happen when the user wants to concatanate genes from one or more
+                    # low-completion bins. We will keep track of them, and tell the user.
+                    sequences_list.append('-' * 42)
+                    gene_names_missing_from_everywhere.append(gene_name)
+
+            sequence = separator.join(sequences_list)
+
+            if wrap:
+                sequence = textwrap.fill(sequence, wrap, break_on_hyphens=False)
+
+            f.write('>%s|genes:%s|separator:%s\n' % (bin_name, ','.join(gene_names), separator))
+            f.write('%s\n' % sequence)
+
+        if len(gene_names_missing_from_everywhere):
+            run.warning("You asked for some genes that were missing from all bins this class had in the\
+            HMM hits dictionary (here is a list of them: '%s'). Not knowing what to do with this werid\
+            situation, anvi'o put gap characters for all of them and retained your order. Here are those\
+            genes that missed the party: '%s'" % \
+                (', '.join(bin_names_in_dict), ', '.join(gene_names_missing_from_everywhere)))
+
+        f.close()
+
+
+    def __store_individual_hmm_sequences_into_FASTA(self, hmm_sequences_dict_for_splits, output_file_path, wrap=120, concatenate_genes=False, separator = 'XXX', genes_order=None):
+        """Stores every sequence in hmm_sequences_dict_for_splits into the `output_file_path`.
+
+           Please do NOT directly access to this function, and use `store_hmm_sequences_into_FASTA`
+           instead.
+        """
+
+        f = open(output_file_path, 'w')
+
+        for gene_unique_id in hmm_sequences_dict_for_splits:
+            header, sequence = self.get_FASTA_header_and_sequence_for_gene_unique_id(hmm_sequences_dict_for_splits, gene_unique_id)
+
+            if wrap:
+                sequence = textwrap.fill(sequence, wrap, break_on_hyphens=False)
+
+            f.write('>%s\n' % header)
+            f.write('%s\n' % sequence)
+
+        f.close()
+
+
+    def store_hmm_sequences_into_FASTA(self, hmm_sequences_dict_for_splits, output_file_path, wrap=120, concatenate_genes=False, separator=None, genes_order=None):
+        """Stores HMM sequences into a FASTA file."""
+
         filesnpaths.is_output_file_writable(output_file_path)
 
         if not isinstance(wrap, int):
             raise ConfigError('"wrap" has to be an integer instance')
 
         if concatenate_genes:
-            if len(self.sources) != 1:
-                raise ConfigError("If you want your genes to be concatenated, you should be requesting a single HMM source. Why?\
-                                   In fact we are not exactly sure why. But when we think of it, we couldn't come up with a \
-                                   scenario where the user might truly be interested in concatenating genes from multiple HMM\
-                                   sources, and we wanted to add a control in case they are making a mistake w/o realizing. If you\
-                                   are sure this is what you must do for the question you are interested in, please send an\
-                                   e-mail to the anvi'o discussion group, and convince us .. or you can just delete this if block\
-                                   to avoid this check if you are not in the mood. We know the feeling.")
-
-            hmm_source = self.sources.pop()
-            gene_names_in_source = [g.strip() for g in self.hmm_hits_info[hmm_source]['genes'].split(',')]
-
-            # the user wants to play rough. FINE. we will concatenate genes for phylogenomic analyses.
-            gene_names = None
-
-            # lets learn about what we have in this dictionary first.
-            bin_names_in_dict = list(set([x['bin_id'] for x in hmm_sequences_dict_for_splits.values()]))
-            gene_names_in_dict = sorted(list(set([x['gene_name'] for x in hmm_sequences_dict_for_splits.values()])))
-
-            # if the function is called with a particular set and order of genes, use those, otherwise
-            # stick with the gene names / order we found in the dictionary.
-            if genes_order:
-                genes_in_genes_order_but_missing_in_hmm_source = [g for g in genes_order if g not in gene_names_in_source]
-                if len(genes_in_genes_order_but_missing_in_hmm_source):
-                    raise ConfigError("One or more gene names in the genes order list does seem to appear among the genes described\
-                                       by the HMM source %s (which translates to 'terrible news'). Here are the genes that cause this\
-                                       issue if you want to fix this: '%s'" \
-                                                  % (hmm_source, ', '.join(genes_in_genes_order_but_missing_in_hmm_source)))
-                gene_names = genes_order
-            else:
-                gene_names = gene_names_in_dict
-
-            # gene lenghts are especially important to accommodate missing genes with proper number of
-            # gap characters
-            gene_lengths = {}
-
-            # buld a simpler dict that keeps genes sequences for each bin for a given gene name
-            genes_in_bins_dict = {}
-            for entry in hmm_sequences_dict_for_splits.values():
-                gene_name = entry['gene_name']
-                bin_name = entry['bin_id']
-                sequence = entry['sequence']
-                if gene_name in genes_in_bins_dict:
-                    genes_in_bins_dict[gene_name][bin_name] = sequence
-                else:
-                    genes_in_bins_dict[gene_name] = {bin_name: sequence}
-
-
-             # align homolog sequences across bins
-            m = Muscle(run=terminal.Run(verbose=False))
-            for gene_name in genes_in_bins_dict:
-                genes_list = [(bin_name, genes_in_bins_dict[gene_name][bin_name]) \
-                                                            for bin_name in genes_in_bins_dict[gene_name] \
-                                                                               if bin_name in genes_in_bins_dict[gene_name]]
-                genes_in_bins_dict[gene_name] = m.run_muscle_stdin(genes_list)
-                gene_lengths[gene_name] = len(list(genes_in_bins_dict[gene_name].values())[0])
-
-            # concatenate all of them and write them in a file
-            f = open(output_file_path, 'w')
-            gene_names_missing_from_everywhere = []
-            for bin_name in bin_names_in_dict:
-                sequences_list = []
-
-                for gene_name in gene_names:
-                    if gene_name in genes_in_bins_dict:
-                        if bin_name in genes_in_bins_dict[gene_name]:
-                            sequences_list.append(genes_in_bins_dict[gene_name][bin_name])
-                        else:
-                            sequences_list.append('-' * gene_lengths[gene_name])
-                    else:
-                        # if we are here, it means this is a gene that has been missing form the hmm hits dict, since it
-                        # was not in any of the bins the dict described, but the user requested to have it in the
-                        # alignment anyway. This can happen when the user wants to concatanate genes from one or more
-                        # low-completion bins. We will keep track of them, and tell the user.
-                        sequences_list.append('-' * 42)
-                        gene_names_missing_from_everywhere.append(gene_name)
-
-                sequence = separator.join(sequences_list)
-
-                if wrap:
-                    sequence = textwrap.fill(sequence, wrap, break_on_hyphens=False)
-
-                f.write('>%s|genes:%s|separator:%s\n' % (bin_name, ','.join(gene_names), separator))
-                f.write('%s\n' % sequence)
-
-            if len(gene_names_missing_from_everywhere):
-                run.warning("You asked for some genes that were missing from all bins this class had in the\
-                HMM hits dictionary (here is a list of them: '%s'). Not knowing what to do with this werid\
-                situation, anvi'o put gap characters for all of them and retained your order. Here are those\
-                genes that missed the party: '%s'" % \
-                    (', '.join(bin_names_in_dict), ', '.join(gene_names_missing_from_everywhere)))
-
-            f.close()
+            self.__store_concatenated_hmm_sequences_into_FASTA(hmm_sequences_dict_for_splits, output_file_path, wrap, concatenate_genes, separator, genes_order)
         else:
-            f = open(output_file_path, 'w')
-
-            for gene_unique_id in hmm_sequences_dict_for_splits:
-                header, sequence = self.get_FASTA_header_and_sequence_for_gene_unique_id(hmm_sequences_dict_for_splits, gene_unique_id)
-
-                if wrap:
-                    sequence = textwrap.fill(sequence, wrap, break_on_hyphens=False)
-
-                f.write('>%s\n' % header)
-                f.write('%s\n' % sequence)
-
-            f.close()
+            self.__store_individual_hmm_sequences_into_FASTA(hmm_sequences_dict_for_splits, output_file_path, wrap, concatenate_genes, separator, genes_order)
