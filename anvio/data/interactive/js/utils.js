2be11b5d6346c82b1380bcb58e8dcfeba52c6637
Ozcan Esen <ozcanesen@gmail.com>
2017-07-25 23:55:37 -0500

    move custom markdown renderer into utils.js

--- anvio/data/interactive/js/utils.js
+++ anvio/data/interactive/js/utils.js
@@ -152,6 +152,17 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+//-------------------------------------------------------------------------------------------------
+function renderMarkdown(content) {
+    var renderer = new marked.Renderer();
+
+    renderer.link = function( href, title, text ) {
+        return '<a target="_blank" href="' + href + '" title="' + title + '">' + text + '</a>';
+    }
+
+    return marked(content, { renderer:renderer });
+}
+
 //--------------------------------------------------------------------------------------------------
 function getCookie(name) {
     var cookieValue = null;
