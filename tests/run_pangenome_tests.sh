d1ed8efc58096fbb76c33fc3f8795def6c812bd6
A. Murat Eren <a.murat.eren@gmail.com>
2017-08-16 17:59:57 -0500

    more examples for AA and DNA exports

--- tests/run_pangenome_tests.sh
+++ tests/run_pangenome_tests.sh
@@ -34,15 +34,26 @@ anvi-pan-genome -g TEST-GENOMES.h5 -o TEST/ -n TEST --use-ncbi-blast --descripti
 INFO "Running the pangenome analysis again utilizing previous search results"
 anvi-pan-genome -g TEST-GENOMES.h5 -o TEST/ -n ANOTHER_TEST --use-ncbi-blast --min-occurrence 2 --description example_description.md
 
-INFO "Importing the default state for pretty outputs"
-anvi-import-state -p TEST/TEST-PAN.db -s default-state.json -n default
-anvi-import-state -p TEST/ANOTHER_TEST-PAN.db -s default-state.json -n default
-
 INFO "Importing an example collection of protein clusters"
 anvi-import-collection -p TEST/TEST-PAN.db -C test_collection example-PC-collection.txt
 
 INFO "Exporting the collection 'test_collection'"
 anvi-export-collection -p TEST/TEST-PAN.db -C test_collection -O exported_collection --include-unbinned
+
+INFO "Exporting aligned amino acid sequences for some protein clusters"
+anvi-export-pc-alignments -p TEST/TEST-PAN.db -g TEST-GENOMES.h5 -C test_collection -b PCB_1_CORE -o aligned_gene_sequences_in_PCB_1_CORE_AA.fa
+
+INFO "Exporting aligned DNA sequences for some protein clusters"
+anvi-export-pc-alignments -p TEST/TEST-PAN.db -g TEST-GENOMES.h5 -C test_collection -b PCB_1_CORE -o aligned_gene_sequences_in_PCB_1_CORE_DNA.fa --report-DNA-sequences
+
+INFO "First five line from the AA output"
+head -n 5 aligned_gene_sequences_in_PCB_1_CORE_AA.fa
+
+INFO "First five line from the DNA output"
+head -n 5 aligned_gene_sequences_in_PCB_1_CORE_DNA.fa
+
+INFO "Exporting concatenated amino acid sequences for some protein clusters for phylogenomics"
+anvi-export-pc-alignments -p TEST/TEST-PAN.db -g TEST-GENOMES.h5 -C test_collection -b PCB_1_CORE -o aligned_gene_sequences_in_PCB_1_CORE_AA.fa --concatenate-pcs
 
 INFO "Summarizing the pan, using the test collection (in quick mode)"
 anvi-summarize -p TEST/TEST-PAN.db -g TEST-GENOMES.h5 -C test_collection -o TEST_SUMMARY_QUICK --quick
@@ -53,11 +64,12 @@ anvi-summarize -p TEST/TEST-PAN.db -g TEST-GENOMES.h5 -C test_collection -o TEST
 INFO "Listing collections available"
 anvi-show-collections-and-bins -p TEST/TEST-PAN.db
 
+INFO "Importing the default state for pretty outputs"
+anvi-import-state -p TEST/TEST-PAN.db -s default-state.json -n default
+anvi-import-state -p TEST/ANOTHER_TEST-PAN.db -s default-state.json -n default
+
 INFO "Displaying the initial pangenome analysis results"
 anvi-display-pan -p TEST/TEST-PAN.db -s TEST/TEST-SAMPLES.db -g TEST-GENOMES.h5 --title "A mock pangenome analysis"
-
-INFO "Exporting aligned seqeunces for some protein clusters"
-anvi-export-pc-alignments -p TEST/TEST-PAN.db -g TEST-GENOMES.h5 -C test_collection -b PCB_1_CORE -o aligned_gene_sequences_in_PCB_1_CORE.fa
 
 INFO "Displaying the second pangenome analysis results"
 anvi-display-pan -p TEST/ANOTHER_TEST-PAN.db -s TEST/ANOTHER_TEST-SAMPLES.db -g TEST-GENOMES.h5 --title "A mock pangenome analysis (with --min-occurrence 2)"
