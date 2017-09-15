9de916562814f6ca933d020d5e2b9a9edb34c544
Ozcan Esen <ozcanesen@gmail.com>
2017-09-14 23:53:21 -0500

    add single copy gene counts chart to assembly summary

--- anvio/assemblystats.py
+++ anvio/assemblystats.py
@@ -4,38 +4,48 @@ import json
 
 import anvio
 import anvio.dbops as dbops
+import anvio.hmmops as hmmops
 
 class AssemblyStats():
     def __init__(self, args):
-        self.contigs_db = args.contigs_db
+        self.contigs_db_path = args.contigs_db
 
     def get_summary_dict(self):
         summary = {}
+        contigs_db = dbops.ContigsDatabase(self.contigs_db_path)
+        hmm = hmmops.SequencesForHMMHits(self.contigs_db_path)
 
-        contig_lengths = sorted(dbops.ContigsDatabase(self.contigs_db).db.get_single_column_from_table('contigs_basic_info', 'length'), reverse=True)
+        contig_lengths = sorted(contigs_db.db.get_single_column_from_table('contigs_basic_info', 'length'), reverse=True)
         total_length = sum(contig_lengths)
         size = len(contig_lengths)
 
         summary['total_length'] = total_length
         summary['size'] = size
-        summary['n_values'] = []
-
-        temp_length = 0
-        i = 0
-        n_index = 1
-        while n_index <= 100:
-            if (temp_length >= ((total_length / 100) * n_index)):
-                summary['n_values'].append({
-                    'num_contigs': i,
-                    'length': contig_lengths[i - 1]
-                    })
-
-                n_index += 1
-            else:
-                temp_length += contig_lengths[i]
-                i+=1
+        summary['n_values'] = self.calculate_N_values(contig_lengths, total_length, N=100)
+        summary['single_copy_gene_counts'] = hmm.get_single_copy_gene_counts()
 
         return summary
+
+
+    def calculate_N_values(self, contig_lengths, total_length, N=100):
+        results = []
+
+        temp_length = 0
+        contigs_index = 0
+        n_index = 1
+
+        while n_index <= N:
+            if (temp_length >= ((total_length / N) * n_index)):
+                results.append({
+                        'num_contigs': contigs_index,
+                        'length':      contig_lengths[contigs_index - 1]
+                    })
+                n_index += 1
+            else:
+                temp_length += contig_lengths[contigs_index]
+                contigs_index += 1
+
+        return results
 
 
 class AssemblyInteractive():
