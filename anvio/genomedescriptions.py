fde9bb7b72b8edc6cb6623905aa2f7093a99fc56
Ozcan Esen <ozcanesen@gmail.com>
2017-10-25 21:49:03 -0500

    some missing variables at genomedescriptions

--- anvio/genomedescriptions.py
+++ anvio/genomedescriptions.py
@@ -48,7 +48,9 @@ class GenomeDescriptions(object):
         self.external_genomes_dict = None
 
         A = lambda x: self.args.__dict__[x] if x in self.args.__dict__ else None
+        self.functions_are_available = False
         self.function_annotation_sources = set([])
+
         self.input_file_for_internal_genomes = A('internal_genomes')
         self.input_file_for_external_genomes = A('external_genomes')
         self.list_hmm_sources = A('list_hmm_sources')          # <<< these two are look out of place, but if the args requests
