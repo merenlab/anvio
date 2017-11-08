f522b1040a23d580cfc8283f15fde75b78dc0980
Ozcan Esen <ozcanesen@gmail.com>
2017-11-08 10:14:25 -0600

    db_entries -> db_entries_buffer

--- anvio/table/splitsinfo.py
+++ anvio/table/splitsinfo.py
@@ -4,7 +4,8 @@ import anvio.tables as t
 class InfoTableForSplits(object):
     def __init__(self, db):
         self.db = db
-        self.db_entries = []
+        self.db_entries_buffer = []
+
         self.total_splits = 0
 
 
@@ -12,9 +13,9 @@ class InfoTableForSplits(object):
         self.total_splits += 1
         sequence_length = len(sequence)
         db_entry = tuple([seq_id, order, start, end, sequence_length, utils.get_GC_content_for_sequence(sequence), parent_gc_content, parent])
-        self.db_entries.append(db_entry)
+        self.db_entries_buffer.append(db_entry)
 
 
     def store(self):
-        self.db.insert_many(t.splits_info_table_name, self.db_entries)
+        self.db.insert_many(t.splits_info_table_name, self.db_entries_buffer)
 
