1b9b2f7c366e53e294941ec3451abc69a4a1c941
A. Murat Eren <a.murat.eren@gmail.com>
2017-12-10 16:17:08 -0600

    upgrade-genomes-storage-v4-to-v5 does upgrade to v5, so this is appropriate.

--- anvio/tables.py
+++ anvio/tables.py
@@ -17,7 +17,7 @@ profile_db_version = "22"
 pan_db_version = "7"
 samples_info_db_version = "2"
 auxiliary_data_version = "2"
-genomes_storage_vesion = "4"
+genomes_storage_vesion = "5"
 
 versions_for_db_types = {'contigs': contigs_db_version,
                          'profile': profile_db_version,
