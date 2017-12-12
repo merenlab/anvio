f59c82885ba60cc36a08ca3277cbf13f2fb320bf
Ozcan Esen <ozcanesen@gmail.com>
2017-12-12 16:08:13 -0600

    this is dangerous, anvio upgrades profile db even though there is no auxiliary data

--- anvio/migrations/profile/v21_to_v22.py
+++ anvio/migrations/profile/v21_to_v22.py
@@ -36,7 +36,7 @@ def convert_numpy_array_to_binary_blob(array, compress=True):
         return memoryview(array)
 
 
-def migrate(db_path, just_do_it=False, ignore_auxiliary=False):
+def migrate(db_path):
     if db_path is None:
         raise ConfigError("No database path is given.")
 
@@ -46,34 +46,10 @@ def migrate(db_path, just_do_it=False, ignore_auxiliary=False):
     if str(profile_db.get_version()) != current_version:
         raise ConfigError("Version of this profile database is not %s (hence, this script cannot really do anything)." % current_version)
 
-    if not just_do_it:
-        try:
-            run.warning("This script will try to upgrade your profile database from v%s to v%s. If you think you are ready, just press ENTER \
-                         to continue. For those of you who want to know what is goin on: we have been using the HDF5 file format to keep our \
-                         nucleotide-level coverage data. However, we recenlty realized that our data structures do not necessarily exploit the \
-                         HDF5 format in a way to justify its use, and we decided to switch to self-contained sqlite databases. This script \
-                         will simply process your existing AUXILIARY-DATA.h5 file, and turn it into an AUXILIARY-DATA.db file. Although these \
-                         files will be identical with respect to their funciton, the latter can be up to 90%% smaller in size, so yay. If you want \
-                         to cancel the upgrade and think more about it, press CTRL+C now. If you want to avoid this message the next time, \
-                         use '--just-do-it'. PLEASE NOTE, running this script will remove the obsolete '.h5' file from your directory once \
-                         it is successfully finished upgrading your contigs database. If you would like to save it for some reason, please \
-                         consider backing it up first." % (current_version, next_version))
-            input("Press ENTER to continue...\n")
-        except:
-            print()
-            sys.exit()
+    auxiliary_path = os.path.join(os.path.dirname(db_path), 'AUXILIARY-DATA.h5')
+    new_auxiliary_path = os.path.join(os.path.dirname(db_path), 'AUXILIARY-DATA.db')
 
-    if ignore_auxiliary:
-        run.warning("Ignoring auxiliary data")
-    else:
-        auxiliary_path = os.path.join(os.path.dirname(db_path), 'AUXILIARY-DATA.h5')
-        new_auxiliary_path = os.path.join(os.path.dirname(db_path), 'AUXILIARY-DATA.db')
-
-        if not os.path.exists(auxiliary_path):
-            raise ConfigError("Althought he actual purpose of this script is to upgrade your AUXILIARY-DATA.h5 file, it doesn't seem to be where \
-                               anvi'o expects it to be. You *still* can upgrade your profile database if you use the flag --ignore-auxiliary. But \
-                               as a consequence you will not be able to use its auxiliary data with this profile database.")
-
+    if os.path.exists(auxiliary_path):
         fp = h5py.File(auxiliary_path, 'r')
         G = lambda x: fp.attrs[x].decode('utf-8') if isinstance(fp.attrs[x], bytes) else fp.attrs[x]
         auxiliary_db = db.DB(new_auxiliary_path, '2', new_database=True)
@@ -118,6 +94,10 @@ def migrate(db_path, just_do_it=False, ignore_auxiliary=False):
 
         run.info_single("(anvi'o just created a new, up-to-date auxiliary data file (which ends with extension .db), and deleted \
                          the old one (the one that ended with the extension .h5)).", nl_before=2, nl_after=1)
+    else:
+         run.warning("Althought he actual purpose of this script is to upgrade your AUXILIARY-DATA.h5 file, it doesn't seem to be where \
+                      anvi'o expects it to be, Anvi'o will upgrade your Profile.db alone but as a consequence you will not be able to \
+                      use its auxiliary data with this profile database.")
 
     # we also added a totally new table to this version:
     profile_db.create_table(item_additional_data_table_name, item_additional_data_table_structure, item_additional_data_table_types)
@@ -132,12 +112,10 @@ def migrate(db_path, just_do_it=False, ignore_auxiliary=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A simple script to upgrade profile database and AUXILIARY-DATA.h5 from version %s to version %s' % (current_version, next_version))
     parser.add_argument('profile_db', metavar = 'PROFILE_DB', help = "An anvi'o profile database of version %s" % current_version)
-    parser.add_argument('--just-do-it', default=False, action="store_true", help = "Do not bother me with warnings")
-    parser.add_argument('--ignore-auxiliary', default=False, action="store_true", help = "Do not bother me with warnings")
     args, unknown = parser.parse_known_args()
 
     try:
-        migrate(args.profile_db, just_do_it = args.just_do_it, ignore_auxiliary = args.ignore_auxiliary)
+        migrate(args.profile_db)
     except ConfigError as e:
         print(e)
         sys.exit(-1)
