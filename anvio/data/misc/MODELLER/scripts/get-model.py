7812d6faee1a48d4cff0e3f0b022db59ccb9d50f
Evan Kiefl <kiefl.evan@gmail.com>
2017-12-11 23:14:19 -0600

    rough around the edges, but works

--- anvio/data/misc/MODELLER/scripts/get-model.py
+++ anvio/data/misc/MODELLER/scripts/get-model.py
@@ -1,7 +1,14 @@
 # Step 4: model building
-#
-# This script should produce two models, 1fdx_my.B99990001.pdb and
-# 1fdx_my.B99990002.pdb.
+import sys
+ALIGNMENT          = sys.argv[1]
+TARGET_ID          = sys.argv[2]
+TEMPLATE_IDS_FILE  = sys.argv[3]
+NUM_MODELS         = int(sys.argv[4])
+DEVIATION          = float(sys.argv[5])
+
+# read in file written by MODELLER.run_align_to_templates()
+# creates tuple, e.g (1durA, 2fdnB, ...)
+template_ids = tuple(["".join(x.strip().split("\t")) for x in open(TEMPLATE_IDS_FILE).readlines()])
 
 from modeller import *
 from modeller.automodel import *    # Load the automodel class
@@ -10,16 +17,16 @@ log.verbose()
 env = environ(rand_seed=-12312)  # To get different models from another script
 
 # directories for input atom files
-env.io.atom_files_directory = ['../atom_files']
+env.io.atom_files_directory = ['./TEMPLATE_PDBS']
 
 a = automodel(env,
-              alnfile='alignment.ali',      # alignment filename
-              knowns=('1durA', '2fdnA'),    # codes of the templates
-              sequence='1fdx_my',           # code of the target
+              alnfile=ALIGNMENT,            # alignment filename
+              knowns=template_ids,          # codes of the templates
+              sequence=TARGET_ID,           # code of the target
               assess_methods=assess.GA341)  # request GA341 assessment
 a.starting_model= 1                 # index of the first model
-a.ending_model  = 2                 # index of the last model
+a.ending_model  = NUM_MODELS        # index of the last model
                                     # (determines how many models to calculate)
-a.deviation = 4.0                   # has to >0 if more than 1 model
+a.deviation = DEVIATION             # has to >0 if more than 1 model
 
 a.make()                            # do homology modelling
