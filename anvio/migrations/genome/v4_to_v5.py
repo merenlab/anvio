20ab44972ef769cc72d300f5c23642c619e3e5fe
Ozcan Esen <ozcanesen@gmail.com>
2017-12-12 14:50:45 -0600

    change db_type of genome data storage to 'genomestorage' with single S

--- anvio/migrations/genome/v4_to_v5.py
+++ anvio/migrations/genome/v4_to_v5.py
@@ -59,7 +59,7 @@ def migrate(db_path):
     genomes_db.create_table(genome_info_table_name, genome_info_table_structure, genome_info_table_types)
     genomes_db.create_table(gene_info_table_name, gene_info_table_structure, gene_info_table_types)
     genomes_db.create_table(genome_gene_function_calls_table_name, genome_gene_function_calls_table_structure, genome_gene_function_calls_table_types)
-    genomes_db.set_meta_value('db_type', 'genomes data storage')
+    genomes_db.set_meta_value('db_type', 'genomestorage')
     genomes_db.set_meta_value('creation_date', time.time())
     genomes_db.set_meta_value('hash', old_storage_hash)
     genomes_db.set_meta_value('functions_are_available', functions_are_available)
