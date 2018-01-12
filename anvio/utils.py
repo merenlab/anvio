d1f03b08a9f5570259c9c30e7a230ab5f2259189
A. Murat Eren <a.murat.eren@gmail.com>
2018-01-12 15:41:06 -0500

    make get_all_item_names_from_the_database function generic and simple

--- anvio/utils.py
+++ anvio/utils.py
@@ -1874,7 +1874,7 @@ def get_all_sample_names_from_the_database(db_path):
                             with %s databases." % db_type)
 
 
-def get_all_item_names_from_the_database(db_path):
+def get_all_item_names_from_the_database(db_path, run=run):
     """Return all split names or gene cluster names in a given database"""
 
     all_items = set([])
@@ -1882,26 +1882,32 @@ def get_all_item_names_from_the_database(db_path):
     database = db.DB(db_path, get_required_version_for_db(db_path))
     db_type = database.get_meta_value('db_type')
 
-    class Args: pass
-    args = Args()
-
     if db_type == 'profile':
-        args.profile_db = db_path
-        all_items = set(ProfileSuperclass(args).split_names)
+        if int(database.get_meta_value('blank')):
+            run.warning("Someone asked for the split names in a blank profile database. Sadly, anvi'o does not keep track\
+                         of split names in blank profile databases. This function will return an empty set as split names\
+                         to not kill your mojo, but whatever you were trying to do will not work :(")
+            return set([])
+        elif int(database.get_meta_value('merged')):
+            all_items = set(database.get_single_column_from_table('mean_coverage_Q2Q3_splits', 'contig'))
+        else:
+            all_items = set(database.get_single_column_from_table('atomic_data_splits', 'contig'))
     elif db_type == 'pan':
-        args.pan_db = db_path
-        all_items = set(PanSuperclass(args).gene_cluster_names)
+        all_items = set(database.get_single_column_from_table(t.pan_gene_clusters_table_name, 'gene_cluster_id'))
     elif db_type == 'contigs':
-        args.contigs_db = db_path
-        all_items = set(ContigsSuperclass(args).splits_basic_info.keys())
+        all_items = set(database.get_single_column_from_table(t.splits_info_table_name, 'split'))
     else:
+        database.disconnect()
         raise ConfigError("You wanted to get all items in the database %s, but no one here knows aobut its type. Seriously,\
                             what is '%s'?" % (db_path, db_type))
 
     if not len(all_items):
-        raise ConfigError("dbops::get_all_item_names_from_the_database speaking. Something that should never happen happened :/\
-                            There seems to be nothing in this %s database. Anvi'o is as confused as you are. Please get in touch\
-                            with a developer. They will love this story." % db_path)
+        database.disconnect()
+        raise ConfigError("utils::get_all_item_names_from_the_database speaking. Something that should never happen happened :/\
+                           There seems to be nothing in this %s database. Anvi'o is as confused as you are. Please get in touch\
+                           with a developer. They will love this story." % db_path)
+
+    database.disconnect()
 
     return all_items
 
