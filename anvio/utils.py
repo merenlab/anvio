fd210d6cae5419d9f5cda5d1d7b4a3ba62c5dcd7
A. Murat Eren <a.murat.eren@gmail.com>
2018-01-28 14:12:32 -0600

    missing import..

--- anvio/utils.py
+++ anvio/utils.py
@@ -739,6 +739,7 @@ def get_values_of_gene_level_coverage_stats_as_dict(gene_level_coverage_stats_di
     
     if as_pandas:
         # This option is used by the mcg-classifier.
+        import pandas as pd
         return pd.DataFrame(d)
     else:
         return d
