094277de2129c13974ab2869312b7f3d5fbe08a5
ShaiberAlon <alon.shaiber@gmail.com>
2018-02-06 13:56:02 -0600

    migrated snakemake workflows to anvio repo
    @meren @ozcan. This is
    re:https://github.com/merenlab/lab-fiesta/issues/159
    
    Meren, you said you would suggest a structure, so here I am suggesting
    one, and you can let me know if you want to change anything.

---
+++ workflows/assembly-based-metagenomics-workflow/test/00.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+C() {
+    echo -e "\033[1;30m\033[47m$1\033[0m"
+}
+
+INFO() { 
+    echo
+    echo
+    C ":: $1 ..."
+    echo
+
+    if [ "$2" == "wait" ];then
+        read -n 1 -s -r -p "Press any key to continue"
+    fi
+}
+
+SETUP_WITH_OUTPUT_DIR() {
+    if [ -z "$1"  ]
+    then
+        output_dir="`pwd`/sandbox/test-output"
+        rm -rf $output_dir
+    else
+        output_dir="$1/test-output"
+    fi
+
+    mkdir -p $output_dir
+
+    files="sandbox"
+
+    INFO "Output directory"
+    echo "$output_dir"
+    echo
+
+    INFO "Anvo'o version"
+    anvi-profile --version
+}
