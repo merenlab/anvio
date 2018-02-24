29aedbe953450f76278ddfe7c2614735aafaec9a
A. Murat Eren <a.murat.eren@gmail.com>
2018-02-24 11:51:46 -0600

    update alons help

--- anvio/__init__.py
+++ anvio/__init__.py
@@ -642,30 +642,29 @@ D = {
             ['--gene-mode'],
             {'default': False,
              'action': 'store_true',
-             'help': "Initiate the interactive interface in \"gene view\". In this view, the items are genes (instead of\
-                      splits of contigs). The following views are avilable: detection - the detection value of each gene\
-                      in each sample. The mean_coverage - the mean coverage of genes. non_outlier_mean_coverage\
-                       - the mean coverage of the non-outlier nucleotide positions of each gene in each sample (median absolute\
-                      deviation is used to remove outliers per gene per sample). Non_outlier_coverage_std - standrad deviation\
-                      of the coverage of non-outlier positions of genes in samples. You can also choose to order items\
-                      and layers according to each one of the aforementioned view datas. In addition, all layer ordering\
-                      that are avialable in the regular view (i.e. the splits view) are also available in \"gene view\",\
-                      so that, for example, you can choose to order the layers according to \"detection\", and that\
+             'help': "Initiate the interactive interface in \"gene mode\". In this mode, the items are genes (instead of\
+                      splits of contigs). The following views are avilable: detection (the detection value of each gene\
+                      in each sample). The mean_coverage (the mean coverage of genes). The non_outlier_mean_coverage\
+                      (the mean coverage of the non-outlier nucleotide positions of each gene in each sample (median absolute\
+                      deviation is used to remove outliers per gene per sample)). The non_outlier_coverage_std view (standrad deviation\
+                      of the coverage of non-outlier positions of genes in samples). You can also choose to order items\
+                      and layers according to each one of the aforementioned views. In addition, all layer ordering\
+                      that are avialable in the regular mode (i.e. the full mode where you have contigs/splits) are also\
+                      available in \"gene mode\", so that, for example, you can choose to order the layers according to \"detection\", and that\
                       would be the order according to the detection values of splits, whereas if you choose \"genes_detections\"\
                       then the order of layers would be according to the detection values of genes. Inspection and sequence\
                       functionality are available (through the right-click menu), except now sequences are of the specific gene.\
-                      Inspection has two modes available: \"Inspect Context\" - this brings you to the inspection page of the split\
-                      to which the gene belongs (the inspected gene will be highlighted in yellow in the bottom), \"Inspect Gene\" -\
-                      opens inspection view, but with only 100 nucleotides around each side of the gene (the purpose of this mode\
+                      Inspection has now two options available: \"Inspect Context\", which brings you to the inspection page of the split\
+                      to which the gene belongs where the inspected gene will be highlighted in yellow in the bottom, and \"Inspect Gene\",\
+                      whih opens the inspection page only for the gene and 100 nts around each side of it (the purpose of this option\
                       is to make the inspection page load faster if you only want to look at the nucleotide coverage of a specific gene).\
-                      NOTICE: You can't save a state, nor store collections while in \"gene view\". You can make selections, and\
-                      create bins for viewing purposes only. Search options are available, and you can even search for functions\
+                      NOTICE: You can't store states or collections in \"gene mode\". However, you still can make fake selections, and create\
+                      fake bins for your viewing covenience only (smiley). Search options are available, and you can even search for functions\
                       if you have them in your contigs database. ANOTHER NOTICE: loading this mode might take a while if your bin\
                       has many genes, and your profile database has many samples, this is beacause the gene coverages stats are\
                       computed in an ad-hoc manner when you load this mode, we know this is not ideal and we plan to improve that\
                       (along with other things). If you have suggestions/complaints regarding this mode please comment on this\
-                      github issue: https://goo.gl/yHhRei. As of the day of the release of this anvi'o version, we are working\
-                      on a tutorial dedicated for this mode, and we will soon put it online, so look out for it."}
+                      github issue: https://goo.gl/yHhRei. Please refer to the online tutorial for more information."}
                 ),
     'gene-caller-id': (
             ['--gene-caller-id'],
