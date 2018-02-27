4151ae1df794896d9b1c3870e16d7b33a9cc6f8c
ShaiberAlon <alon.shaiber@gmail.com>
2018-02-27 13:34:23 -0600

    acceptable params for iu_... in metagenomics workflow

--- anvio/workflows/metagenomics/__init__.py
+++ anvio/workflows/metagenomics/__init__.py
@@ -29,7 +29,7 @@ class MetagenomicsWorkflow(ContigsDBWorkflow, WorkflowSuperClass):
     def __init__(self):
         ContigsDBWorkflow.__init__(self, config)
 
-        self.rules = ['gen_configs', 'qc', 'gen_qc_report', 'gzip_fastqs',\
+        self.rules = ['iu_gen_configs', 'iu_filter_quality_minoche', 'gen_qc_report', 'gzip_fastqs',\
                      'fq2fa', 'merge_fastas_for_co_assembly', 'megahit', 'anvi_script_anvi_script_reformat_fasta',\
                      'anvi_gen_contigs_database', 'export_gene_calls', 'centrifuge',\
                      'anvi_import_taxonomy', 'anvi_run_hmms', 'anvi_run_ncbi_cogs',\
@@ -39,8 +39,8 @@ class MetagenomicsWorkflow(ContigsDBWorkflow, WorkflowSuperClass):
         rule_acceptable_params_dict = {}
 
         # defining the accesible params per rule
-        rule_acceptable_params_dict['gen_configs'] = []
-        rule_acceptable_params_dict['qc'] = []
+        rule_acceptable_params_dict['iu_gen_configs'] = ["--r1-prefix", "--r2-prefix"]
+        rule_acceptable_params_dict['iu_filter_quality_minoche'] = ['visualize_quality_curves', 'ignore_deflines', 'limit_num_pairs', 'print_qual_scores', 'store_read_fate']
         rule_acceptable_params_dict['gen_qc_report'] = []
         rule_acceptable_params_dict['gzip_fastqs'] = []
         rule_acceptable_params_dict['fq2fa'] = []
