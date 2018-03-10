e4edff6271af16d5be484287a1113a43a060ed7f
ShaiberAlon <alon.shaiber@gmail.com>
2018-03-09 20:23:33 -0600

    run_contigs_workflow_tests.sh now works
    just do:
    ```
    cd $ANVIO_FOLDER/test
    ./run_contigs_workflow_tests.sh
    ```
    and then
    ```
    cd sandbox/test-output/workflow_test/
    ```
    And you can see that contigs databases were created, and annotated with
    default hmms and with COGS.
    @meren, this should help you (and anyone else) next time you want to
    play with the workflow code. I did some ugly shortcuts that I'm
    counting on your help. Also, when you do `--list-dependencies` then it
    doesn't exactly list them, but it prints the relevant information to a
    file (along with a lot of garbage), I would love to get your or ozcan's
    help to make it work :-)

--- tests/run_contigs_workflow_tests.sh
+++ tests/run_contigs_workflow_tests.sh
@@ -8,6 +8,14 @@ SETUP_WITH_OUTPUT_DIR $1
 INFO "Setting up the pan analysis directory"
 mkdir $output_dir/workflow_test
 cp $files/mock_data_for_pangenomics/*.fa $output_dir/workflow_test/
-cp ../anvio/workflows/contigs/Snakefile  $output_dir/workflow_test/
+cp $files/workflows/contigs/* $output_dir/workflow_test/
 cd $output_dir/workflow_test
 
+INFO "Creating a default config for contigs workflow"
+anvi-run-snakemake-workflow -w contigs --get-default-config default-config.json
+
+INFO "Listing dependencies for contigs workflow"
+anvi-run-snakemake-workflow -w contigs -c default-config.json --list-dependencies
+
+INFO "Running contigs workflow"
+anvi-run-snakemake-workflow -w contigs -c default-config.json
