f2341f499534806eee979f6e90fe9569857d0732
ShaiberAlon <alon.shaiber@gmail.com>
2018-03-13 14:05:28 -0500

    restructure bowtie rule in metagenomics workflow

--- anvio/workflows/metagenomics/__init__.py
+++ anvio/workflows/metagenomics/__init__.py
@@ -59,7 +59,7 @@ class MetagenomicsWorkflow(ContigsDBWorkflow, WorkflowSuperClass):
                                                   "--use-gpu", "--gpu-mem", "--keep-tmp-files",
                                                   "--tmp-dir", "--continue", "--verbose"]
         rule_acceptable_params_dict['bowtie_build'] = []
-        rule_acceptable_params_dict['bowtie'] = []
+        rule_acceptable_params_dict['bowtie'] = ["additional_params"]
         rule_acceptable_params_dict['samtools_view'] = ["additional_params"]
         rule_acceptable_params_dict['anvi_init_bam'] = []
         rule_acceptable_params_dict['anvi_profile'] = []
@@ -78,6 +78,7 @@ class MetagenomicsWorkflow(ContigsDBWorkflow, WorkflowSuperClass):
         self.default_config.update({'megahit': {"--min_contig": 1000, "--memory": 0.4, "threads": 11},
                                     'iu_filter_quality_minoche': {"--ignore-deflines": True, "threads": 2},
                                     "gzip_fastqs": {"run": True},
+                                    "bowtie": {"additional_params": "--no-unal", "threads": 10},
                                     "samtools_view": {"additional_params": "-F 4", "threads": 4}})
 
         self.rules_dependencies.update({'megahit': 'megahit',
