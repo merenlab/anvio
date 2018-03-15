ab7a9d94e4a49d700342778543b8d190c1f85398
ShaiberAlon <alon.shaiber@gmail.com>
2018-03-15 01:20:40 -0500

    added test for metagenomics workflow
    It currently fails, sadly

---
+++ tests/run_metagenomics_workflow_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source 00.sh
+
+# Setup #############################
+SETUP_WITH_OUTPUT_DIR $1
+#####################################
+
+INFO "Setting up the metagenomics analysis directory"
+mkdir $output_dir/workflow_test
+cp -r $files/workflows/metagenomics/* $output_dir/workflow_test/
+cd $output_dir/workflow_test
+
+INFO "unzipping fasta files"
+gzip -d three_samples_example/*.fa.gz
+
+INFO "Creating a default config for metagenomics workflow"
+anvi-run-snakemake-workflow -w metagenomics --get-default-config default-config.json
+
+INFO "Runnind a dry run"
+anvi-run-snakemake-workflow -w metagenomics --config config.json --dry-run
