788e4ec08866e3c4f656ad76e878d3bafcdd3fa0
ShaiberAlon <alon.shaiber@gmail.com>
2018-03-21 10:50:45 -0500

    added test for metagenomics workflow with no qc and no gzip

--- tests/run_metagenomics_workflow_tests.sh
+++ tests/run_metagenomics_workflow_tests.sh
@@ -16,17 +16,23 @@ gzip -d three_samples_example/*.fa.gz
 INFO "Creating a default config for metagenomics workflow"
 anvi-run-snakemake-workflow -w metagenomics --get-default-config default-config.json
 
-INFO "List dependencies for metagenomics workflow"
+INFO "List dependencies for metagenomics workflow with megahit"
 anvi-run-snakemake-workflow -w metagenomics --config config-megahit.json --list-dependencies
 
-INFO "Runnind a dry run"
+INFO "Runnind a dry run for metagenomics workflow with megahit"
 anvi-run-snakemake-workflow -w metagenomics --config config-megahit.json --dry-run
+
+INFO "Runnind a dry run for metagenomics workflow with megahit with no qc"
+anvi-run-snakemake-workflow -w metagenomics --config config-megahit-no-qc.json --dry-run
 
 INFO "List dependencies for metagenomics workflow in references mode"
 anvi-run-snakemake-workflow -w metagenomics --config config-references-mode.json --list-dependencies
 
 INFO "Runnind a dry run in references mode"
 anvi-run-snakemake-workflow -w metagenomics --config config-references-mode.json --dry-run
+
+INFO "Runnind a dry run in references mode with no qc and no gzip"
+anvi-run-snakemake-workflow -w metagenomics --config config-references-mode-no-qc-no-gzip.json --dry-run
 
 INFO "List dependencies for metagenomics workflow with idba_ud"
 anvi-run-snakemake-workflow -w metagenomics --config config-idba_ud.json --list-dependencies
