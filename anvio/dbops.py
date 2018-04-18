ee19c397d4f7e668d725464f08c6169cca8490a2
A. Murat Eren <a.murat.eren@gmail.com>
2018-04-17 22:02:36 -0500

    fixy

--- anvio/dbops.py
+++ anvio/dbops.py
@@ -2315,7 +2315,7 @@ class ProfileDatabase:
             meta_table = self.db.get_table_as_dict('self')
             self.meta = dict([(k, meta_table[k]['value']) for k in meta_table])
 
-            for key in ['min_contig_length', 'SNVs_profiled', 'AA_frequencies_profiled', 'min_coverage_for_variability',
+            for key in ['min_contig_length', 'SNVs_profiled', 'SCVs_profiled', 'min_coverage_for_variability',
                         'merged', 'blank', 'contigs_ordered', 'report_variability_full', 'num_contigs',
                         'num_splits', 'total_length']:
                 try:
