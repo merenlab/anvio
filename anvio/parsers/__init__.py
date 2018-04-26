b2c9f74471fa1793a7cc7eb7ba0c306099d0632c
A. Murat Eren <a.murat.eren@gmail.com>
2018-04-26 09:49:23 -0500

    cleaner design.

--- anvio/parsers/__init__.py
+++ anvio/parsers/__init__.py
@@ -10,6 +10,10 @@
 #
 # Please read the COPYING file.
 
+import anvio.terminal as terminal
+
+from anvio.errors import ConfigError
+
 from anvio.parsers.defaultmatrix import DefaultMatrix
 from anvio.parsers.centrifuge import Centrifuge
 from anvio.parsers.hmmscan import HMMScan
@@ -17,8 +21,35 @@ from anvio.parsers.concoct import CONCOCT
 from anvio.parsers.interproscan import InterProScan
 
 parser_modules = {}
-parser_modules['taxonomy'] = {"default_matrix": DefaultMatrix,
-                              "centrifuge": Centrifuge}
-parser_modules['functions'] = {"interproscan": InterProScan}
-parser_modules['search'] = {"hmmscan": HMMScan}
-parser_modules['collections'] = {"concoct": CONCOCT}
+parser_modules['taxonomy_genes']  = {"default_matrix": DefaultMatrix, "centrifuge": Centrifuge}
+parser_modules['taxonomy_layers'] = {"default_matrix": DefaultMatrix, "kraken_hll": Centrifuge}
+parser_modules['functions']       = {"interproscan": InterProScan}
+parser_modules['search']          = {"hmmscan": HMMScan}
+parser_modules['collections']     = {"concoct": CONCOCT}
+
+run = terminal.Run()
+
+def get_parser_names(module):
+    parser_module = get_parser_module(module)
+
+    return list(parser_module.keys())
+
+
+def get_parser_module(module):
+    if module not in parser_modules:
+        raise ConfigError("Anvi'o parser modules do not recognize any module called '%s'. But it has\
+                           these in case if your honour would change their minds: '%s'." % \
+                                (module, ', '.join(list(parser_modules.keys()))))
+
+    return parser_modules[module]
+
+
+def get_parser_obj(module, parser):
+    parser_module = get_parser_module(module)
+ 
+    if parser not in parser_module:
+        raise ConfigError("Parser modules speaking: the parser module '%s' does exist (yay),\
+                           but there is no parser '%s' in it (boo). It has these instad: '%s'."\
+                                % (module, parser, ', '.join(list(parser_module.keys()))))
+
+    return parser_modules[module][parser]
