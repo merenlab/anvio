c2673efa0dca91855be2618e1aafc006fa60ae99
Ozcan Esen <ozcanesen@gmail.com>
2018-04-29 14:00:47 -0500

    migrate all relevant keys

--- anvio/data/interactive/js/migrations.js
+++ anvio/data/interactive/js/migrations.js
@@ -3,9 +3,20 @@ function migrate_state(state) {
     let current_version = state['version'];
 
     if (current_version == '1') {
-        if (state.hasOwnProperty('samples-layers')) {
-            let layer_info = $.extend(true, {}, state['samples-layers']);
-            delete state['samples-layers']
+        ['samples-layers', 'samples-categorical-colors', 'samples-stack-bar-colors'].forEach((key) => {
+            if (state.hasOwnProperty(key)) {
+                let backup = $.extend(true, {}, state[key]); // deep copy
+                delete state[key];
+
+                state[key] = {'default': backup};
+            }
+
+        });
+        
+        if (state.hasOwnProperty('samples-layer-order')) {
+            let new_order = convert_samples_order_to_array(state['samples-layer-order']);
+            delete state['samples-layer-order'];
+            state['samples-layer-order'] = new_order;
         }
 
         state['version'] = '2';
