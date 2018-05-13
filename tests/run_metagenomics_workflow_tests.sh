9838deb88cf223fa902c06e43b7480fd347c816f
A. Murat Eren <a.murat.eren@gmail.com>
2018-05-13 15:05:40 -0500

    fixy

--- tests/run_metagenomics_workflow_tests.sh
+++ tests/run_metagenomics_workflow_tests.sh
@@ -28,26 +28,26 @@ anvi-run-snakemake-workflow -w metagenomics --get-default-config default-config.
 INFO "List dependencies for metagenomics workflow with megahit"
 anvi-run-snakemake-workflow -w metagenomics --config config-megahit.json --list-dependencies
 
-INFO "Runnind a dry run for metagenomics workflow with megahit"
+INFO "Running a dry run for metagenomics workflow with megahit"
 anvi-run-snakemake-workflow -w metagenomics --config config-megahit.json --dry-run
 
-INFO "Runnind a dry run for metagenomics workflow with megahit with no qc"
+INFO "Running a dry run for metagenomics workflow with megahit with no qc"
 anvi-run-snakemake-workflow -w metagenomics --config config-megahit-no-qc-all-against-all.json --dry-run
 
 INFO "List dependencies for metagenomics workflow in references mode"
 anvi-run-snakemake-workflow -w metagenomics --config config-references-mode.json --list-dependencies
 
-INFO "Runnind a dry run in references mode"
+INFO "Running a dry run in references mode"
 anvi-run-snakemake-workflow -w metagenomics --config config-references-mode.json --dry-run
 
-INFO "Runnind a dry run in references mode with no qc and no gzip"
+INFO "Running a dry run in references mode with no qc and no gzip"
 anvi-run-snakemake-workflow -w metagenomics --config config-references-mode-no-qc-no-gzip-no-groups.json --dry-run
 
 INFO "List dependencies for metagenomics workflow with idba_ud"
 anvi-run-snakemake-workflow -w metagenomics --config config-idba_ud.json --list-dependencies
 
-INFO "Runnind a dry run with idba_ud"
+INFO "Running a dry run with idba_ud"
 anvi-run-snakemake-workflow -w metagenomics --config config-idba_ud.json --dry-run
 
-INFO "Runnind a dry run with idba_ud with no qc"
+INFO "Running a dry run with idba_ud with no qc"
 anvi-run-snakemake-workflow -w metagenomics --config config-idba_ud-no-qc.json --list-dependencies
