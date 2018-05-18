c8939d6761635839708459395ef69638800b84e2
Ozcan Esen <ozcanesen@gmail.com>
2018-05-18 15:14:02 -0500

    save tree dialog, not finished

---
+++ anvio/data/interactive/js/dialogs/save-tree.js
@@ -0,0 +1,102 @@
+/**
+ *  Dialog for saving modified trees.
+ *
+ *  Author: Özcan Esen <ozcanesen@gmail.com>
+ *  Credits: A. Murat Eren
+ *  Copyright 2018, The anvio Project
+ *
+ * This file is part of anvi'o (<https://github.com/merenlab/anvio>).
+ * 
+ * Anvi'o is a free software. You can redistribute this program
+ * and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with anvi'o. If not, see <http://opensource.org/licenses/GPL-3.0>.
+ *
+ * @license GPL-3.0+ <http://opensource.org/licenses/GPL-3.0>
+ */
+
+
+function SaveTreeDialog() {
+    this.dialog = document.createElement('div');
+    this.dialog.setAttribute('class', 'modal fade in');
+
+    this.dialog.innerHTML = `<div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button class="close" data-dismiss="modal" type="button"><span>&times;</span></button>
+                    <h4 class="modal-title">Save Tree</h4>
+                </div>
+
+                <div class="modal-body">
+                    <div class="col-md-12">
+                        <label class="col-md-4 settings-label"><input type="radio" name="overwrite[]" value="no" checked="checked">Create new tree:</label>  
+                        <div class="col-md-8">
+                            <input type="text" id="tree_name" value="new_tree">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <label class="col-md-4 settings-label"><input type="radio" name="overwrite[]" value="yes">Overwrite exists tree:</label>  
+                        <div class="col-md-8">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button class="btn btn-primary" data-dismiss="modal" type="button">Save</button>
+                    <button class="btn btn-default" data-dismiss="modal" type="button">Close</button>
+                </div>
+            </div>
+        </div>`;
+
+    this.dialog.querySelector('.btn-primary').addEventListener('click', (event) => { this.SaveTree(); });
+    this.dialog.querySelectorAll('input[type="radio"]').forEach((input) => { 
+        input.addEventListener('click', (event) => { 
+            if (this.dialog.querySelectorAll('input[type="radio"]')[0].checked) {
+                this.dialog.querySelector('#tree_name').disabled = false;
+            } else {
+                this.dialog.querySelector('#tree_name').disabled = true;
+            }
+        })
+    });
+};
+
+
+SaveTreeDialog.prototype.Show = async function() {
+
+    $(this.dialog).modal('show').on('hidden.bs.modal', () => this.dialog.remove());
+};
+
+
+
+SaveTreeDialog.prototype.LoadCollection = function() {
+    let collection_name = this.dialog.querySelector('.collection-list').value;
+    
+    if (collection_name == "") {
+        toastr.warning('Please select a collection.');
+    }
+
+    if (bins.GetTotalSelectionCount() > 0 && !confirm("You are trying to load a collection but you already have some selections,\
+                                                       you will lose them if they are not stored. Do you want to continue?")) {
+        return;
+    }
+
+    let threshold_value = this.dialog.querySelector('.threshold-value').value;
+    let threshold_base  = this.dialog.querySelector('.threshold-base').value;
+    let threshold = parseInt(threshold_value) * parseInt(threshold_base);
+
+    $.ajax({
+        type: 'GET',
+        cache: false,
+        url: '/data/collection/' + collection_name,
+        success: function(data) {
+            bins.ImportCollection(data, threshold);
+            
+            $(this.dialog).modal('hide');
+            this.dialog.remove();
+        }.bind(this)
+    });
+};
+
