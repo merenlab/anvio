375860ae2e55ec6a70e81228b7b29e9fc46f76ba
Ozcan Esen <ozcanesen@gmail.com>
2018-05-30 15:45:16 -0500

    one more

--- anvio/migrations/genomestorage/v4_to_v5.py
+++ anvio/migrations/genomestorage/v4_to_v5.py
@@ -47,7 +47,7 @@ def migrate(db_path):
 
     fp = h5py.File(db_path, 'r')
 
-    if fp.attrs['version'] != current_version:
+    if int(fp.attrs['version']) != int(current_version):
         fp.close()
         raise ConfigError("Genome storage version is not %s." % current_version)
 
