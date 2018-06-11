405c44bacffd3e72fafe1983fe831c013c516466
Ozcan Esen <ozcanesen@gmail.com>
2018-06-11 16:02:13 -0500

    generate order for layer additional

--- anvio/tables/miscdata.py
+++ anvio/tables/miscdata.py
@@ -315,16 +315,8 @@ class OrderDataBaseClass(AdditionalAndOrderDataBaseClass, object):
         #        they describe.
 
         for data_key in additional_data_keys:
-            if '!' in data_key:
-                # we don't order stacked bar charts
-                continue
-
-            # predict the type for proper assignment of 'null' values
-            if '!' in data_key:
-                predicted_key_type = "stackedbar"
-            else:
-                type_class = utils.get_predicted_type_of_items_in_a_dict(additional_data_dict, data_key)
-                predicted_key_type = type_class.__name__ if type_class else 'unknown'
+            type_class = utils.get_predicted_type_of_items_in_a_dict(additional_data_dict, data_key)
+            predicted_key_type = type_class.__name__ if type_class else 'unknown'
 
             layer_name_layer_data_tuples = [(additional_data_dict[layer][data_key] if additional_data_dict[layer][data_key] else self.nulls_per_type[predicted_key_type], layer) for layer in additional_data_dict]
             order_data_dict['>> ' + data_key] = {'newick': None, 'basic': ','.join([t[1] for t in sorted(layer_name_layer_data_tuples)])}
