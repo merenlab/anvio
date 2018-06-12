986f9b8a3f1a1fed4a267f74f0b29cc31ada0ec2
Ozcan Esen <ozcanesen@gmail.com>
2018-06-12 11:39:22 -0500

    sum stack bar entries per item not all

--- anvio/tables/miscdata.py
+++ anvio/tables/miscdata.py
@@ -309,19 +309,23 @@ class OrderDataBaseClass(AdditionalAndOrderDataBaseClass, object):
         if order_data_dict:
             self.data_dict_sanity_check(order_data_dict, treat_data_dict_as='layer_orders')
 
-        self.data_dict_sanity_check(additional_data_dict, data_keys_list=additional_data_keys, treat_data_dict_as='layers')
-
         # FIXME: here we need to check whether the two dictionaries are in fact 'compatible' with respect to sample names
         #        they describe.
+        self.data_dict_sanity_check(additional_data_dict, data_keys_list=additional_data_keys, treat_data_dict_as='layers')
 
         sum_stackbar_items = {}
         for data_key in additional_data_keys:
             if '!' in data_key:
                 stackbar_name = data_key.split('!')[0]
-                sum_stackbar_items[stackbar_name] = 0
+
+                if stackbar_name not in sum_stackbar_items:
+                    sum_stackbar_items[stackbar_name] = {}
 
                 for layer in additional_data_dict:
-                    sum_stackbar_items[stackbar_name] += float(additional_data_dict[layer][data_key])
+                    if layer not in sum_stackbar_items[stackbar_name]:
+                        sum_stackbar_items[stackbar_name][layer] = 0.0
+
+                    sum_stackbar_items[stackbar_name][layer] += float(additional_data_dict[layer][data_key])
 
         for data_key in additional_data_keys:
             if '!' in data_key:
@@ -332,7 +336,7 @@ class OrderDataBaseClass(AdditionalAndOrderDataBaseClass, object):
 
             if predicted_key_type == "stackedbar":
                 stackbar_name = data_key.split('!')[0]
-                layer_name_layer_data_tuples = [(float(additional_data_dict[layer][data_key]) * 1.0 / sum_stackbar_items[stackbar_name] if additional_data_dict[layer][data_key] else self.nulls_per_type[predicted_key_type], layer) for layer in additional_data_dict]
+                layer_name_layer_data_tuples = [(float(additional_data_dict[layer][data_key]) / (1.0 * sum_stackbar_items[stackbar_name][layer]) if additional_data_dict[layer][data_key] else self.nulls_per_type[predicted_key_type], layer) for layer in additional_data_dict]
             else:
                 layer_name_layer_data_tuples = [(additional_data_dict[layer][data_key] if additional_data_dict[layer][data_key] else self.nulls_per_type[predicted_key_type], layer) for layer in additional_data_dict]
             order_data_dict['>> ' + data_key] = {'newick': None, 'basic': ','.join([t[1] for t in sorted(layer_name_layer_data_tuples)])}
