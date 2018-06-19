52524e8b3d7b708121201c459729b9122b636f46
Ozcan Esen <ozcanesen@gmail.com>
2018-06-19 15:43:40 -0500

    version bump for profile and pan dbs

--- anvio/tables/__init__.py
+++ anvio/tables/__init__.py
@@ -14,8 +14,8 @@ __email__ = "a.murat.eren@gmail.com"
 
 
 contigs_db_version = "12"
-profile_db_version = "28"
-pan_db_version = "11"
+profile_db_version = "29"
+pan_db_version = "12"
 auxiliary_data_version = "2"
 genomes_storage_vesion = "6"
 
