ae6739bbcaefd2b52e693d3f48fc16a7f37b75cb
A. Murat Eren <a.murat.eren@gmail.com>
2018-06-26 17:08:28 -0500

    a new place for anvi'o items dict

--- anvio/programs.py
+++ anvio/programs.py
@@ -31,6 +31,68 @@ M = lambda m: [x for x in G(os.path.dirname(utils.is_program_exists(m)))]
 S = lambda s: [x for x in G(os.path.dirname(utils.is_program_exists(s)))]
 J = lambda x: '\n'.join(x) if x else ''
 
+# this dictionary describes all anvi'o items that are referred from 'requires' and
+# 'provudes' statements written in anvi'o programs
+ANVIO_ITEMS = {'pan-db': {'name': 'PAN', 'type': 'DB', 'internal': True},
+               'contigs-db': {'name': 'CONTIGS', 'type': 'DB', 'internal': True},
+               'contigs-fasta': {'name': 'CONTIGS', 'type': 'FASTA', 'internal': False},
+               'concatenated-gene-alignment-fasta': {'name': 'CONCATENATED GENE ALIGNMENT', 'type': 'FASTA', 'internal': False},
+               'short-reads-fasta': {'name': 'SHORT READS', 'type': 'FASTA', 'internal': False},
+               'genes-fasta': {'name': 'GENES', 'type': 'FASTA', 'internal': False},
+               'bam-file': {'name': 'BAM FILE', 'type': 'BAM', 'internal': False},
+               'raw-bam-file': {'name': 'RAW BAM FILE', 'type': 'BAM', 'internal': False},
+               'locus-fasta': {'name': 'LOCUS', 'type': 'FASTA', 'internal': False},
+               'structure-db': {'name': 'STRUCTURE', 'type': 'DB', 'internal': True},
+               'single-profile-db': {'name': 'SINGLE PROFILE', 'type': 'DB', 'internal': True},
+               'profile-db': {'name': 'PROFILE', 'type': 'DB', 'internal': True},
+               'genomes-storage-db': {'name': 'GENOMES STORAGE', 'type': 'DB', 'internal': True},
+               'contigs-stats': {'name': 'CONTIGS STATS', 'type': 'STATS', 'internal': False},
+               'svg': {'name': 'SVG', 'type': 'SVG', 'internal': False},
+               'bin': {'name': 'BIN', 'type': 'BIN', 'internal': True},
+               'collection': {'name': 'COLLECTION', 'type': 'COLLECTION', 'internal': True},
+               'collection-txt': {'name': 'COLLECTION', 'type': 'TXT', 'internal': False},
+               'hmm-source': {'name': 'HMM SOURCE', 'type': 'HMM', 'internal': False},
+               'hmm-profile': {'name': 'HMM PROFILE', 'type': 'CONCEPT', 'internal': True},
+               'cogs-data': {'name': 'COGs DATA', 'type': 'DATA', 'internal': True},
+               'pfams-data': {'name': 'PFAMs DATA', 'type': 'DATA', 'internal': True},
+               'misc-data-items-txt': {'name': 'ITEMS DATA', 'type': 'TXT', 'internal': False},
+               'misc-data-items': {'name': 'ITEMS DATA', 'type': 'CONCEPT', 'internal': False},
+               'misc-data-layers-txt': {'name': 'LAYERS DATA', 'type': 'TXT', 'internal': False},
+               'misc-data-layers': {'name': 'LAYERS DATA', 'type': 'CONCEPT', 'internal': False},
+               'misc-data-layers-category': {'name': 'LAYERS DATA CATEGORY', 'type': 'CONCEPT', 'internal': True},
+               'misc-data-layer-orders': {'name': 'LAYER ORDERS DATA', 'type': 'CONCEPT', 'internal': False},
+               'misc-data-layer-orders-txt': {'name': 'LAYER ORDERS DATA', 'type': 'TXT', 'internal': False},
+               'dendrogram': {'name': 'DENDROGRAM', 'type': 'NEWICK', 'internal': False},
+               'metapangenome': {'name': 'METAPANGENOME', 'type': 'CONCEPT', 'internal': True},
+               'oligotypes': {'name': 'OLIGOTYPES', 'type': 'CONCEPT', 'internal': False},
+               'linkmers-txt': {'name': 'LINKMERS', 'type': 'TXT', 'internal': False},
+               'phylogeny': {'name': 'PHYLOGENY', 'type': 'NEWICK', 'internal': False},
+               'gene-calls-txt': {'name': 'GENE CALLS', 'type': 'TXT', 'internal': False},
+               'functions': {'name': 'GENE FUNCTIONS', 'type': 'CONCEPT', 'internal': True},
+               'functions-txt': {'name': 'GENE FUNCTIONS', 'type': 'TXT', 'internal': False},
+               'functional-enrichment-txt': {'name': 'ENRICHED FUNCTIONS', 'type': 'TXT', 'internal': False},
+               'interactive': {'name': 'INTERACTIVE DISPLAY', 'type': 'DISPLAY', 'internal': True},
+               'view-data': {'name': 'VIEW DATA', 'type': 'TXT', 'internal': False},
+               'layer-taxonomy': {'name': 'LAYER TAXONOMY', 'type': 'CONCEPT', 'internal': True},
+               'layer-taxonomy-txt': {'name': 'LAYER TAXONOMY', 'type': 'TXT', 'internal': False},
+               'gene-taxonomy': {'name': 'GENE TAXONOMY', 'type': 'CONCEPT', 'internal': True},
+               'gene-taxonomy-txt': {'name': 'GENE TAXONOMY', 'type': 'TXT', 'internal': False},
+               'external-genomes': {'name': 'EXTERNAL GENOMES', 'type': 'TXT', 'internal': False},
+               'internal-genomes': {'name': 'INTERNAL GENOMES', 'type': 'TXT', 'internal': False},
+               'ani-txt': {'name': 'ANI ESTIMATES', 'type': 'TXT', 'internal': False},
+               'ani': {'name': 'ANI ESTIMATES', 'type': 'CONCEPT', 'internal': True},
+               'variability-profile': {'name': 'VARIABILITY PROFILE', 'type': 'CONCEPT', 'internal': False},
+               'codon-frequencies-txt': {'name': 'CODON FREQUENCIES', 'type': 'TXT', 'internal': False},
+               'aa-frequencies-txt': {'name': 'AA FREQUENCIES', 'type': 'TXT', 'internal': False},
+               'summary': {'name': 'STATIC SUMMARY', 'type': 'SUMMARY', 'internal': False},
+               'split-bins': {'name': 'SPLIT BINS', 'type': 'CONCEPT', 'internal': False},
+               'state': {'name': 'INTERACTIVE STATE', 'type': 'CONCEPT', 'internal': True},
+               'state-json': {'name': 'INTERACTIVE STATE', 'type': 'JSON', 'internal': False}}
+
+ANVIO_CONCEPTS = {'functions': {'goes_in': ['contigs_db', 'genomes-storage-db'],
+                               'used_by': ['anvi-search-functions']}
+                 }
+
 run = terminal.Run()
 progress = terminal.Progress()
 
