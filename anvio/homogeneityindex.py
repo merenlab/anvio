ccdb6f6dfdd524ddfaf92722aeb25d6399e555fb
mahmoudyousef98 <mahmoudyousef@uchicago.edu>
2018-09-12 23:32:09 -0500

    Corrected flaw with geometric index

--- anvio/homogeneityindex.py
+++ anvio/homogeneityindex.py
@@ -7,7 +7,7 @@ It is divided into functional and geometric homogeneity.
 Author: Mahmoud Yousef
 """
 
-conserved_groups = {
+conserved_amino_acid_groups = {
     'Nonpolar': ['L','V','I','M','C','H','A'],
     'Aromatic': ['F','W','Y'],
     'Bases': ['K','R','H'],
@@ -21,40 +21,39 @@ conserved_groups = {
     'None': []
 }
 
-residue_id = {}
+amino_acid_property_group = {}
 for key in ['A','I','L','V','M','C']:
-    residue_id[key] = 'Nonpolar'
+    amino_acid_property_group[key] = 'Nonpolar'
 for key in ['F','W']:
-    residue_id[key] = 'Aromatic'
+    amino_acid_property_group[key] = 'Aromatic'
 for key in ['K','R']:
-    residue_id[key] = 'Bases'
+    amino_acid_property_group[key] = 'Bases'
 for key in ['Q', 'N']:
-    residue_id[key] = 'Neutral Amines'
+    amino_acid_property_group[key] = 'Neutral Amines'
 for key in ['D','E']:
-    residue_id[key] = 'Acids'
+    amino_acid_property_group[key] = 'Acids'
 for key in ['H','Y']:
-    residue_id[key] = 'Polar and Nonpolar'
+    amino_acid_property_group[key] = 'Polar and Nonpolar'
 for key in ['S','T']:
-    residue_id[key] = 'Mostly nonpolar'
-for key in ['G','P']:
-    residue_id[key] = 'None'
-for key in ['X']:
-    residue_id[key] = 'None'
-residue_id['B'] = 'B'
-residue_id['Z'] = 'Z'
-residue_id['J'] = 'J'
+    amino_acid_property_group[key] = 'Mostly nonpolar'
+for key in ['G','P','X']:
+    amino_acid_property_group[key] = 'None'
+amino_acid_property_group['B'] = 'B'
+amino_acid_property_group['Z'] = 'Z'
+amino_acid_property_group['J'] = 'J'
 
 
-def is_conserved(r1, r2):
-    group = residue_id[r1]
-    conserved_group = conserved_groups[group]
+def is_amino_acid_functionally_conserved(amino_acid_residue_1, amino_acid_residue_2):
+    group = amino_acid_property_group[amino_acid_residue_1]
+    conserved_group = conserved_amino_acid_groups[group]
 
-    if r2 in conserved_group:
+    if amino_acid_residue_2 in conserved_group:
         return True
     if group == 'Polar and Nonpolar': #they fall in more than one group, multiple tests needed
-        if r1 == 'H' and (r2 in conserved_groups['Nonpolar'] or r2 in conserved_groups['Bases']):
+        if amino_acid_residue_1 == 'H' and (amino_acid_residue_2 in conserved_amino_acid_groups['Nonpolar'] \
+                                            or amino_acid_residue_2 in conserved_amino_acid_groups['Bases']):
             return True
-        if r1 == 'Y' and (r2 in conserved_groups['Aromatic']):
+        if amino_acid_residue_1 == 'Y' and (amino_acid_residue_2 in conserved_amino_acid_groups['Aromatic']):
             return True
     return False
 
@@ -80,80 +79,79 @@ class HomogeneityCalculator(object):
         elif num_sequences == 0: #this is an error condition
             return 0
         
-        length = len(gene_cluster_sequences[0])
-        similarity = 0
-        max = 0
-        for spot in range(0, length):
+        num_residues = len(gene_cluster_sequences[0])
+        similarity_score = 0
+        max_score = 0
+        for residue_number in range(0, num_residues):
             residues = []
-            for sequence in range(0, num_sequences):
-                residues.append(gene_cluster_sequences[sequence][spot])
-            for a in range(0,len(residues)):
-                for b in range(0,len(residues)):
-                    if a >= b:
-                        continue
-                    r1 = residues[a]
-                    r2 = residues[b]
-                    if r1 == "-":
-                        r1 = residues[b]
-                        r2 = "-"
-                    max += 3
-                    if r1 == r2 and (r1 != 'X' and r1 != 'J' and r1 != 'B' and r1 != 'Z'):
-                        similarity += 3
-                    elif is_conserved(r1,r2):
-                        similarity += 2
-                    elif r2 != "-":
-                        similarity += 1
+            for gene_sequence in range(0, num_sequences):
+                residues.append(gene_cluster_sequences[gene_sequence][residue_number])
+            for spot_a in range(0,len(residues)):
+                for spot_b in range(spot_a+1,len(residues)):
+                    amino_acid_residue_1 = residues[spot_a]
+                    amino_acid_residue_2 = residues[spot_b]
+                    if amino_acid_residue_1 == "-":
+                        amino_acid_residue_1 = residues[spot_b]
+                        amino_acid_residue_2 = "-"
+                    max_score += 3
+                    if amino_acid_residue_1 == amino_acid_residue_2 and (amino_acid_residue_1 != 'X' and amino_acid_residue_1 != 'J' \
+                                                                        and amino_acid_residue_1 != 'B' and amino_acid_residue_1 != 'Z'):
+                        similarity_score += 3
+                    elif is_amino_acid_functionally_conserved(amino_acid_residue_1,amino_acid_residue_2):
+                        similarity_score += 2
+                    elif amino_acid_residue_2 != "-":
+                        similarity_score += 1
                     else:
-                        similarity += 0
-        index = (similarity / max) * 100
-        return index
+                        similarity_score += 0
+        functional_index = (similarity_score / max_score) * 100
+        return functional_index
 
 
-    def label_gaps(self, gene_sequences, bygene = False): #1 indicates gaps
-        if bygene == False:
-            array = [0] * len(gene_sequences[0])
-            for i in range(len(gene_sequences)):
-                for j in range(len(gene_sequences[0])):
-                    if gene_sequences[i][j] == "-":
-                        array[j] = ((array[j]) << 1) + 1
+    def convert_sequences_to_binary_array(self, gene_sequences, bygene = False): #1 indicates gaps
+        num_genes = len(gene_sequences)
+        num_residues = len(gene_sequences[0])
+        if not bygene: #array will be ordered by residue column
+            array = [0] * num_residues
+            for gene in range(num_genes):
+                for residue in range(num_residues):
+                    if gene_sequences[gene][residue] == "-":
+                        array[residue] = ((array[residue]) << 1) + 1
                     else:
-                        array[j] = ((array[j]) << 1) + 0
-        else: #array will be ordered by gene, rather than by column
-            array = [0] * len(gene_sequences)
-            for i in range(len(gene_sequences[0])):
-                for j in range(len(gene_sequences)):
-                    if gene_sequences[j][i] == "-":
-                        array[j] = ((array[j]) << 1) + 1
+                        array[residue] = ((array[residue]) << 1) + 0
+        else: #array will be ordered by gene, rather than by residue column
+            array = [0] * num_genes
+            for residue in range(num_residues):
+                for gene in range(num_genes):
+                    if gene_sequences[gene][residue] == "-":
+                        array[gene] = ((array[gene]) << 1) + 1
                     else:
-                        array[j] = ((array[j]) << 1) + 0
+                        array[gene] = ((array[gene]) << 1) + 0
 
         return array
-        #now we have an array of binary numbers that represent the gap-residue pattern of each column
+        #now we have an array of binary numbers that represent the gap-residue pattern of the entire gene cluster
 
 
-    @profile
     def compute_geometric_index(self, gene_cluster_sequences, quick_homogeneity=False): 
         num_genes = len(gene_cluster_sequences)
         if num_genes == 1:
             return 100
         
-        grid = self.label_gaps(gene_cluster_sequences)
-        length = len(grid)
+        binary_matrix_by_residue = self.convert_sequences_to_binary_array(gene_cluster_sequences)
+        num_residues = len(binary_matrix_by_residue)
         residue_uniformity = []
 
-        for col in range(length):
+        for col in range(num_residues):
             differences = []
-            for counter in range(length):
+            for counter in range(num_residues):
                 if col == counter:
                     continue
-                diff = grid[col] ^ grid[counter]
-                runsum = len([ones for ones in bin(diff).zfill(num_genes)[-num_genes:] if ones=='0'])
-                #runsum = 0
-                #for i in range(num_genes):
-                #    if abs(diff) % 2 == 0:
-                #        runsum += 1
-                #    diff = diff >> 1
-                differences.append(runsum / num_genes)
+                diff = bin(binary_matrix_by_residue[col] ^ binary_matrix_by_residue[counter])[2:].zfill(num_genes)
+                number_of_similarities = len([similarities for similarities in diff[-num_genes:]\
+                                             if similarities=='0'])
+                #Let's explain what happened - we converted diff into a binary number. .zfill ensured that we have the sufficient number of digits.
+                #This line of code creates an array - similariies - containing all of the digits that are 0 (equal to each other in the ^ expresson)
+                #number_of_similarities is the length of that array - effectively, the number of 0s in the binary number
+                differences.append(number_of_similarities / num_genes)
             residue_uniformity.append(sum(differences) / len(differences))
         
         by_residue = 100 * (sum(residue_uniformity) / len(residue_uniformity))
@@ -161,29 +159,24 @@ class HomogeneityCalculator(object):
         if quick_homogeneity:
             return by_residue
 
-        grid2 = self.label_gaps(gene_cluster_sequences, bygene=True)
-        length = len(grid2)
+        binary_matrix_by_gene = self.convert_sequences_to_binary_array(gene_cluster_sequences, bygene=True)
         gene_uniformity = []
 
-        for gene in range(length):
+        for gene in range(num_genes):
             differences = []
-            for counter in range(length):
+            for counter in range(num_genes):
                 if gene == counter:
                     continue
-                diff = grid2[gene] ^ grid2[counter]
-                runsum = len([ones for ones in bin(diff).zfill(len(grid))[-len(grid):] if ones=='0'])
-                #runsum = 0
-                #for i in range(len(gene_cluster_sequences[0])):
-                #    if abs(diff) % 2 == 0:
-                #        runsum += 1
-                #    diff = diff >> 1
-                differences.append(runsum / len(grid))
+                diff = bin(binary_matrix_by_gene[gene] ^ binary_matrix_by_gene[counter])[2:].zfill(num_residues)
+                number_of_similarities = len([similarities for similarities in diff[-num_residues:]\
+                             if similarities=='0'])
+                differences.append(number_of_similarities / num_residues)
             gene_uniformity.append(sum(differences) / len(differences))
 
         by_gene = 100 * (sum(gene_uniformity) / len(gene_uniformity))
 
-        index = (by_residue + by_gene) / 2
-        return index
+        geometric_index = (by_residue + by_gene) / 2
+        return geometric_index
 
 
     def get_homogeneity_dicts(self):
@@ -200,6 +193,5 @@ class HomogeneityCalculator(object):
 
             self.functional[gene_cluster] = self.compute_functional_index(cluster_sequences)
             self.geometric[gene_cluster] = self.compute_geometric_index(cluster_sequences, self.quick_homogeneity)
-            #self.cluster_sizes[gene_cluster] = len(cluster_sequences)
         
         return self.functional, self.geometric
