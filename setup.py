4101fe3b1b1ffa7d800599e4925cce8319350b0a
A. Murat Eren <a.murat.eren@gmail.com>
2018-09-21 13:06:07 -0500

    v5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 import glob
 
-anvio_version='5.1-master'
+anvio_version='5.2'
 
 requirements = [req.strip() for req in open('requirements.txt', 'rU').readlines() if not req.startswith('#')]
 
