b957d4935c0bc3b25aa17c9c63c494bba3e1ba69
ShaiberAlon <alon.shaiber@gmail.com>
2018-11-19 15:49:39 -0600

    phylogenomics workflow inherits contigs workflow for creating external genomes.
    test now includes testing external genomes.

--- anvio/tests/run_phylogenomics_workflow_tests.sh
+++ anvio/tests/run_phylogenomics_workflow_tests.sh
@@ -7,26 +7,23 @@ SETUP_WITH_OUTPUT_DIR $1
 
 INFO "Setting up the pan analysis directory"
 mkdir $output_dir/workflow_test
-cp $files/mock_data_for_pangenomics/*.fa $output_dir/workflow_test/
-cp $files/mock_data_for_pangenomics/default-state.json        $output_dir/workflow_test/
-cp $files/workflows/pangenomics/fasta.txt $output_dir/workflow_test/
-cp $files/workflows/pangenomics/config.json $output_dir/workflow_test/
+cp $files/workflows/phylogenomics/* $output_dir/workflow_test/
 cd $output_dir/workflow_test
 
 INFO "Creating a default config for contigs workflow"
-anvi-run-workflow -w contigs --get-default-config default-config.json
+anvi-run-workflow -w phylogenomics --get-default-config default-config.json
 
-INFO "Listing dependencies for contigs workflow"
-anvi-run-workflow -w contigs -c default-config.json --list-dependencies
+INFO "Listing dependencies for phylogenomics workflow"
+anvi-run-workflow -w phylogenomics -c config.json --list-dependencies
 
 INFO "Saving a workflow graph"
-anvi-run-workflow -w contigs -c default-config.json --save-workflow-graph
+anvi-run-workflow -w phylogenomics -c config.json --save-workflow-graph
 
-INFO "Running contigs workflow with a dry-run"
-anvi-run-workflow -w contigs -c default-config.json --dry-run
+INFO "Running phylogenomics workflow with a dry-run"
+anvi-run-workflow -w phylogenomics -c config.json --dry-run
 
-INFO "Running contigs workflow"
-anvi-run-workflow -w contigs -c default-config.json
+INFO "Running phylogenomics workflow"
+anvi-run-workflow -w phylogenomics -c config.json
 
-INFO "Examine contigs databases with anvi-display-contigs-stats"
-anvi-display-contigs-stats 02_CONTIGS/*db
+INFO "Display phylogenomic tree"
+anvi-interactive -t 01_PHYLOGENOMICS/TEST-proteins_GAPS_REMOVED.fa.contree -p manual.db --manual
