6b7dc708ccde5550aae552bf9b734b049e8832d3
Ozcan Esen <ozcanesen@gmail.com>
2018-11-30 13:38:12 -0600

    fix python being weird parsing strings, Related #1050

--- anvio/tables/miscdata.py
+++ anvio/tables/miscdata.py
@@ -620,8 +620,10 @@ class AdditionalDataBaseClass(AdditionalAndOrderDataBaseClass, object):
             key = entry['data_key']
             value = entry['data_value']
 
-            if entry['data_type'] in ['int', 'float']:
-                d[additional_data_item_name][key] = eval(entry['data_type'])(value or self.nulls_per_type[entry['data_type']])
+            if entry['data_type'] == 'int':
+                d[additional_data_item_name][key] = int(float(value or self.nulls_per_type[entry['data_type']]))
+            elif entry['data_type'] == 'float':
+                d[additional_data_item_name][key] = float(value or self.nulls_per_type[entry['data_type']])
             else:
                 d[additional_data_item_name][key] = value
 
