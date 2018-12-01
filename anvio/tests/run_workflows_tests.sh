b2cc544ad8a656be5e202bb848dd2d27b3630b85
ShaiberAlon <alon.shaiber@gmail.com>
2018-11-30 23:35:36 -0600

    fix unintended commit

--- anvio/tests/run_workflows_tests.sh
+++ anvio/tests/run_workflows_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-#./run_contigs_workflow_tests.sh
+./run_contigs_workflow_tests.sh
 ./run_phylogenomics_workflow_tests.sh
 ./run_pangenomics_workflow_tests.sh
 ./run_metagenomics_workflow_tests.sh
