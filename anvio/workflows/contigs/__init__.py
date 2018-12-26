08092144492f0ec2557146f273bc978457788411
ShaiberAlon <alon.shaiber@gmail.com>
2018-12-26 15:10:45 -0600

    If the user supplied external gene calls then we ignore the --prodigal-translation-table parameter
    That way we can have a mix of genomes with and without external gene calls and we will treat these accordingly.

--- anvio/workflows/contigs/__init__.py
+++ anvio/workflows/contigs/__init__.py
@@ -199,6 +199,15 @@ class ContigsDBWorkflow(WorkflowSuperClass):
             return ""
 
 
+    def get_prodigal_translation_table_param(self, wildcards):
+        external_gene_calls_param = self.get_external_gene_calls_param(wildcards)
+        if external_gene_calls_param:
+            # If the user supplied external gene calls then we ignore the --prodigal-translation-table parameter
+            return ''
+        else:
+            return M.get_rule_param("anvi_gen_contigs_database", "--prodigal-translation-table")
+
+
     def get_fasta(self, wildcards):
         '''
             Define the path to the input fasta files.
