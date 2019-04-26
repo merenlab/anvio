3a80300bc08243169e96e935883b2180f1e5437d
Ozcan Esen <ozcanesen@gmail.com>
2019-04-26 11:00:54 -0500

    remove concoct from mini test

--- anvio/tests/run_mini_test.sh
+++ anvio/tests/run_mini_test.sh
@@ -72,5 +72,3 @@ anvi-interactive -p $output_dir/SAMPLES-MERGED/PROFILE.db \
                  -c $output_dir/CONTIGS.db \
                  --split-hmm-layers
 
-INFO "Summarizing CONCOCT results"
-anvi-summarize -p $output_dir/SAMPLES-MERGED/PROFILE.db -c $output_dir/CONTIGS.db -o $output_dir/SAMPLES-MERGED-SUMMARY -C 'CONCOCT' --init-gene-coverages
