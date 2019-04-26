38f541204ba126c6192a324b194810a880df6723
Ozcan Esen <ozcanesen@gmail.com>
2019-04-26 10:21:44 -0500

    clear setup.py and requirements

--- setup.py
+++ setup.py
@@ -21,12 +21,6 @@ except ImportError:
     print("You need to have numpy installed on your system to run setup.py. Sorry!")
     sys.exit()
 
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    print("You need to have Cython installed on your system to run setup.py. Sorry!")
-    sys.exit()
-
 from setuptools import setup, find_packages, Extension
 
 if os.environ.get('USER','') == 'vagrant':
@@ -37,8 +31,6 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-include_dirs_for_concoct = [numpy.get_include(), '/opt/local/include/', '/usr/local/include/']
-library_dirs_for_concoct = ['/opt/local/lib/', '/usr/local/lib/']
 
 setup(
     name = "anvio",
@@ -50,13 +42,6 @@ setup(
     packages = find_packages(),
 
     install_requires = requirements,
-
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [
-                    Extension("anvio.vbgmm", sources=["./anvio/extensions/concoct/vbgmm.pyx", "./anvio/extensions/concoct/c_vbgmm_fit.c"],
-                                libraries =['gsl', 'gslcblas'], include_dirs=include_dirs_for_concoct, library_dirs=library_dirs_for_concoct),
-                  ],
-
     author = "anvi'o Authors",
     author_email = "a.murat.eren@gmail.com",
     description = "An interactive analysis and visualization platform for 'omics data. See https://merenlab.org/projects/anvio for more information",
