261cca9e8c9fe95bbc0863b57089b4769ebd7abb
A. Murat Eren <a.murat.eren@gmail.com>
2019-05-01 10:18:52 -0500

    bump.

--- anvio/__init__.py
+++ anvio/__init__.py
@@ -11,7 +11,7 @@ import platform
 # unless you want to explode `bottle`:
 import pkg_resources
 
-anvio_version = '5.5'
+anvio_version = '5.5-master'
 anvio_codename = 'margaret'
 
 DEBUG = '--debug' in sys.argv
