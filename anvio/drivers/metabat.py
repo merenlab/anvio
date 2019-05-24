a2b25ada8dffd1fa2162ada1b192159be348e3e1
Ozcan Esen <ozcanesen@gmail.com>
2019-05-24 14:39:42 -0500

    metabat driver

--- anvio/drivers/metabat.py
+++ anvio/drivers/metabat.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 """Interface to MetaBAT."""
-
+import os
+import glob
 from subprocess import Popen, PIPE
 
 import anvio
@@ -25,6 +26,7 @@ pp = terminal.pretty_print
 
 class MetaBAT:
     arguments = {
+        "infile"
         'seed': (
                 ['--seed'],
                 {'metavar': "INT",
@@ -38,26 +40,53 @@ class MetaBAT:
                  'help': "Number of threads"}
                     ),
     }
+    citation = "asdads"
 
-    def __init__(self, run=run):
+    def __init__(self, profile_db=None, contigs_db=None, run=run, progress=progress):
         self.run = run
         self.progress = progress
+        self.program_name = 'metabat2'
 
-        utils.is_program_exists('metabat2')
+        self.profile_db = profile_db
+        self.contigs_db = contigs_db
+
+        utils.is_program_exists(self.program_name)
 
         self.temp_path = filesnpaths.get_temp_directory_path()
 
 
-    def get_parser(self):
-        pass
+    def cluster(self, input_files, args):
+        input_files.coverage
 
+        self.run.info('Working directory', self.temp_path)
 
-    def run_command(self, args):
-        utils.is_profile_db_and_contigs_db_compatible(args.profile_db, args.contigs_db)
+        P = lambda x: os.path.join(self.temp_path, x)
+        coverage_path = P('coverages.txt')
+        fasta_path = P('sequence.fa')
+        log_path = P('logs.txt')
+        bin_prefix = P('Bin')
 
-        merged_profile_db = dbops.ProfileDatabase(args.profile_db)
+        coverages = self.profile_db.db.get_table_as_dict('mean_coverage_contigs')
 
-        if(merged_profile_db.meta['merged'] != True):
-            raise ConfigError("'%s' does not seem to be a merged profile database :/" % args.profile_db)
+        utils.export_sequences_from_contigs_db(self.contigs_db.db_path, fasta_path, seq_names_to_export=sorted(coverages.keys()), splits_mode=True)
+        utils.store_dict_as_TAB_delimited_file(coverages, coverage_path, ['contig'] + sorted(list(self.profile_db.samples)))
 
+        cmd_line = [self.program_name, '-i', fasta_path, '-a', coverage_path, '-o', bin_prefix ,'--cvExt', '-l', '--verbose', '--debug']
+
+        self.progress.new(self.program_name)
+        self.progress.update('Running...')
+        utils.run_command(cmd_line, log_path)
+        self.progress.end()
+
+        clusters = {}
+        bin_count = 0
+        for bin_file in glob.glob(bin_prefix + '*'):
+            bin_count += 1
+            with open(bin_file, 'r') as f:
+                bin_name = os.path.basename(bin_file).replace('.', '_')
+                clusters[bin_name] = list(map(str.strip, f.readlines()))
+
+        self.run.info('Bins formed', bin_count)
+
+        return clusters
         
