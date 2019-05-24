5cf32ddba665e497b8b4f89dfac653cfcb21c82b
Ozcan Esen <ozcanesen@gmail.com>
2019-05-24 16:35:40 -0500

    move file operations out of binning modules, provide input_files namespace

--- anvio/drivers/metabat.py
+++ anvio/drivers/metabat.py
@@ -2,7 +2,7 @@
 """Interface to MetaBAT."""
 import os
 import glob
-from subprocess import Popen, PIPE
+import shutil
 
 import anvio
 import anvio.utils as utils
@@ -40,38 +40,30 @@ class MetaBAT:
                  'help': "Number of threads"}
                     ),
     }
-    citation = "asdads"
+    citation = "Citation here"
 
-    def __init__(self, profile_db=None, contigs_db=None, run=run, progress=progress):
+    def __init__(self, run=run, progress=progress):
         self.run = run
         self.progress = progress
         self.program_name = 'metabat2'
 
-        self.profile_db = profile_db
-        self.contigs_db = contigs_db
-
         utils.is_program_exists(self.program_name)
-
         self.temp_path = filesnpaths.get_temp_directory_path()
 
 
-    def cluster(self, input_files, args):
-        input_files.coverage
+    def cluster(self, input_files, args, threads=1):
+        if anvio.DEBUG:
+            self.run.info('Working directory', self.temp_path)
 
-        self.run.info('Working directory', self.temp_path)
+        bin_prefix = os.path.join(self.temp_path, 'Bin')
+        log_path = os.path.join(self.temp_path, 'logs.txt')
 
-        P = lambda x: os.path.join(self.temp_path, x)
-        coverage_path = P('coverages.txt')
-        fasta_path = P('sequence.fa')
-        log_path = P('logs.txt')
-        bin_prefix = P('Bin')
-
-        coverages = self.profile_db.db.get_table_as_dict('mean_coverage_contigs')
-
-        utils.export_sequences_from_contigs_db(self.contigs_db.db_path, fasta_path, seq_names_to_export=sorted(coverages.keys()), splits_mode=True)
-        utils.store_dict_as_TAB_delimited_file(coverages, coverage_path, ['contig'] + sorted(list(self.profile_db.samples)))
-
-        cmd_line = [self.program_name, '-i', fasta_path, '-a', coverage_path, '-o', bin_prefix ,'--cvExt', '-l', '--verbose', '--debug']
+        cmd_line = [self.program_name,
+            '-i', input_files.fasta, 
+            '-a', input_files.coverage, 
+            '-o', bin_prefix,
+            '--cvExt',
+            '-l']
 
         self.progress.new(self.program_name)
         self.progress.update('Running...')
@@ -83,10 +75,13 @@ class MetaBAT:
         for bin_file in glob.glob(bin_prefix + '*'):
             bin_count += 1
             with open(bin_file, 'r') as f:
-                bin_name = os.path.basename(bin_file).replace('.', '_')
-                clusters[bin_name] = list(map(str.strip, f.readlines()))
+                pretty_bin_name = os.path.basename(bin_file).replace('.', '_')
+                clusters[pretty_bin_name] = list(map(str.strip, f.readlines()))
 
         self.run.info('Bins formed', bin_count)
+
+        if not anvio.DEBUG:
+            shutil.rmtree(self.temp_path)
 
         return clusters
         
