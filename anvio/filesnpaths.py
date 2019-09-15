2ef0a126600e42c748a6b7b64aa512a87371f542
A. Murat Eren <a.murat.eren@gmail.com>
2019-09-15 17:17:53 -0500

    check if directory

--- anvio/filesnpaths.py
+++ anvio/filesnpaths.py
@@ -73,6 +73,9 @@ def is_file_exists(file_path, dont_raise=False):
 def is_output_file_writable(file_path, ok_if_exists=True):
     if not file_path:
         raise FilesNPathsError("No output file is declared...")
+    if os.path.isdir(file_path):
+        raise FilesNPathsError("The path you have provided for your output file ('%s') already is used .. by\
+                                a directory :/" % (os.path.abspath(file_path)))
     if not os.access(os.path.dirname(os.path.abspath(file_path)), os.W_OK):
         raise FilesNPathsError("You do not have permission to generate the output file '%s'" % file_path)
     if os.path.exists(file_path) and not ok_if_exists:
