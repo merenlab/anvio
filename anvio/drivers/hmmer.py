cdc63e7e8961cefcab34cafa3eb5ed7d2709749e
A. Murat Eren <a.murat.eren@gmail.com>
2019-09-22 17:35:34 -0400

    clarity += 1

--- anvio/drivers/hmmer.py
+++ anvio/drivers/hmmer.py
@@ -168,10 +168,9 @@ class HMMer:
 
         if not os.path.exists(shitty_output_file):
             self.progress.end()
-            raise ConfigError("Something went wrong with hmmscan, and it failed to generate the\
-                                expected output :/ Fortunately, this log file should tell you what\
-                                might be the problem: '%s'. Please do not forget to include this\
-                                file if you were to ask for help." % log_file)
+            raise ConfigError("Something went wrong with hmmscan and it failed to generate the expected output :/ Fortunately\
+                               we have this log file which should clarify the problem: '%s'. Please do not forget to include this\
+                               file in your question if you were to seek help from the community." % log_file)
 
         detected_non_ascii = False
         lines_with_non_ascii = []
