83c49afc62011218814e610252bb76b1d7d37216
Ozcan Esen <ozcanesen@gmail.com>
2019-10-25 11:15:50 +0200

    oops

--- anvio/drivers/binsanity.py
+++ anvio/drivers/binsanity.py
@@ -69,7 +69,7 @@ class BinSanity:
                 clustering of environmental microbial assemblies using coverage and \
                 affinity propagation. PeerJ 5:e3035 https://doi.org/10.7717/peerj.3035"
 
-    cluster_type = 'contigsss'
+    cluster_type = 'contig'
 
     def __init__(self, run=run, progress=progress):
         self.run = run
