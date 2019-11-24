cf47173041b2c2c7f81604ac3d5cf0055a236cde
A. Murat Eren <a.murat.eren@gmail.com>
2019-11-24 17:48:36 -0600

    permission issues be clearer

--- anvio/filesnpaths.py
+++ anvio/filesnpaths.py
@@ -78,6 +78,8 @@ def is_output_file_writable(file_path, ok_if_exists=True):
                                 a directory :/" % (os.path.abspath(file_path)))
     if not os.access(os.path.dirname(os.path.abspath(file_path)), os.W_OK):
         raise FilesNPathsError("You do not have permission to generate the output file '%s'" % file_path)
+    if os.path.exists(file_path) and not os.access(file_path, os.W_OK):
+        raise FilesNPathsError("You do not have permission to update the contents of the file '%s' :/" % file_path)
     if os.path.exists(file_path) and not ok_if_exists:
         raise FilesNPathsError("The file, '%s', already exists. anvio does not like overwriting stuff." % file_path)
     return True
