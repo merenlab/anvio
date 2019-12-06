e44d56a9ace0efb10a30521e1f3d5f5a4200883a
Evan Kiefl <kiefl.evan@gmail.com>
2019-12-06 11:15:47 -0600

    revert

--- anvio/variabilityops.py
+++ anvio/variabilityops.py
@@ -823,8 +823,7 @@ class VariabilitySuper(VariabilityFilter, object):
             try:
                 splits_of_interest = list(set([self.gene_callers_id_to_split_name_dict[g] for g in (self.genes_of_interest or self.gene_caller_ids)]))
             except KeyError as e:
-                not_present_in = 'variability table' if self.table_provided else 'contigs database'
-                raise ConfigError("Some of the gene caller IDs you provided are not in your %s..." % not_present_in)
+                raise ConfigError("Some of the gene caller IDs you provided are not in your contigs database...")
 
         elif split_source == "bin_id":
             if self.collection_name and not self.bin_id:
