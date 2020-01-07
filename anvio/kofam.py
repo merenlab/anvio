e80ca45b6fbfd852eee462a8d4900e4e37b0604f
Iva Veseli <iva.veseli@gmail.com>
2020-01-07 15:53:16 -0600

    initial kofam.py with imports based on pfam.py

---
+++ anvio/kofam.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+"""
+    This file contains KofamSetup and Kofam classes.
+
+"""
+
+import os
+import gzip
+import shutil
+import requests
+
+import anvio
+import anvio.dbops as dbops
+import anvio.utils as utils
+import anvio.terminal as terminal
+import anvio.filesnpaths as filesnpaths
+
+__author__ = "Developers of anvi'o (see AUTHORS.txt)"
+__copyright__ = "Copyleft 2015-2020, the Meren Lab (http://merenlab.org/)"
+__license__ = "GPL 3.0"
+__version__ = anvio.__version__
+__maintainer__ = "Özcan Esen"
+__email__ = "ozcanesen@gmail.com"
+
+run = terminal.Run()
+progress = terminal.Progress()
+pp = terminal.pretty_print
