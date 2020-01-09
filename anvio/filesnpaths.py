897680f4cc43f518132433fd3c0c025a65a82d3d
A. Murat Eren <a.murat.eren@gmail.com>
2020-01-08 21:01:25 -0600

    optional parameter to not 'touch' the file

--- anvio/filesnpaths.py
+++ anvio/filesnpaths.py
@@ -210,10 +210,14 @@ def get_temp_directory_path():
     return tempfile.mkdtemp()
 
 
-def get_temp_file_path(prefix=None):
+def get_temp_file_path(prefix=None, just_the_path=True):
     f = tempfile.NamedTemporaryFile(delete=False, prefix=prefix)
     temp_file_name = f.name
     f.close()
+
+    if just_the_path:
+        os.remove(temp_file_name)
+
     return temp_file_name
 
 
