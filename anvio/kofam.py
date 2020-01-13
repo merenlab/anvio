11809d45ceb0005a572f836e34de36417b7caf25
Iva Veseli <iva.veseli@gmail.com>
2020-01-13 09:41:07 -0600

    download function only downloads now

--- anvio/kofam.py
+++ anvio/kofam.py
@@ -75,8 +75,6 @@ class KofamSetup(object):
             utils.download_file(self.database_url + '/' + file_name,
                 os.path.join(self.kofam_data_dir, file_name), progress=self.progress, run=self.run)
 
-        self.decompress_files()
-
 
     def decompress_files(self):
         """This function decompresses the Kofam profiles."""
@@ -87,6 +85,3 @@ class KofamSetup(object):
                 utils.tar_extract_file(full_path, output_file_path = self.kofam_data_dir, keep_original=False)
             else:
                 utils.gzip_decompress_file(full_path, keep_original=False)
-
-             utils.gzip_decompress_file(full_path)
-             os.remove(full_path)
