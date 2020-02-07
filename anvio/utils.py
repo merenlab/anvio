95c9c81f8ddc060ca280c0c9d718bde18fe0eacf
A. Murat Eren <a.murat.eren@gmail.com>
2020-02-06 18:48:08 -0600

    cosmetics.

--- anvio/utils.py
+++ anvio/utils.py
@@ -1563,12 +1563,16 @@ def is_amino_acid_functionally_conserved(amino_acid_residue_1, amino_acid_residu
 
     if conserved_group and amino_acid_residue_2 in conserved_group:
         return True
-    if group == 'Polar and Nonpolar': #they fall in more than one group, multiple tests needed
+
+    if group == 'Polar and Nonpolar':
+        #they fall in more than one group, multiple tests needed
         if amino_acid_residue_1 == 'H' and (amino_acid_residue_2 in constants.conserved_amino_acid_groups['Nonpolar'] \
                                             or amino_acid_residue_2 in constants.conserved_amino_acid_groups['Bases']):
             return True
+
         if amino_acid_residue_1 == 'Y' and (amino_acid_residue_2 in constants.conserved_amino_acid_groups['Aromatic']):
             return True
+
     return False
 
 
