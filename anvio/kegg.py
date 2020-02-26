5decfc7960133e2a2191e39e26cc4d38dfb5f770
Iva Veseli <iva.veseli@gmail.com>
2020-02-26 12:13:54 -0600

    fix module name in module dictionary

--- anvio/kegg.py
+++ anvio/kegg.py
@@ -251,7 +251,7 @@ class KeggSetup(KeggContext):
                 elif first_char == "D":
                     fields = re.split('\s{2,}', line)
                     mnum = fields[1]
-                    self.module_dict[mnum] = {"name" : module_name}
+                    self.module_dict[mnum] = {"name" : fields[2]}
                 # unknown code
                 else:
                     raise ConfigError("While parsing the KEGG file %s, we found an unknown line code %s. This has \
