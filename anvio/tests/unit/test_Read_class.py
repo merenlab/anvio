8061df109d37d3cbb2936e4ea066d91fbc9ec497
Evan Kiefl <kiefl.evan@gmail.com>
2020-02-26 16:32:44 -0600

    Update unittest

--- anvio/tests/unit/test_Read_class.py
+++ anvio/tests/unit/test_Read_class.py
@@ -8,7 +8,7 @@ import unittest
 
 import anvio
 
-from anvio.sequence import Read
+from anvio.bamops import Read
 from anvio.errors import ConfigError
 
 __author__ = "Developers of anvi'o (see AUTHORS.txt)"
@@ -25,45 +25,46 @@ class FakePySamAlignedSegment:
         for key, v in output.items():
             setattr(self, key, v)
 
-    def get_reference_positions(self):
-        return self.reference_positions
-
-    def get_reference_sequence(self):
-        return self.reference_sequence
-
 
 class TestRead(unittest.TestCase):
     """
     CASE #1
     =======
-    A A C C T T G G
-    A C T G T C T G A C T G = reference
-    [(0,8)]
+    <anvio.bamops.Read object at 0x12b042470>
+     ├── start, end : [0, 8)
+     ├── cigartuple : [(0, 8)]
+     ├── read       : AACCTTGG
+     └── reference  : XXXXXXXX
 
     CASE #2
     =======
-    A A C C T T G G
-    A C - - - - T G A C T G A C T G = reference
-    [(0,2), (1,4), (0,2)]
+    <anvio.bamops.Read object at 0x12b042358>
+     ├── start, end : [0, 4)
+     ├── cigartuple : [(0, 2), (1, 4), (0, 2)]
+     ├── read       : AACCTTGG
+     └── reference  : XX----XX
 
     CASE #3
     =======
-    A A C C - - T T G G
-    A - - - C T G A C T G A C T G = reference
-    [(0,1), (1,3), (2,2), (0,4)]
+    <anvio.bamops.Read object at 0x12b042390>
+     ├── start, end : [0, 7)
+     ├── cigartuple : [(0, 1), (1, 3), (2, 2), (0, 4)]
+     ├── read       : AACC--TTGG
+     └── reference  : X---XXXXXX
 
     CASE #4
     =======
-    A - A - C - C - T T G G
-    A C T G A C T G A C - T G = reference
-    [(0,1), (2,1), (0,1), (2,1), (0,1), (2,1), (0,1), (2,1), (0,2), (1,1), (0,1)]
+    <anvio.bamops.Read object at 0x12b042400>
+     ├── start, end : [0, 11)
+     ├── cigartuple : [(0, 1), (2, 1), (0, 1), (2, 1), (0, 1), (2, 1), (0, 1), (2, 1), (0, 2), (1, 1), (0, 1)]
+     ├── read       : A-A-C-C-TTGG
+     └── reference  : XXXXXXXXXX-X
     """
 
     def setUp(self):
         self.read_attrs = [
             {
                 'query_sequence' : 'AACCTTGG',
-                'reference_sequence' : 'ACTGACTG',
                 'reference_positions': np.array([0,1,2,3,4,5,6,7]),
                 'reference_start': 0,
                 'reference_end': 8,
@@ -78,7 +79,6 @@ class TestRead(unittest.TestCase):
             },
             {
                 'query_sequence' : 'AACCTTGG',
-                'reference_sequence' : 'ACTGACTG',
                 'reference_positions': np.array([0,1,2,3]),
                 'reference_start': 0,
                 'reference_end': 4,
@@ -93,7 +93,6 @@ class TestRead(unittest.TestCase):
             },
             {
                 'query_sequence' : 'AACCTTGG',
-                'reference_sequence' : 'ACTGACTG',
                 'reference_positions': [0,3,4,5,6],
                 'reference_start': 0,
                 'reference_end': 7,
@@ -108,7 +107,6 @@ class TestRead(unittest.TestCase):
             },
             {
                 'query_sequence' : 'AACCTTGG',
-                'reference_sequence' : 'ACTGACTG',
                 'reference_positions': np.array([0,2,4,6,8,9,10]),
                 'reference_start': 0,
                 'reference_end': 11,
@@ -130,28 +128,28 @@ class TestRead(unittest.TestCase):
 
 
     def test_trim(self):
-        print('=========================')
         for target in range(len(self.reads)):
-            print("Working on target %d" % target)
+            print("\n\nWorking on target %d:\n%s" % (target, self.reads[target]))
             attrs = self.read_attrs[target]
 
-            print("Trimming left")
+            print("Trimming left ...")
             read = copy.deepcopy(self.reads[target])
             read.trim(attrs['trim_by'], side='left')
+            print("After trimming left by %d:\n%s" % (attrs['trim_by'], read))
             self.assertEqual(read.cigartuples.tolist(), attrs['output_tuples_left'].tolist())
 
-            print("Trimming right")
+            print("Trimming right ...")
             read = copy.deepcopy(self.reads[target])
             read.trim(attrs['trim_by'], side='right')
+            print("After trimming right by %d:\n%s" % (attrs['trim_by'], read))
             self.assertEqual(read.cigartuples.tolist(), attrs['output_tuples_right'].tolist())
 
 
 class TestCaseStudy1(unittest.TestCase):
     def test_case_study_1(self):
-        d = {'gene_overlap_start': 9460, 'gene_overlap_end': 9481, 'cigartuples': [(0, 20), (2, 3), (0, 14), (1, 1), (0, 19), (1, 2), (0, 45)], 'reference_start': 9460, 'reference_end': 9561, 'reference_positions': [9460, 9461, 9462, 9463, 9464, 9465, 9466, 9467, 9468, 9469, 9470, 9471, 9472, 9473, 9474, 9475, 9476, 9477, 9478, 9479, 9483, 9484, 9485, 9486, 9487, 9488, 9489, 9490, 9491, 9492, 9493, 9494, 9495, 9496, 9497, 9498, 9499, 9500, 9501, 9502, 9503, 9504, 9505, 9506, 9507, 9508, 9509, 9510, 9511, 9512, 9513, 9514, 9515, 9516, 9517, 9518, 9519, 9520, 9521, 9522, 9523, 9524, 9525, 9526, 9527, 9528, 9529, 9530, 9531, 9532, 9533, 9534, 9535, 9536, 9537, 9538, 9539, 9540, 9541, 9542, 9543, 9544, 9545, 9546, 9547, 9548, 9549, 9550, 9551, 9552, 9553, 9554, 9555, 9556, 9557, 9558, 9559, 9560], 'query_sequence': 'ACTAGTTTCTTACCTCTATAATTCATAGAGAAAGAAAAATTTAATATGCGCCAATTTTTAAAAAAATTGGTGCCTATTTTTTTAACCAAAATTCTAATATA', 'reference_sequence': 'ACTAGTTTCTTACCTCTATAATTCATAGAGAAAGAAAAATTTAATATGCGCCAATTTTTAAAAAAATTGGTGCCTATTTTTTTAACCAAAATTCTAATATA'}
+        d = {'gene_overlap_start': 9460, 'gene_overlap_end': 9481, 'cigartuples': [(0, 20), (2, 3), (0, 14), (1, 1), (0, 19), (1, 2), (0, 45)], 'reference_start': 9460, 'reference_end': 9561, 'reference_positions': [9460, 9461, 9462, 9463, 9464, 9465, 9466, 9467, 9468, 9469, 9470, 9471, 9472, 9473, 9474, 9475, 9476, 9477, 9478, 9479, 9483, 9484, 9485, 9486, 9487, 9488, 9489, 9490, 9491, 9492, 9493, 9494, 9495, 9496, 9497, 9498, 9499, 9500, 9501, 9502, 9503, 9504, 9505, 9506, 9507, 9508, 9509, 9510, 9511, 9512, 9513, 9514, 9515, 9516, 9517, 9518, 9519, 9520, 9521, 9522, 9523, 9524, 9525, 9526, 9527, 9528, 9529, 9530, 9531, 9532, 9533, 9534, 9535, 9536, 9537, 9538, 9539, 9540, 9541, 9542, 9543, 9544, 9545, 9546, 9547, 9548, 9549, 9550, 9551, 9552, 9553, 9554, 9555, 9556, 9557, 9558, 9559, 9560], 'query_sequence': 'ACTAGTTTCTTACCTCTATAATTCATAGAGAAAGAAAAATTTAATATGCGCCAATTTTTAAAAAAATTGGTGCCTATTTTTTTAACCAAAATTCTAATATA'}
         read = Read(FakePySamAlignedSegment(**d))
 
-        print('=========================')
         read2 = copy.copy(read)
         read2.trim(21, side='left')
 
