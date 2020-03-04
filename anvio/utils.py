5b8a9a6e242029d0ef62ab9ea9ff72939f07e4f0
Evan Kiefl <kiefl.evan@gmail.com>
2020-03-03 20:02:43 -0600

    A convenient way to show what objects are taking memory

--- anvio/utils.py
+++ anvio/utils.py
@@ -13,8 +13,10 @@ import shutil
 import smtplib
 import hashlib
 import textwrap
+import linecache
 import webbrowser
 import subprocess
+import tracemalloc
 import configparser
 import multiprocessing
 import urllib.request, urllib.error, urllib.parse
@@ -82,6 +84,67 @@ def get_total_memory_usage():
             pass
 
     return human_readable_file_size(mem)
+
+
+def display_top_memory_usage(snapshot, key_type='lineno', limit=10):
+    """A pretty-print for the tracemalloc memory usage module
+
+    Modified from https://docs.python.org/3/library/tracemalloc.html
+
+    Examples
+    ========
+    >>> import tracemalloc
+    >>> import anvio.utils as utils
+    >>> tracemalloc.start()
+    >>> snap = tracemalloc.take_snapshot
+    >>> utils.display_top_memory_usage(snap)
+    Top 10 lines
+    #1: anvio/bamops.py:160: 4671.3 KiB
+        constants.cigar_consumption,
+    #2: anvio/bamops.py:96: 2571.6 KiB
+        self.cigartuples = np.array(read.cigartuples)
+    #3: python3.6/linecache.py:137: 1100.0 KiB
+        lines = fp.readlines()
+    #4: <frozen importlib._bootstrap_external>:487: 961.4 KiB
+    #5: typing/templates.py:627: 334.3 KiB
+        return type(base)(name, (base,), dct)
+    #6: typing/templates.py:923: 315.7 KiB
+        class Template(cls):
+    #7: python3.6/_weakrefset.py:84: 225.2 KiB
+        self.data.add(ref(item, self._remove))
+    #8: targets/npyimpl.py:411: 143.2 KiB
+        class _KernelImpl(_Kernel):
+    #9: _vendor/pyparsing.py:3349: 139.7 KiB
+        self.errmsg = "Expected " + _ustr(self)
+    #10: typing/context.py:456: 105.1 KiB
+        def on_disposal(wr, pop=self._globals.pop):
+    3212 other: 4611.9 KiB
+    Total allocated size: 15179.4 KiB
+    """
+
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
 
 
 def rev_comp(seq):
