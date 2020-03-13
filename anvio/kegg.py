67646bfdfa9b3edd44871ffb4050689f2255a305
Iva Veseli <iva.veseli@gmail.com>
2020-03-13 12:57:06 -0500

    MAJOR BUG FIX add leftover db entries to modules DB after processing all to make sure all modules get stored

--- anvio/kegg.py
+++ anvio/kegg.py
@@ -1045,7 +1045,7 @@ class KeggModulesDatabase(KeggContext):
 
         self.touch()
 
-        self.progress.new("Loading KEGG modules into Modules DB...")
+        self.progress.new("Loading %s KEGG modules into Modules DB..." % len(self.module_dict.keys()))
 
         # sanity check that we setup the modules previously.
         # It shouldn't be a problem since this function should only be called during the setup process after modules download, but just in case.
@@ -1094,9 +1094,14 @@ class KeggModulesDatabase(KeggContext):
                         # append_and_store will collect db entries and store every 10000 at a time
                         mod_table.append_and_store(self.db, mnum, name, val, definition, line)
 
-
+                f.close()
 
             num_modules_parsed += 1
+        # once we are done parsing all modules, we store whatever db entries remain in the db_entries list
+        # this is necessary because append_and_store() above only stores every 10000 entries
+        self.progress.update("Storing final batch of module entries into DB")
+        mod_table.store(self.db)
+
         self.progress.end()
 
         # warn user about parsing errors
