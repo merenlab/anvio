4360fa6cf5c8f5d9a8a8ac66abee624b18e34fa2
mschecht <mschechter.lb@gmail.com>
2020-03-29 19:09:48 -0500

    added ngram concept to programs.py and removed redundant user message

--- anvio/programs.py
+++ anvio/programs.py
@@ -101,6 +101,7 @@ ANVIO_ITEMS = {'pan-db': {'name': 'PAN', 'type': 'DB', 'internal': True},
                'summary': {'name': 'STATIC SUMMARY', 'type': 'SUMMARY', 'internal': False},
                'split-bins': {'name': 'SPLIT BINS', 'type': 'CONCEPT', 'internal': False},
                'state': {'name': 'INTERACTIVE STATE', 'type': 'CONCEPT', 'internal': True},
+               'ngram': {'name': 'NGRAM', 'type': 'CONCEPT', 'internal': True},
                'state-json': {'name': 'INTERACTIVE STATE', 'type': 'JSON', 'internal': False}}
 
 ANVIO_CONCEPTS = {'functions': {'goes_in': ['contigs_db', 'genomes-storage-db'],
