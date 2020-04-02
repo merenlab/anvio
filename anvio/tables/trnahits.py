38ef2184e592eacd2d35baf9e2a4a97661b7393b
A. Murat Eren <a.murat.eren@gmail.com>
2020-04-02 11:05:18 -0500

    fixy fix. do not use the same codon for all entries :/

--- anvio/tables/trnahits.py
+++ anvio/tables/trnahits.py
@@ -235,7 +235,7 @@ class TablesForTransferRNAs:
             entry = search_results_dict[entry_id]
 
             function_text = 'tRNA gene for amino acid %s (codon: %s; anticodon:%s; score:%.1f; intron_start:%d; intron_end:%d)' \
-                                            % (entry['amino_acid'], codon, entry['anticodon'], \
+                                            % (entry['amino_acid'], utils.rev_comp(entry['anticodon']), entry['anticodon'], \
                                                entry['score'], entry['intron_start'], entry['intron_end'])
 
             functions_dict[entry_id] = {'gene_callers_id': entry['gene_callers_id'],
