72115b7c37d1927c8650ee6dc7a15d90e785e6f1
A. Murat Eren <a.murat.eren@gmail.com>
2020-05-10 13:25:55 -0500

    switch lingo from items to artifacts

--- anvio/programs.py
+++ anvio/programs.py
@@ -16,6 +16,9 @@ import anvio.terminal as terminal
 import anvio.filesnpaths as filesnpaths
 
 from anvio.errors import ConfigError
+from anvio.artifacts import ANVIO_ARTIFACTS
+from anvio.summaryhtml import SummaryHTMLOutput
+
 
 __author__ = "Developers of anvi'o (see AUTHORS.txt)"
 __copyright__ = "Copyleft 2015-2018, the Meren Lab (http://merenlab.org/)"
@@ -32,88 +35,6 @@ M = lambda m: [x for x in G(os.path.dirname(utils.is_program_exists(m)))]
 S = lambda s: [x for x in G(os.path.dirname(utils.is_program_exists(s)))]
 J = lambda x: '\n'.join(x) if x else ''
 
-# this dictionary describes all anvi'o items that are referred from 'requires' and
-# 'provides' statements written in anvi'o programs
-ANVIO_ITEMS = {'pan-db': {'name': 'PAN', 'type': 'DB', 'internal': True},
-               'contigs-db': {'name': 'CONTIGS', 'type': 'DB', 'internal': True},
-               'contigs-fasta': {'name': 'CONTIGS', 'type': 'FASTA', 'internal': False},
-               'concatenated-gene-alignment-fasta': {'name': 'CONCATENATED GENE ALIGNMENT', 'type': 'FASTA', 'internal': False},
-               'short-reads-fasta': {'name': 'SHORT READS', 'type': 'FASTA', 'internal': False},
-               'genes-fasta': {'name': 'GENES', 'type': 'FASTA', 'internal': False},
-               'bam-file': {'name': 'BAM FILE', 'type': 'BAM', 'internal': False},
-               'protein-structure': {'name': 'PDB FILE', 'type': 'TXT', 'internal': False},
-               'raw-bam-file': {'name': 'RAW BAM FILE', 'type': 'BAM', 'internal': False},
-               'locus-fasta': {'name': 'LOCUS', 'type': 'FASTA', 'internal': False},
-               'structure-db': {'name': 'STRUCTURE', 'type': 'DB', 'internal': True},
-               'pdb-db': {'name': 'PDB DB', 'type': 'CONCEPT', 'internal': True},
-               'kegg-db': {'name': 'KEGG DB', 'type': 'DB', 'internal': True},
-               'single-profile-db': {'name': 'SINGLE PROFILE', 'type': 'DB', 'internal': True},
-               'profile-db': {'name': 'PROFILE', 'type': 'DB', 'internal': True},
-               'genes-db': {'name': 'GENES', 'type': 'DB', 'internal': True},
-               'genomes-storage-db': {'name': 'GENOMES STORAGE', 'type': 'DB', 'internal': True},
-               'contigs-stats': {'name': 'CONTIGS STATS', 'type': 'STATS', 'internal': False},
-               'svg': {'name': 'SVG', 'type': 'SVG', 'internal': False},
-               'bin': {'name': 'BIN', 'type': 'BIN', 'internal': True},
-               'collection': {'name': 'COLLECTION', 'type': 'COLLECTION', 'internal': True},
-               'collection-txt': {'name': 'COLLECTION', 'type': 'TXT', 'internal': False},
-               'hmm-source': {'name': 'HMM SOURCE', 'type': 'HMM', 'internal': False},
-               'hmm-profile': {'name': 'HMM PROFILE', 'type': 'CONCEPT', 'internal': True},
-               'cogs-data': {'name': 'COGs DATA', 'type': 'DATA', 'internal': True},
-               'pfams-data': {'name': 'PFAMs DATA', 'type': 'DATA', 'internal': True},
-               'trna-genes': {'name': 'TRNA GENES', 'type': 'CONCEPT', 'internal': True},
-               'misc-data-items-txt': {'name': 'ITEMS DATA', 'type': 'TXT', 'internal': False},
-               'misc-data-items': {'name': 'ITEMS DATA', 'type': 'CONCEPT', 'internal': False},
-               'misc-data-layers-txt': {'name': 'LAYERS DATA', 'type': 'TXT', 'internal': False},
-               'misc-data-layers': {'name': 'LAYERS DATA', 'type': 'CONCEPT', 'internal': False},
-               'misc-data-nucleotides-txt': {'name': 'NUCLEOTIDES DATA', 'type': 'TXT', 'internal': False},
-               'misc-data-nucleotides': {'name': 'NUCLEOTIDES DATA', 'type': 'CONCEPT', 'internal': False},
-               'misc-data-amino-acids-txt': {'name': 'AMINO_ACIDS DATA', 'type': 'TXT', 'internal': False},
-               'misc-data-amino-acids': {'name': 'AMINO_ACIDS DATA', 'type': 'CONCEPT', 'internal': False},
-               'misc-data-layers-category': {'name': 'LAYERS DATA CATEGORY', 'type': 'CONCEPT', 'internal': True},
-               'genome-similarity': {'name': 'GENOME SIMILARITY', 'type': 'CONCEPT', 'internal': False},
-               'misc-data-layer-orders': {'name': 'LAYER ORDERS DATA', 'type': 'CONCEPT', 'internal': False},
-               'misc-data-item-orders': {'name': 'ITEM ORDERS DATA', 'type': 'CONCEPT', 'internal': False},
-               'misc-data-layer-orders-txt': {'name': 'LAYER ORDERS DATA', 'type': 'TXT', 'internal': False},
-               'misc-data-item-orders-txt': {'name': 'LAYER ORDERS DATA', 'type': 'TXT', 'internal': False},
-               'dendrogram': {'name': 'DENDROGRAM', 'type': 'NEWICK', 'internal': False},
-               'metapangenome': {'name': 'METAPANGENOME', 'type': 'CONCEPT', 'internal': True},
-               'oligotypes': {'name': 'OLIGOTYPES', 'type': 'CONCEPT', 'internal': False},
-               'linkmers-txt': {'name': 'LINKMERS', 'type': 'TXT', 'internal': False},
-               'phylogeny': {'name': 'PHYLOGENY', 'type': 'NEWICK', 'internal': False},
-               'gene-calls-txt': {'name': 'GENE CALLS', 'type': 'TXT', 'internal': False},
-               'functions': {'name': 'GENE FUNCTIONS', 'type': 'CONCEPT', 'internal': True},
-               'functions-txt': {'name': 'GENE FUNCTIONS', 'type': 'TXT', 'internal': False},
-               'functional-enrichment-txt': {'name': 'ENRICHED FUNCTIONS', 'type': 'TXT', 'internal': False},
-               'kegg-functions': {'name': 'KOFAM FUNCTIONS', 'type': 'CONCEPT', 'internal': True},
-               'interactive': {'name': 'INTERACTIVE DISPLAY', 'type': 'DISPLAY', 'internal': True},
-               'view-data': {'name': 'VIEW DATA', 'type': 'TXT', 'internal': False},
-               'layer-taxonomy': {'name': 'LAYER TAXONOMY', 'type': 'CONCEPT', 'internal': True},
-               'layer-taxonomy-txt': {'name': 'LAYER TAXONOMY', 'type': 'TXT', 'internal': False},
-               'gene-taxonomy': {'name': 'GENE TAXONOMY', 'type': 'CONCEPT', 'internal': True},
-               'gene-taxonomy-txt': {'name': 'GENE TAXONOMY', 'type': 'TXT', 'internal': False},
-               'genome-taxonomy': {'name': 'GENOME TAXONOMY', 'type': 'CONCEPT', 'internal': True},
-               'genome-taxonomy-txt': {'name': 'GENOME TAXONOMY', 'type': 'TXT', 'internal': False},
-               'scgs-taxonomy-db': {'name': 'SCG TAXONOMY DB', 'type': 'CONCEPT', 'internal': True},
-               'scgs-taxonomy': {'name': 'SCG TAXONOMY', 'type': 'CONCEPT', 'internal': True},
-               'external-genomes': {'name': 'EXTERNAL GENOMES', 'type': 'TXT', 'internal': False},
-               'internal-genomes': {'name': 'INTERNAL GENOMES', 'type': 'TXT', 'internal': False},
-               'coverages-txt': {'name': 'COVERAGES', 'type': 'TXT', 'internal': False},
-               'genome-distance-txt': {'name': 'DISTANCE ESTIMATES', 'type': 'TXT', 'internal': False},
-               'genome-distance': {'name': 'DISTANCE ESTIMATES', 'type': 'CONCEPT', 'internal': True},
-               'variability-profile': {'name': 'VARIABILITY PROFILE', 'type': 'CONCEPT', 'internal': False},
-               'codon-frequencies-txt': {'name': 'CODON FREQUENCIES', 'type': 'TXT', 'internal': False},
-               'aa-frequencies-txt': {'name': 'AA FREQUENCIES', 'type': 'TXT', 'internal': False},
-               'fixation-index-matrix': {'name': 'FIXATION INDEX MATRIX', 'type': 'TXT', 'internal': False},
-               'summary': {'name': 'STATIC SUMMARY', 'type': 'SUMMARY', 'internal': False},
-               'split-bins': {'name': 'SPLIT BINS', 'type': 'CONCEPT', 'internal': False},
-               'state': {'name': 'INTERACTIVE STATE', 'type': 'CONCEPT', 'internal': True},
-               'ngrams': {'name': 'NGRAM', 'type': 'CONCEPT', 'internal': True},
-               'state-json': {'name': 'INTERACTIVE STATE', 'type': 'JSON', 'internal': False},
-               'kegg-metabolism': {'name': 'KEGG METABOLISM ESTIMATES', 'type': 'TXT', 'internal': False}}
-
-ANVIO_CONCEPTS = {'functions': {'goes_in': ['contigs_db', 'genomes-storage-db'],
-                               'used_by': ['anvi-search-functions']}
-                 }
 
 run = terminal.Run()
 progress = terminal.Progress()
@@ -345,8 +266,8 @@ class Program:
                 info = self.meta_info[info_type]['null_object']
 
             if info_type == 'requires' or info_type == 'provides':
-                # these info_types have their items cast as Item types
-                info = [Item(item_name) for item_name in info]
+                # these info_types have their items cast as Artifact types
+                info = [Artifact(artifact_name) for artifact_name in info]
 
             if type(info) == str:
                 if 'read_as_is' in self.meta_info[info_type] and self.meta_info[info_type]['read_as_is']:
@@ -392,18 +313,20 @@ class Program:
         return "PROG::%s" % self.name
 
 
-class Item:
-    def __init__(self, item_id, internal=True, optional=True, single=True):
-        if item_id not in ANVIO_ITEMS:
-            raise ConfigError("Ehem. Anvi'o does not know about item '%s'. There are two was this could happen: "
-                              "one, you made a type (easy to fix), two, you just added a new program into anvi'o "
-                              "but have not yet updated `anvio/programs.py`." % item_id)
+class Artifact:
+    """A class to describe an anvi'o artifact"""
 
-        item = ANVIO_ITEMS[item_id]
-        self.id = item_id
-        self.name = item['name']
-        self.type = item['type']
-        self.internal = item['internal']
+    def __init__(self, artifact_id, internal=True, optional=True, single=True):
+        if artifact_id not in ANVIO_ARTIFACTS:
+            raise ConfigError("Ehem. Anvi'o does not know about artifact '%s'. There are two was this could happen: "
+                              "one, you made a type (easy to fix), two, you just added a new program into anvi'o "
+                              "but have not yet updated `anvio/programs.py`." % artifact_id)
+
+        artifact = ANVIO_ARTIFACTS[artifact_id]
+        self.id = artifact_id
+        self.name = artifact['name']
+        self.type = artifact['type']
+        self.internal = artifact['internal']
 
         # attributes set by the context master
         self.single = single
@@ -411,7 +334,7 @@ class Item:
 
 
     def __repr__(self):
-        return "ITEM::%s" % self.id
+        return "ARTIFACT::%s" % self.id
 
 
 class AnvioDocs(AnvioPrograms):
@@ -457,21 +380,21 @@ class ProgramsNetwork(AnvioPrograms):
 
 
     def report_network(self):
-        item_names_seen = set([])
-        items_seen = Counter({})
-        all_items = []
+        artifact_names_seen = set([])
+        artifacts_seen = Counter({})
+        all_artifacts = []
         for program in self.programs.values():
-            for item in program.meta_info['provides']['value'] + program.meta_info['requires']['value']:
-                items_seen[item.id] += 1
-                if not item.id in item_names_seen:
-                    all_items.append(item)
-                    item_names_seen.add(item.id)
+            for artifact in program.meta_info['provides']['value'] + program.meta_info['requires']['value']:
+                artifacts_seen[artifact.id] += 1
+                if not artifact.id in artifact_names_seen:
+                    all_artifacts.append(artifact)
+                    artifact_names_seen.add(artifact.id)
 
         programs_seen = Counter({})
-        for item in all_items:
+        for artifact in all_artifacts:
             for program in self.programs.values():
-                for program_item in program.meta_info['provides']['value'] + program.meta_info['requires']['value']:
-                    if item.name == program_item.name:
+                for program_artifact in program.meta_info['provides']['value'] + program.meta_info['requires']['value']:
+                    if artifact.name == program_artifact.name:
                         programs_seen[program.name] += 1
 
         network_dict = {"graph": [], "nodes": [], "links": [], "directed": False, "multigraph": False}
@@ -480,16 +403,16 @@ class ProgramsNetwork(AnvioPrograms):
 
         index = 0
         types_seen = set(["PROGRAM"])
-        for item in all_items:
-            types_seen.add(item.type)
-            network_dict["nodes"].append({"size": items_seen[item.id],
-                                          "score": 0.5 if item.internal else 1,
-                                          "color": '#00AA00' if item.internal else "#AA0000",
-                                          "id": item.id,
-                                          "name": item.name,
-                                          "internal": True if item.internal else False,
-                                          "type": item.type})
-            node_indices[item.id] = index
+        for artifact in all_artifacts:
+            types_seen.add(artifact.type)
+            network_dict["nodes"].append({"size": artifacts_seen[artifact.id],
+                                          "score": 0.5 if artifact.internal else 1,
+                                          "color": '#00AA00' if artifact.internal else "#AA0000",
+                                          "id": artifact.id,
+                                          "name": artifact.name,
+                                          "internal": True if artifact.internal else False,
+                                          "type": artifact.type})
+            node_indices[artifact.id] = index
             index += 1
 
         for program in self.programs.values():
@@ -502,19 +425,19 @@ class ProgramsNetwork(AnvioPrograms):
             node_indices[program.name] = index
             index += 1
 
-        for item in all_items:
+        for artifact in all_artifacts:
             for program in self.programs.values():
-                for item_provided in program.meta_info['provides']['value']:
-                    if item_provided.id == item.id:
-                        network_dict["links"].append({"source": node_indices[program.name], "target": node_indices[item.id]})
-                for item_needed in program.meta_info['requires']['value']:
-                    if item_needed.id == item.id:
-                        network_dict["links"].append({"target": node_indices[program.name], "source": node_indices[item.id]})
+                for artifact_provided in program.meta_info['provides']['value']:
+                    if artifact_provided.id == artifact.id:
+                        network_dict["links"].append({"source": node_indices[program.name], "target": node_indices[artifact.id]})
+                for artifact_needed in program.meta_info['requires']['value']:
+                    if artifact_needed.id == artifact.id:
+                        network_dict["links"].append({"target": node_indices[program.name], "source": node_indices[artifact.id]})
 
         open(self.output_file_path, 'w').write(json.dumps(network_dict, indent=2))
 
         self.run.info('JSON description of network', self.output_file_path)
-        self.run.info('Item types seen', ', '.join(sorted(list(types_seen))))
+        self.run.info('Artifacts seen', ', '.join(sorted(list(types_seen))))
 
 
 class ProgramsVignette(AnvioPrograms):
