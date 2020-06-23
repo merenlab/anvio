41ffa27d809427e35e11b23ffb814fd855c1824d
Iva Veseli <iva.veseli@gmail.com>
2020-06-23 00:47:47 -0500

    loop to split multiple KOs to individual entries

--- anvio/migrations/modules/v1_to_v2.py
+++ anvio/migrations/modules/v1_to_v2.py
@@ -3,6 +3,7 @@
 
 import sys
 import argparse
+import re
 
 import anvio.db as db
 import anvio.tables as t
@@ -29,6 +30,37 @@ def migrate(db_path):
         modules_db.disconnect()
         raise ConfigError("Version of this modules database is not %s (hence, "
                           "this script cannot really do anything)." % current_version)
+
+    # split any orthology entries with multiple KOs into separate lines
+    where_clause = "data_name = 'ORTHOLOGY' AND (data_value LIKE '%+%' OR data_value LIKE '%-%')"
+    bad_rows = modules_db.get_some_rows_from_table(t.module_table_name, where_clause)
+
+    modules_db.remove_some_rows_from_table(t.module_table_name, where_clause)
+
+    good_rows = []
+    for mod, orth, kos, definition, line in bad_rows:
+        if orth != "ORTHOLOGY":
+            raise ConfigError("Serious SQL error here. We asked for ORTHOLOGY but we got %s" % orth)
+        split_kos = re.split('\+|\-', kos)
+        for ko in split_kos:
+            if len(ko) != 6 or ko[0] != 'K':
+                raise ConfigError("Uh oh. We split a KO that doesn't seem to have the right format. It looks like this: %s, "
+                                  "and the string that it came from is %s." % (ko, kos))
+            good_rows.append((mod, orth, ko, definition, line))
+
+    modules_db.insert_many(t.module_table_name, entries=good_rows)
+
+
+    prev_total_entries = modules_db.get_meta_value('total_entries')
+    modules_db.remove_meta_key_value_pair('total_entries')
+    modules_db.set_meta_value('total_entries', prev_total_entries - len(bad_rows) + len(good_rows))
+    modules_db.remove_meta_key_value_pair('version')
+    modules_db.set_version(next_version)
+    modules_db.disconnect()
+
+    run.info_single("Huzzah! Your modules db is now %s. This update fixed KOs that did not have individual "
+                    "entries in the modules table." % (next_version), nl_after=1, nl_before=1, mc='green')
+
 
 
 if __name__ == '__main__':
