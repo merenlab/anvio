2f0a2dc57871cd35d398149abbad283149225ed1
Sam Miller <samuelmiller10@gmail.com>
2020-06-25 23:45:16 -0500

    Rudiments of constants package

---
+++ anvio/constants_package/trnaseq.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+
+__author__ = "Developers of anvi'o (see AUTHORS.txt)"
+__copyright__ = "Copyleft 2015-2020, the Meren Lab (http://merenlab.org/)"
+__credits__ = []
+__license__ = "GPL 3.0"
+__maintainer__ = "Samuel Miller"
+__email__ = "samuelmiller10@gmail.com"
+__status__ = "Development"
+
+
+THREEPRIME_VARIANT_LIST = ['CCA',
+                           'C', 'CC',
+                           'CCC', 'CCG', 'CCT',
+                           'CAA', 'CGA', 'CTA',
+                           'ACA', 'GCA', 'TCA',
+                           'CCAA', 'CCAC', 'CCAG', 'CCAT',
+                           'CCAAA', 'CCAAC', 'CCAAG', 'CCAAT',
+                           'CCACA', 'CCACC', 'CCACG', 'CCACT',
+                           'CCAGA', 'CCAGC', 'CCAGG', 'CCAGT',
+                           'CCATA', 'CCATC', 'CCATG', 'CCATT']
+
