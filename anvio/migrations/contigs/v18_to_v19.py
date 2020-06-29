6ff849e2e13fb5a6b6a5877b8cdfbb2b282564b0
A. Murat Eren <a.murat.eren@gmail.com>
2020-06-29 15:31:14 -0500

    updating my migration script. all your transfer RNA belong to us.

--- anvio/migrations/contigs/v18_to_v19.py
+++ anvio/migrations/contigs/v18_to_v19.py
@@ -31,8 +31,8 @@ def migrate(db_path):
     if str(contigs_db.get_version()) != current_version:
         raise ConfigError("Version of this contigs database is not %s (hence, this script cannot really do anything)." % current_version)
 
-    progress.new("Creating a new table for tRNA taxonomy")
-    progress.update("...")
+    progress.new("Le migrateaoux")
+    progress.update("Creating a new table for tRNA taxonomy")
 
     # just to be on the safe side.
     try:
@@ -50,6 +50,24 @@ def migrate(db_path):
     contigs_db.set_meta_value('trna_taxonomy_database_version', None)
     contigs_db.create_table(trna_taxonomy_table_name, trna_taxonomy_table_structure, trna_taxonomy_table_types)
 
+    progress.update("Removing tRNA hits")
+    relevant_gene_calls = [g[0] for g in contigs_db._exec("select entry_id from hmm_hits where source='Transfer_RNAs'").fetchall()]
+
+    if len(relevant_gene_calls):
+        for table_name in ['hmm_hits_info', 'hmm_hits', 'hmm_hits_in_splits', 'genes_in_contigs', 'gene_functions']:
+            contigs_db.remove_some_rows_from_table(table_name, 'source IN ("Transfer_RNAs")')
+
+        CLAUSE = "gene_callers_id in (%s)" % (','.join([str(x) for x in relevant_gene_calls]))
+        for table_name in ['gene_amino_acid_sequences', 'genes_taxonomy', 'genes_in_splits']:
+            contigs_db.remove_some_rows_from_table(table_name, CLAUSE)
+
+        gene_function_sources = contigs_db.get_meta_value('gene_function_sources')
+        if len(gene_function_sources):
+            new_gene_function_sources = ','.join([f for f in gene_function_sources.split(',') if f != "Transfer_RNAs"])
+
+            contigs_db.remove_meta_key_value_pair('gene_function_sources')
+            contigs_db.set_meta_value('gene_function_sources', new_gene_function_sources)
+
     progress.update("Updating version")
     contigs_db.remove_meta_key_value_pair('version')
     contigs_db.set_version(next_version)
@@ -58,9 +76,17 @@ def migrate(db_path):
     contigs_db.disconnect()
 
     progress.end()
-    run.info_single("The contigs database is now %s. This upgrade added an empty tRNA taxonomy table "
-                    "in it. Probably you will never use it becasue who cares about tRNA taxonomy amirite? "
-                    "Well, IT WILL FOREVER BE THERE ANYWAY. BOOM." % (next_version), nl_after=1, nl_before=1, mc='green')
+    message = (f"The contigs database is now {next_version}. This upgrade added an empty tRNA taxonomy table "
+               f"in it. Probably you will never use it becasue who cares about tRNA taxonomy amirite? "
+               f"Well, IT WILL FOREVER BE THERE ANYWAY. BOOM.")
+
+    if relevant_gene_calls:
+        message += (" This update also removed tRNA HMM hits from your contis database :/ It was really very "
+                    "necessary since one of the developers of anvi'o (*COUGH* meren *COUGH*) was very confused "
+                    "anticodons. Now everything is fixed, but unfortunatley you will have to re-run "
+                    "`anvi-scan-trnas` program on your contigs database :( ")
+
+    run.info_single(message, nl_after=1, nl_before=1, mc='green')
 
 
 if __name__ == '__main__':
