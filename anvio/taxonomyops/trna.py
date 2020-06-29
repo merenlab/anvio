aa3d1b221237a92b885222dcade616f2a0aef271
A. Murat Eren <a.murat.eren@gmail.com>
2020-06-29 13:47:26 -0500

    the default max-num-target-sequences is 100 for tRNA searchers (unlike SCGs, which is still 20).

--- anvio/taxonomyops/trna.py
+++ anvio/taxonomyops/trna.py
@@ -512,7 +512,7 @@ class PopulateContigsDatabaseWithTRNATaxonomy(TRNATaxonomyArgs, SanityCheck, Pop
 
         self.ctx = ctx
 
-        self.max_target_seqs = 20
+        self.max_target_seqs = int(A('max_num_target_sequences')) or 100
         self.evalue = float(A('e_value')) if A('e_value') else 1e-05
         self.min_pct_id = float(A('min_percent_identity')) if A('min_percent_identity') else 90
 
