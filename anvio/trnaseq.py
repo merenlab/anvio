180150d75200d4b93253b7b83fed599de6099517
Sam Miller <samuelmiller10@gmail.com>
2020-07-21 15:15:04 -0500

    aesthetic

--- anvio/trnaseq.py
+++ anvio/trnaseq.py
@@ -983,23 +983,23 @@ class TRNASeqDataset:
 
 
     def calc_normalization_stats(self):
-        self.progress.new("Calculating normalized tRNA statistics")
+        self.progress.new("Calculating normalized tRNA stats")
 
-        self.progress.update("Counting the normalized sequences containing each trimmed sequence")
+        self.progress.update("Counting normalized sequences containing each trimmed sequence")
         normalized_count_dict = OrderedDict([(trimmed_seq.representative_id, 0) for trimmed_seq in self.trimmed_trna_seqs])
         for normalized_seq in self.normalized_trna_seqs:
             for trimmed_seq in normalized_seq.trimmed_seqs:
                 normalized_count_dict[trimmed_seq.representative_id] += 1
         self.counts_of_normalized_seqs_containing_trimmed_seqs = [normalized_count for normalized_count in normalized_count_dict.values()]
 
-        self.progress.update("Finding the \"multiplicity\" of each trimmed sequence among normalized sequences")
+        self.progress.update("Finding the \"multiplicity\" of trimmed sequences among normalized sequences")
         multiplicity_dict = OrderedDict()
         for trimmed_seq, normalized_count_item in zip(self.trimmed_trna_seqs, normalized_count_dict.items()):
             trimmed_representative_id, normalized_count = normalized_count_item
             multiplicity_dict[trimmed_representative_id] = trimmed_seq.input_count * normalized_count
         self.multiplicities_of_trimmed_seqs_among_normalized_seqs = [multiplicity for multiplicity in multiplicity_dict.values()]
 
-        self.progress.update("Finding the \"average multiplicity\" of each normalized sequence")
+        self.progress.update("Finding the \"average multiplicity\" of normalized sequences")
         for normalized_seq in self.normalized_trna_seqs:
             multiplicity_sum = 0
             for trimmed_seq in normalized_seq.trimmed_seqs:
