fe9cbd33d4e306612d1a450fb0f42672fc4a24b8
Evan Kiefl <kiefl.evan@gmail.com>
2020-07-23 12:36:09 -0500

    clarify error msg

--- anvio/tables/miscdata.py
+++ anvio/tables/miscdata.py
@@ -72,7 +72,7 @@ class AdditionalAndOrderDataBaseClass(Table, object):
 
         if not self.db_path:
             raise ConfigError("The AdditionalAndOrderDataBaseClass is inherited with an args object that did not "
-                              "contain any database path :/ Even though any of the following would "
+                              "contain any db_path :/ Even though any of the following would "
                               "have worked: %s :(" % ', '.join(["'%s'" % x for x in acceptable_db_inputs]))
 
         if not self.table_name:
