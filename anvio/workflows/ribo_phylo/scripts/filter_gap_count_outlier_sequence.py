872532d3cb13ab1de2d85c1dcb3a9c802d14d384
mschechter <mschechter.lb@gmail.com>
2020-10-19 15:53:18 -0500

    Added FIXME to filter_gap_count_outlier_sequence.py

--- anvio/workflows/ribo_phylo/scripts/filter_gap_count_outlier_sequence.py
+++ anvio/workflows/ribo_phylo/scripts/filter_gap_count_outlier_sequence.py
@@ -10,6 +10,11 @@ gap_counts_distribution_df = pd.read_csv(snakemake.input.tsv, \
 									          sep="\t", \
 									          index_col=None)
 
+# FIXME: Here the workflow either uses a user defined num gaps threshold or the default 
+# value of 1.5*IQR to filter out sequences from the MSA. The problem is that the user
+# defined value is currently fixed for every single MSA while the 1.5*IQR is modular for 
+# each MSA. The user should be able to provide a dict with the name of the MSA and the gap
+# gap threshold specific to it i.e. {"Ribosome_L16": "3", "Ribosome_L16": "5"}
 if snakemake.params.gap_threshold:
 
 	outlier_threshold = int(snakemake.params.gap_threshold)
