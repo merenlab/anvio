e017943c88dd355e8a46b1ce8949dded92f96e8b
A. Murat Eren <a.murat.eren@gmail.com>
2020-12-31 17:00:19 -0600

    an extremely important detail re: h5py
    it turns out installing newer versions of h5py totall breaks our
    migration of databases from a few years ago. while it is recoverable
    with no problem, it would have been a horrible experience for the users.
    
    testing the release candidate is pretty boring, but it reveals a lot of
    minor and major issues.

--- anvio/utils.py
+++ anvio/utils.py
@@ -4067,10 +4067,11 @@ def check_h5py_module():
         import h5py
         h5py.__version__
     except:
-        raise ConfigError("Please install the Python module `h5py` manually for this migration task to continue. "
-                          "The reason why the standard anvi'o installation did not install module is complicated, "
-                          "and really unimportant. If you run `pip install h5py` in your Python virtual environment "
-                          "for anvi'o, and try running the migration program again things should be alright.")
+        raise ConfigError("There is an issue but it is easy to resolve and everything is fine! To continue, please "
+                          "first install the Python module `h5py` by running `pip install h5py==2.8.0` in your "
+                          "anvi'o environment. The reason why the standard anvi'o package does not include "
+                          "this module is both complicated and really unimportant. Re-running the migration "
+                          "after `h5py` is installed will make things go smootly.")
 
 
 def RepresentsInt(s):
