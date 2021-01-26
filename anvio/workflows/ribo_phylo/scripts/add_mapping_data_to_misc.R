90399fc23b415c34170a4df715eaf1f1b00fb181
mschechter <mschechter.lb@gmail.com>
2021-01-26 15:33:24 -0600

    updating misc data for mapping script

--- anvio/workflows/ribo_phylo/scripts/add_mapping_data_to_misc.R
+++ anvio/workflows/ribo_phylo/scripts/add_mapping_data_to_misc.R
@@ -3,39 +3,24 @@
 packages <- c("tidyverse")
 suppressMessages(lapply(packages, library, character.only = TRUE))
 
-setwd("~/scratch/OSD")
+setwd("/Users/mschechter/github/ACE_SO_metagenomes/analyses/test_case")
 
-# misc_data <- read_tsv("RIBO_PHYLO_WORKFLOW/07_MISC_DATA/Ribosomal_L16/Ribosomal_L16_all_misc_data_final.tsv")
-# mapping_data <- read_tsv("profile_SCGs/07_SUMMARY/Ribosomal_l16/bin_by_bin/EVERYTHING/EVERYTHING-gene_coverages.txt")
-# reformat_file <- read_tsv("profile_SCGs/02_FASTA/Ribosomal_l16/Ribosomal_l16-reformat-report.txt", col_names = FALSE) %>%
-#   rename(contig = X1, name = X2)
-# 
-# misc_data <- misc_data %>%
-#   filter(new_header != "new_header")
-# 
-# index <- external_gene_calls %>%
-#   select(gene_callers_id, contig) %>%
-#   left_join(mapping_data) %>%
-#   left_join(reformat_file)
-# 
-# misc_plus_cov <- external_gene_calls %>%
-#   select(gene_callers_id, contig) %>%
-#   left_join(index) %>%
-#   select(-contig, -gene_callers_id)
-# 
-# misc_plus_cov_2 <- misc_data %>% left_join(misc_plus_cov)
-
-# For now I am hacking this and just col_binding the two datasets. 
 misc_data <- read_tsv("RIBO_PHYLO_WORKFLOW/07_MISC_DATA/Ribosomal_L16/Ribosomal_L16_all_misc_data_final.tsv")
 mapping_data <- read_tsv("profile_SCGs/07_SUMMARY/Ribosomal_l16/bin_by_bin/EVERYTHING/EVERYTHING-gene_coverages.txt")
+reformat_file <- read_tsv("profile_SCGs/02_FASTA/Ribosomal_l16/Ribosomal_l16-reformat-report.txt", col_names = FALSE) %>%
+  rename(contig = X1, name = X2)
+gene_calls <- read_tsv("profile_SCGs/07_SUMMARY/Ribosomal_l16/bin_by_bin/EVERYTHING/EVERYTHING-gene_calls.txt")
 
-misc_data <- misc_data %>%
-  filter(new_header != "new_header")
+# Make index of names
+index <- mapping_data %>% left_join(gene_calls) %>% left_join(reformat_file) %>% select(gene_callers_id, contig, name)
 
-misc_plus_cov_2 <- misc_data %>%
-  bind_cols(mapping_data) %>%
-  select(-gene_callers_id)
+# Make final table
+final <- mapping_data %>% 
+          left_join(index) %>%
+          left_join(misc_data %>% rename(name = new_header)) %>%
+          select(-gene_callers_id, -contig) %>%
+          select(name, sample, contig_db_type, t_domain, t_phylum,t_order    ,    t_family    ,      t_genus ,   t_species , has_genomic_SCG_in_cluster , ERR771092)
+write_tsv(final, "RIBO_PHYLO_WORKFLOW/07_MISC_DATA/Ribosomal_L16/Ribosomal_L16_all_misc_data_final_coverage.tsv")
 
-write_tsv(misc_plus_cov_2, "08_MISC_DATA/Ribosomal_L16/Ribosomal_L16_all_misc_data_final_coverage.tsv")
 
 
