720ac6ce0dff6a79920b0e3671f98ad75879a8d7
Evan Kiefl <kiefl.evan@gmail.com>
2021-02-02 12:44:39 -0600

    DBInfo is now a factory class (and a base class) for all other DB types. This means DBInfo('a_database.db') is now smart enough to figure out which kind of DB it is. DBInfo('a_database.db').type will reveal 'structure', for example

--- anvio/dbinfo.py
+++ anvio/dbinfo.py
@@ -4,82 +4,110 @@ from anvio.db import DB
 import anvio.utils as utils
 import anvio.filesnpaths as filesnpaths
 from anvio.terminal import Run, Progress
+from anvio.errors import ConfigError
 
 import os
 
+from abc import ABC, abstractmethod
 
-class DBInfo(object):
-    def __init__(self, path, dont_raise=False):
-        self.path = path
-        self.type, self.hash, self.variant = (None, None, None)
 
-        if not self.is_db(dont_raise):
+class DBInfo(ABC):
+    db_type = None
+
+    def __new__(cls, path, dont_raise=False):
+        if not cls.is_db(path, dont_raise=dont_raise):
             return
 
-        self.type, self.hash, self.variant = self.get_basic_info()
+        db_type = cls.get_type(path)
+
+        if db_type == 'contigs':
+            return super().__new__(ContigsDBInfo)
+        elif db_type == 'profile':
+            return super().__new__(ProfileDBInfo)
+        elif db_type == 'structure':
+            return super().__new__(StructureDBInfo)
+        else:
+            raise NotImplementedError(f"db_type {db_type} has not been implemented for DBInfo")
 
 
-    def is_db(self, dont_raise=False):
+    def __init__(self, path):
+        if self.db_type is None:
+            raise NotImplementedError(f"{self.__class__} must set a `db_type` attribute")
+
+        self.path = path
+
+
+    @staticmethod
+    def is_db(path, dont_raise=False):
         try:
-            with self.load_db() as database:
+            with DB(path, None, ignore_version=True) as database:
                 if 'self' not in database.get_table_names():
                     return False
         except Exception as e:
             if dont_raise:
                 return False
             else:
-                raise ConfigError(f"Someone downstream doesn't like your so called database, '{db_path}'. They say "
+                raise ConfigError(f"Someone downstream doesn't like your so called database, '{path}'. They say "
                                   f"\"{e}\". Awkward :(")
-
         return True
+
+
+    @staticmethod
+    def get_type(path):
+        with DB(path, None, ignore_version=True) as database:
+            return database.get_meta_value('db_type')
+
+
+    @abstractmethod
+    def hash(self):
+        """This must be defined in the Child class. @abstractmethod takes no crap and ensures this happens"""
+        pass
+
+
+    @property
+    def variant(self):
+        with self.load_db() as database:
+            return database.get_meta_value('db_variant', return_none_if_not_in_table=True)
 
 
     def load_db(self):
         return DB(self.path, None, ignore_version=True)
 
 
-    def get_type(self):
+
+class ContigsDBInfo(DBInfo):
+    db_type = 'contigs'
+    def __init__(self, path, dont_raise):
+        DBInfo.__init__(self, path)
+
+
+    @property
+    def hash(self):
         with self.load_db() as database:
-            return database.get_meta_value('db_type')
+            return database.get_meta_value('contigs_db_hash', return_none_if_not_in_table=True)
 
 
-    def get_variant(self):
+class StructureDBInfo(DBInfo):
+    db_type = 'structure'
+    def __init__(self, path, dont_raise):
+        DBInfo.__init__(self, path)
+
+
+    @property
+    def hash(self):
         with self.load_db() as database:
-            try:
-                db_variant = database.get_meta_value('db_variant')
-            except:
-                db_variant = None
+            return database.get_meta_value('contigs_db_hash', return_none_if_not_in_table=True)
 
-            return db_variant
+class ProfileDBInfo(DBInfo):
+    db_type = 'profile'
+    def __init__(self, path, dont_raise):
+        DBInfo.__init__(self, path)
 
 
-    def get_hash(self):
+    @property
+    def hash(self):
         with self.load_db() as database:
-            db_type = self.get_type() if self.type is None else self.type
-
-            if db_type in ['profile', 'contigs', 'structure']:
-                try:
-                    db_hash = database.get_meta_value('contigs_db_hash')
-                except:
-                    db_hash = None
-            elif db_type in ['pan']:
-                try:
-                    db_hash = database.get_meta_value('genomes_storage_hash')
-                except:
-                    db_hash = None
-            elif db_type in ['genomestorage']:
-                try:
-                    db_hash = database.get_meta_value('hash')
-                except:
-                    db_hash = None
-            else:
-                db_hash = None
-
-            return db_hash
-
-
-    def get_basic_info(self):
-        return self.get_type(), self.get_hash(), self.get_variant()
+            return database.get_meta_value('contigs_db_hash', return_none_if_not_in_table=True)
 
 
 class FindAnvioDBs(object):
@@ -111,13 +139,13 @@ class FindAnvioDBs(object):
         for db_path, level in self.walk(depth=3):
             db_info = DBInfo(db_path, dont_raise=True)
 
-            if db_info.type is not None:
-                if db_info.type not in self.anvio_dbs:
-                    self.anvio_dbs[db_info.type] = []
+            if db_info is not None:
+                if db_info.db_type not in self.anvio_dbs:
+                    self.anvio_dbs[db_info.db_type] = []
 
                 # Add a cheeky `level` attribute to db_info for no other reason than to order dbs after the walk
                 db_info.level = level
-                self.anvio_dbs[db_info.type].append(db_info)
+                self.anvio_dbs[db_info.db_type].append(db_info)
 
         # sort by level, so we know what is closest to the search_path root directory
         for db_type in self.anvio_dbs:
