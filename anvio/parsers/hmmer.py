673326eabcc014427ae6a422d86b6578322c33c9
Iva Veseli <iva.veseli@gmail.com>
2021-02-08 15:11:59 -0600

    include bitscores in annotation dictionary, instead of separate dict

--- anvio/parsers/hmmer.py
+++ anvio/parsers/hmmer.py
@@ -640,7 +640,7 @@ class HMMERTableOutput(Parser):
         return list(zip(*col_info))
 
 
-    def get_search_results(self, noise_cutoff_dict=None, return_bitscore_dict=False):
+    def get_search_results(self, noise_cutoff_dict=None):
         """Goes through the hits provided by `hmmscan` and generates an annotation dictionary with the relevant information about each hit.
 
         This function makes sure only hits with a high enough bit score make it into the annotation dictionary.
@@ -650,31 +650,24 @@ class HMMERTableOutput(Parser):
         noise_cutoff_dict : dictionary
             dictionary of noise cutoff terms; see setup_ko_dict in kofam.py for an example
 
-        return_bitscore_dict : boolean
-            if True, this function will also return a dictionary of bitscores for each hit
-
         Returns
         =======
         annotations_dict : dictionary
-            dictionary of annotations, one annotation per HMM hit
-        bitscore_dict : dictionary
-            dictionary of bitscore information, one entry per HMM hit, including full and domain-level bitscore.
-            only returned if return_bitscore_dict is True, and only applies to GENE context.
+            dictionary of annotations, one annotation per HMM hit. Includes full and domain-level bitscore
+            for GENE context.
         """
 
         annotations_dict = {}
-        bit_score_info_dict = {}
-
 
         # this is the stuff we are going to try to fill with this:
         # search_table_structure = ['entry_id', 'source', 'alphabet', 'contig', 'gene_callers_id' 'gene_name', 'gene_hmm_id', 'e_value']
+        # plus we also add bitscore values for GENE context
 
         entry_id = 0
         num_hits_removed = 0 # a counter for the number of hits we don't add to the annotation dictionary
 
         for hit in list(self.dicts['hits'].values()):
             entry = None
-            bit_score_info_dict_entry = None
 
             if self.context == 'GENE':
                 # Here we only add the hit to the annotations_dict if the appropriate bit score is above the
@@ -700,15 +693,9 @@ class HMMERTableOutput(Parser):
                                  'gene_name': hit['gene_name'],
                                  'gene_hmm_id': hit['gene_hmm_id'],
                                  'gene_callers_id': hit['gene_callers_id'],
-                                 'e_value': hit['e_value']}
-                        if return_bitscore_dict:
-                            bit_score_info_dict_entry = {'entry_id': entry_id,
-                                     'gene_name': hit['gene_name'],
-                                     'gene_hmm_id': hit['gene_hmm_id'],
-                                     'gene_callers_id': hit['gene_callers_id'],
-                                     'e_value': hit['e_value'],
-                                     'bit_score': hit['bit_score'],
-                                     'domain_bit_score': hit['dom_bit_score']}
+                                 'e_value': hit['e_value'],
+                                 'bit_score': hit['bit_score'],
+                                 'domain_bit_score': hit['dom_bit_score']}
                     else:
                         num_hits_removed += 1
 
@@ -722,30 +709,19 @@ class HMMERTableOutput(Parser):
                              'gene_name': hit['gene_name'],
                              'gene_hmm_id': hit['gene_hmm_id'],
                              'gene_callers_id': hit['gene_callers_id'],
-                             'e_value': hit['e_value']}
-                    if return_bitscore_dict:
-                        bit_score_info_dict_entry = {'entry_id': entry_id,
-                                 'gene_name': hit['gene_name'],
-                                 'gene_hmm_id': hit['gene_hmm_id'],
-                                 'gene_callers_id': hit['gene_callers_id'],
-                                 'e_value': hit['e_value'],
-                                 'bit_score': hit['bit_score'],
-                                 'domain_bit_score': hit['dom_bit_score']}
+                             'e_value': hit['e_value'],
+                             'bit_score': hit['bit_score'],
+                             'domain_bit_score': hit['dom_bit_score']}
 
                 else:
                     entry = {'entry_id': entry_id,
                              'gene_name': hit['gene_name'],
                              'gene_hmm_id': hit['gene_hmm_id'],
                              'gene_callers_id': hit['gene_callers_id'],
-                             'e_value': hit['e_value']}
-                    if return_bitscore_dict:
-                        bit_score_info_dict_entry = {'entry_id': entry_id,
-                                 'gene_name': hit['gene_name'],
-                                 'gene_hmm_id': hit['gene_hmm_id'],
-                                 'gene_callers_id': hit['gene_callers_id'],
-                                 'e_value': hit['e_value'],
-                                 'bit_score': hit['bit_score'],
-                                 'domain_bit_score': hit['dom_bit_score']}
+                             'e_value': hit['e_value'],
+                             'bit_score': hit['bit_score'],
+                             'domain_bit_score': hit['dom_bit_score']}
+
             elif self.context == 'CONTIG' and (self.alphabet == 'DNA' or self.alphabet == 'RNA'):
                 entry = {'entry_id': entry_id,
                          'gene_name': hit['gene_name'],
@@ -760,14 +736,9 @@ class HMMERTableOutput(Parser):
             if entry:
                 entry_id += 1
                 annotations_dict[entry_id] = entry
-                if return_bitscore_dict and bit_score_info_dict_entry:
-                    bit_score_info_dict[entry_id] = bit_score_info_dict_entry
 
         self.run.info("Number of weak hits removed", num_hits_removed)
         self.run.info("Number of hits in annotation dict ", len(annotations_dict.keys()))
 
-        if return_bitscore_dict:
-            return annotations_dict, bit_score_info_dict
 
         return annotations_dict
-
