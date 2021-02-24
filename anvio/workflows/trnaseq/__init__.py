897ee3f8115c318ea97f367b766d519cdf9b1bc5
Sam Miller <samuelmiller10@gmail.com>
2021-02-24 14:06:39 -0600

    `--min-distance-between-deletions` added to workflow, and, correcting oversight, `--max-distinct-deletions` now in `anvi-trnaseq` command

--- anvio/workflows/trnaseq/__init__.py
+++ anvio/workflows/trnaseq/__init__.py
@@ -83,6 +83,7 @@ class TRNASeqWorkflow(WorkflowSuperClass):
             '--fiveprimemost-deletion-stop',
             '--threeprimemost-deletion-stop',
             '--max-distinct-deletions',
+            '--min-distance-between-deletions',
             '--skip-fasta-check',
             '--alignment-target-chunk-size',
             '--fragment-mapping-query-chunk-length',
@@ -156,6 +157,7 @@ class TRNASeqWorkflow(WorkflowSuperClass):
                 '--fiveprimemost-deletion-stop': anvio.D['fiveprimemost-deletion-stop'][1]['default'],
                 '--threeprimemost-deletion-stop': anvio.D['threeprimemost-deletion-stop'][1]['default'],
                 '--max-distinct-deletions': anvio.D['max-distinct-deletions'][1]['default'],
+                '--min-distance-between-deletions': anvio.D['min-distance-between-deletions'][1]['default'],
                 '--skip-fasta-check': True, # not the default in anvi-trnaseq
                 '--alignment-target-chunk-size': anvio.D['alignment-target-chunk-size'][1]['default'],
                 '--fragment-mapping-query-chunk-length': anvio.D['fragment-mapping-query-chunk-length'][1]['default'],
