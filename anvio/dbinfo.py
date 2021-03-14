e515fadf6e2d2139e1738cce7b543e9333754fdb
A. Murat Eren <a.murat.eren@gmail.com>
2021-03-13 22:16:03 -0600

    cleaner

--- anvio/dbinfo.py
+++ anvio/dbinfo.py
@@ -116,7 +116,11 @@ class DBInfo(ABC):
         if expecting is not None and db_type not in expecting:
             if dont_raise:
                 return
-            raise ConfigError(f"Was expecting any of the db types {expecting}, but '{path}' has type '{db_type}'")
+            if len(expecting) == 1:
+                raise ConfigError(f"The database at '{path}' is a {db_type} database but you passed it as a '{expecting[0]}' database :/")
+            else:
+                raise ConfigError(f"The database at '{path}' is a {db_type} database but your parameters claim that it is of type "
+                                  f"either {' or '.join(expecting)} :/")
 
         if db_type in dbinfo_classes:
             # This is the most important line in this method:
