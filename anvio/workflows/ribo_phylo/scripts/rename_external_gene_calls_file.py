d8721578bc3cd82b4252b73c50b1c29e44eee61c
mschechter <mschechter.lb@gmail.com>
2021-04-08 13:22:30 -0500

    Duplicate gene names across metagenomes... the improbable happend
    Basically I was getting dupicate contig names which can only happen if by chance there is an SCG on the same contig name at the same gene-caller-id in multiple genomes... and it happened

--- anvio/workflows/ribo_phylo/scripts/rename_external_gene_calls_file.py
+++ anvio/workflows/ribo_phylo/scripts/rename_external_gene_calls_file.py
@@ -11,11 +11,10 @@ reformat_report = pd.read_csv(snakemake.input.reformat_file_all, \
                   sep="\t", \
                   index_col=False, \
                   names=["new_header", "header"])
-
+# print(reformat_report.head)
 external_gene_calls = pd.read_csv(snakemake.input.external_gene_calls_all, \
-                  sep="\t", \
+                  delim_whitespace=True, \
                   index_col=False)
-
 
 headers = pd.read_csv(snakemake.input.headers, \
                   sep="\t", \
@@ -23,20 +22,25 @@ headers = pd.read_csv(snakemake.input.headers, \
                   names=["contig"])
 # print(headers)
 
-fasta_df = pd.DataFrame({'header': [], 'sequence': []})
+# fasta_df = pd.DataFrame({'header': [], 'sequence': []})
 
-for seq_record in SeqIO.parse(snakemake.input.SCGs_nt, "fasta"):
-    fasta_df = fasta_df.append({'header': str(seq_record.description), 'sequence': str(seq_record.seq)}, ignore_index=True)
+# for seq_record in SeqIO.parse(snakemake.input.SCGs_nt, "fasta"):
+#     fasta_df = fasta_df.append({'header': str(seq_record.description), 'sequence': str(seq_record.seq)}, ignore_index=True)
 
 
 # reformat_report["sample"] = reformat_report['header'].str.split("bin_id:|\|source:", expand=True)[1].astype(str).str.strip("-contigs")
-reformat_report["sample"] = reformat_report['header'].str.split("contig:|\|gene_callers_id:", expand=True)[1].astype(str)
+reformat_report["sample_contig"] = reformat_report['header'].str.split("contig:|\|gene_callers_id:", expand=True)[1].astype(str)
 reformat_report["gene_callers_id"] = reformat_report['header'].str.split("gene_callers_id:|\|start:", expand=True)[1].astype(str)
-reformat_report["contig"] =  reformat_report['sample'] + "_" + reformat_report['gene_callers_id'].astype(str)
+reformat_report["sample_name"] = reformat_report['header'].str.split("bin_id:|\|source:", expand=True)[1].astype(str)
+reformat_report['sample_name'] = reformat_report['sample_name'].map(lambda x: x.rstrip('-contigs'))
+reformat_report["contig"] =  reformat_report['sample_name'] + "_" + reformat_report['sample_contig'] + "_" + reformat_report['gene_callers_id'].astype(str)
 reformat_report = reformat_report.drop(columns=['gene_callers_id'])
+# print(reformat_report.head)
 
 # Join
-external_gene_calls_filtered = reformat_report.merge(external_gene_calls, on="contig", how="inner").drop(columns=['header', 'sample', 'contig']).rename(columns={'new_header': 'contig'})
+# print(reformat_report.contig)
+# print(external_gene_calls)
+external_gene_calls_filtered = reformat_report.merge(external_gene_calls, on="contig", how="inner").drop(columns=['header', 'sample_name', 'contig']).rename(columns={'new_header': 'contig'})
 external_gene_calls_filtered_ed = external_gene_calls_filtered.merge(headers, on="contig", how="inner")
 external_gene_calls_filtered_final = external_gene_calls_filtered_ed[["gene_callers_id", "contig", "start", "stop", "direction", "partial", "call_type", "source", "version", "aa_sequence"]]
 external_gene_calls_filtered_final['gene_callers_id'] = external_gene_calls_filtered_ed.index
