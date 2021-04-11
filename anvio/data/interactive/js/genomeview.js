a4ddc62f501a22efc5e6920bedf6f75d2f97271f
A. Murat Eren <a.murat.eren@gmail.com>
2021-04-11 18:06:54 -0500

    placeholders for genome view interactive files

---
+++ anvio/data/interactive/js/genomeview.js
@@ -0,0 +1,36 @@
+/**
+ * Javascript library for anvi'o genome view
+ *
+ *  Authors: Isaac Fink <iafink@uchicago.edu>
+ *           Matthew Klein <mtt.l.kln@gmail.com>
+ *  Copyright 2021, The anvi'o project
+ *
+ * This file is part of anvi'o (<https://github.com/meren/anvio>).
+ *
+ * Anvi'o is a free software. You can redistribute this program
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with anvi'o. If not, see <http://opensource.org/licenses/GPL-3.0>.
+ *
+ * @license GPL-3.0+ <http://opensource.org/licenses/GPL-3.0>
+ */
+
+
+$(document).ready(function() {
+    initData();
+});
+
+function initData() {
+// initialize the bulk of the data.
+    $.ajax({
+        type: 'POST',
+        cache: false,
+        url: '/data/get_genome_view_data',
+        success: function(data) {
+            console.log(data);
+        }
+    });
+}
