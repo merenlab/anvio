714c04dad6cef3cfdbb2686290323e909bd391a0
Matthew Klein <mtt.l.kln@gmail.com>
2021-08-23 17:12:03 +0000

    filename change to reflect broader application

---
+++ anvio/data/interactive/js/genomeview/navigation.js
@@ -0,0 +1,24 @@
+/**
+ * Javascript library for anvi'o genome view
+ *
+ *  Authors: Isaac Fink <iafink@uchicago.edu>
+ *           Matthew Klein <mtt.l.kln@gmail.com>
+ *           A. Murat Eren <a.murat.eren@gmail.com>
+ *
+ * Copyright 2021, The anvi'o project (http://anvio.org)
+ *
+ * Anvi'o is a free software. You can redistribute this program
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with anvi'o. If not, see <http://opensource.org/licenses/GPL-3.0>.
+ *
+ * @license GPL-3.0+ <http://opensource.org/licenses/GPL-3.0>
+ */
+
+/**
+ * File Overview : This file contains code related to the functionality of genomeview's scale element and other general navigation items. 
+ * Scale is great! Scale is dynamic! Scale is responsive. 
+ */
