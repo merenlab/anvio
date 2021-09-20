7e2b80f29e2e683425fd22d83c997403ce5d264d
mschechter <mschechter.lb@gmail.com>
2021-09-20 09:58:38 -0500

    file names matching

--- anvio/workflows/scg_phylo/__init__.py
+++ anvio/workflows/scg_phylo/__init__.py
@@ -217,6 +217,11 @@ class SCGPhylogeneticsWorkflow(WorkflowSuperClass):
             target_file = os.path.join(self.dirs_dict['RIBOSOMAL_PROTEIN_FASTAS'], ribosomal_protein_name, tail_path)
             target_files.append(target_file)
 
+
+            tail_path = "%s_external_gene_calls_all_renamed.done" % (ribosomal_protein_name)
+            target_file = os.path.join(self.dirs_dict['RIBOSOMAL_PROTEIN_FASTAS'], ribosomal_protein_name, tail_path)
+            target_files.append(target_file)
+
             # The FINAL trees :)
             # For iq-tree
             if self.run_iqtree == True:
--- anvio/workflows/scg_phylo/scripts/rename_external_gene_calls_file.py
+++ anvio/workflows/scg_phylo/scripts/rename_external_gene_calls_file.py
@@ -6,45 +6,38 @@ import glob
 import os.path
 
 # This script replaces the contig name of the external_gene_calls
-# file with the reformated name. 
+# file with the reformated name.
 
-# Import tables 
-#----------------------------
-reformat_report = pd.read_csv(snakemake.input.reformat_file_all, \
-                  sep="\t", \
-                  index_col=False, \
-                  names=["new_header", "header"])
+# This script reformats the names from the external_gene_calls.txt file (which comes from anvi-get-sequences-for-gene-calls) with 
+# the names of sequences from anvi-estimate-scg-taxonomy
 
+# Import tables
+#--------------
 external_gene_calls = pd.read_csv(snakemake.input.external_gene_calls_all, \
-                  delim_whitespace=True, \
-                  index_col=False)
+                                  delim_whitespace=True, \
+                                  index_col=False)
+
 
 headers = pd.read_csv(snakemake.input.headers, \
-                  sep="\t", \
-                  index_col=False,
-                  names=["contig"])
+                      sep="\t", \
+                      index_col=False,
+                      names=["contig"])
 
 # Make new columns
 #-----------------
-reformat_report["sample_contig"] = reformat_report['header'].str.split("contig:|\|gene_callers_id:", expand=True)[1].astype(str)
-reformat_report["gene_callers_id"] = reformat_report['header'].str.split("gene_callers_id:|\|start:", expand=True)[1].astype(str)
-reformat_report["sample_name"] = reformat_report['header'].str.split("bin_id:|\|source:", expand=True)[1].astype(str)
-reformat_report['sample_name'] = reformat_report['sample_name'].map(lambda x: x.rstrip('-contigs'))
-reformat_report["contig"] =  reformat_report['sample_name'] + "_" + reformat_report['sample_contig'] + "_" + reformat_report['gene_callers_id'].astype(str)
-reformat_report = reformat_report.drop(columns=['gene_callers_id'])
+external_gene_calls[['sample_name', 'contig_number', 'gene_callers_id']] = external_gene_calls['contig'].str.split('_',expand=True)
+external_gene_calls['reference_protein_name'] = snakemake.wildcards.reference_protein_name
+external_gene_calls['contig'] = external_gene_calls['sample_name'] + '_' + external_gene_calls['reference_protein_name'] + '_' + external_gene_calls['gene_callers_id']
 
-# Join reformat_report with external_gene_calls
-#----------------------------------------------
-external_gene_calls_filtered = reformat_report.merge(external_gene_calls, on="contig", how="inner").drop(columns=['header', 'sample_name', 'contig']).rename(columns={'new_header': 'contig'})
-external_gene_calls_filtered_ed = external_gene_calls_filtered.merge(headers, on="contig", how="inner")
-external_gene_calls_filtered_final = external_gene_calls_filtered_ed[["gene_callers_id", "contig", "start", "stop", "direction", "partial", "call_type", "source", "version", "aa_sequence"]]
-external_gene_calls_filtered_final['gene_callers_id'] = external_gene_calls_filtered_ed.index
+# # Join reformat_report with external_gene_calls
+#------------------------------------------------
+external_gene_calls_filtered = external_gene_calls.merge(headers, on="contig", how="inner").drop(columns=['reference_protein_name', 'sample_name', 'contig_number', 'reference_protein_name'])
 
 
-# Write file
-#-----------
-external_gene_calls_filtered_final.to_csv(snakemake.output.external_gene_calls_renamed, \
-           sep="\t", \
-           index=False, \
-           header=True, \
-           na_rep="NA")
+# # Write file
+#-------------
+external_gene_calls_filtered.to_csv(snakemake.output.external_gene_calls_renamed, \
+                                    sep="\t", \
+                                    index=False, \
+                                    header=True, \
+                                    na_rep="NA")
