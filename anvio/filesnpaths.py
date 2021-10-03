7a2de4807df4718d24280fcdc62b6f1e319bcd89
A. Murat Eren <a.murat.eren@gmail.com>
2021-10-02 21:55:22 -0500

    option to not raise for is file TAB delim

--- anvio/filesnpaths.py
+++ anvio/filesnpaths.py
@@ -184,7 +184,7 @@ def is_file_empty(file_path):
     return os.stat(file_path).st_size == 0
 
 
-def is_file_tab_delimited(file_path, separator='\t', expected_number_of_fields=None):
+def is_file_tab_delimited(file_path, separator='\t', expected_number_of_fields=None, dont_raise=False):
     is_file_exists(file_path)
     f = open(file_path, 'rU')
 
@@ -196,22 +196,34 @@ def is_file_tab_delimited(file_path, separator='\t', expected_number_of_fields=N
             else:
                 break
     except UnicodeDecodeError:
-        raise FilesNPathsError("The probability that `%s` is a tab-delimited file is zero." % file_path)
+        if dont_raise:
+            return False
+        else:
+            raise FilesNPathsError("The probability that `%s` is a tab-delimited file is zero." % file_path)
 
     if len(line.split(separator)) == 1 and expected_number_of_fields != 1:
-        raise FilesNPathsError("File '%s' does not seem to have TAB characters. "
-                               "Did you export this file on MAC using EXCEL? :(" % file_path)
+        if dont_raise:
+            return False
+        else:
+            raise FilesNPathsError("File '%s' does not seem to have TAB characters. "
+                                   "Did you export this file on MAC using EXCEL? :(" % file_path)
 
     f.seek(0)
     num_fields_set = set([len(line.split(separator)) for line in f.readlines()])
     if len(num_fields_set) != 1:
-        raise FilesNPathsError("Not all lines in the file '%s' have equal number of fields..." % file_path)
+        if dont_raise:
+            return False
+        else:
+            raise FilesNPathsError("Not all lines in the file '%s' have equal number of fields..." % file_path)
 
     if expected_number_of_fields:
         num_fields_in_file = list(num_fields_set)[0]
         if num_fields_in_file != expected_number_of_fields:
-            raise FilesNPathsError("The expected number of columns for '%s' is %d. Yet, it has %d "
-                                   "of them :/" % (file_path, expected_number_of_fields, num_fields_in_file))
+            if dont_raise:
+                return False
+            else:
+                raise FilesNPathsError("The expected number of columns for '%s' is %d. Yet, it has %d "
+                                       "of them :/" % (file_path, expected_number_of_fields, num_fields_in_file))
 
     f.close()
     return True
