78da6911a5d40981bef979621ce8e932714773af
Matthew Klein <mtt.l.kln@gmail.com>
2021-10-25 12:34:33 +0000

    deprecate processes related to news tab

--- anvio/data/interactive/js/news.js
+++ anvio/data/interactive/js/news.js
@@ -22,25 +22,15 @@ function checkNews() {
                     unread_count++;
                 }
                 $('#modNewsBadger-inner').append('<div class="news-item"> \
-                                              <h1>' + ((hash_found) ? 'asdk' : '<span class="blue-dot">') + '</span>'+news_item['title']+'</h1> \
+                                              <h1>' + ((hash_found) ? '' : '<span class="blue-dot">') + '</span>'+news_item['title']+'</h1> \
                                               <span class="news-date">'+news_item['date']+'</span>'+renderMarkdown(news_item['content'])+'</div>')
             }
 
             if (unread_count > 0) {
                 $('#modNewsBadger').modal('show')
-                $('#toggle-panel-right-3').css('color', '#FF0000');
-                $('#toggle-panel-right-3').addClass('fading-button');
-
-		$('#toggle-panel-right-3').mouseover(function() {
-			if(!$('#news-panel').is(':visible')) {
-				$(this).addClass('toggle-panel-right-pos-3');
-                		toggleRightPanel('#news-panel');
-                		$('#toggle-panel-right-3-inner').html('&#9658;');
-                	}
-		});
             } else {
-		// remove upon starting interface in case 'last-seen-hash' cookie is not cleared
-	    	$('#news-mark-read').remove();
+		        // remove upon starting interface in case 'last-seen-hash' cookie is not cleared
+	    	    $('#news-mark-read').remove();
             }
         }
     });
@@ -48,9 +38,6 @@ function checkNews() {
 
 function newsMarkRead() {
     $('.blue-dot').remove();
-    $('#toggle-panel-right-3').css('color', '#000000');
-    $('#toggle-panel-right-3').removeClass('fading-button');
     $('#news-mark-read').remove();
-    $('#toggle-panel-right-3').off('mouseover');
     createCookie('last_seen_hash', md5($('.news-item > h1')[0].textContent), -1);
 }
