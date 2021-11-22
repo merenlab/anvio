aad184adaf08a203d91f86c5be30b6d26bf3ceac
mschechter <mschechter.lb@gmail.com>
2021-11-22 15:56:33 -0600

    using anvi-script-reformat-fasta names for binding for now

--- anvio/workflows/ecophylo/scripts/rename_external_gene_calls_file.py
+++ anvio/workflows/ecophylo/scripts/rename_external_gene_calls_file.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-# This script reformats the names from the external_gene_calls.txt file (which comes from anvi-get-sequences-for-gene-calls) with
-# the names of sequences from anvi-estimate-scg-taxonomy
+# This script will replace the external-gene-calls names with the simplified names from anvi-script-reformat-fasta
+# sample-HMM-number
 
 # Import tables
 #--------------
@@ -9,15 +9,26 @@ external_gene_calls = pd.read_csv(snakemake.input.external_gene_calls, \
                                   delim_whitespace=True, \
                                   index_col=False)
 
-# Make new columns
-#-----------------
-external_gene_calls[['name', 'contig_number', 'gene_callers_id']] = external_gene_calls['contig'].str.rsplit('_',2, expand=True)
-external_gene_calls['HMM'] = snakemake.wildcards.HMM
-external_gene_calls['sample_name'] = snakemake.wildcards.sample_name
-external_gene_calls['gene_callers_id_leading_zeros'] = external_gene_calls['gene_callers_id'].apply(lambda x: x.zfill(12))
-external_gene_calls['contig'] = external_gene_calls['sample_name'] + '_' + external_gene_calls['HMM'] + '_' + external_gene_calls['gene_callers_id_leading_zeros']
+reformat_report = pd.read_csv(snakemake.input.reformat_file, \
+                              sep="\t", \
+                              index_col=False, \
+                              names=["new_header", "header"])
 
-external_gene_calls = external_gene_calls[["gene_callers_id", "contig", "start", "stop", "direction", "partial", "call_type", "source", "version", "aa_sequence"]]
+# Parse input files
+#-----------------
+# Parse reformat_report to get gene-callers-ids
+reformat_report["gene_callers_id"] = reformat_report['header'].str.split("gene_callers_id:|\|start:", expand=True)[1].astype(str)
+
+# Parse external-gene-calls contig column to get gene-callers-ids
+external_gene_calls[['name', 'contig_number', 'gene_callers_id']] = external_gene_calls['contig'].str.rsplit('_',2, expand=True)
+
+# Join external-gene-calls with reformat-report on gene-callers-id
+#-----------------
+external_gene_calls = external_gene_calls.merge(reformat_report, on="gene_callers_id", how="inner")
+
+# Replace contig name from anvi-export-gene-calls with the simplified, new header from anvi-script-reformat-fasta
+external_gene_calls = external_gene_calls[["gene_callers_id", "new_header", "start", "stop", "direction", "partial", "call_type", "source", "version", "aa_sequence"]]
+external_gene_calls = external_gene_calls.rename(columns={'new_header': 'contig'})
 
 # Write file
 #-----------
