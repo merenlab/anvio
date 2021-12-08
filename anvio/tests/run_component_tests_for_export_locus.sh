f42fa05ebdfa3a3ee998a3df26957ef528e26e80
mschechter <mschechter.lb@gmail.com>
2021-12-07 18:08:53 -0600

    add a component test to keep track of edge cases

---
+++ anvio/tests/run_component_tests_for_export_locus.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+source 00.sh
+
+# Setup #############################
+SETUP_WITH_OUTPUT_DIR $1 $2
+#####################################
+
+INFO "Setting up the anvi-export-locus test directory"
+mkdir $output_dir/export_locus_test
+cp $files/data/genomes/bacteria/*.db                    $output_dir/export_locus_test
+cp $files/data/genomes/archaea/*.db                     $output_dir/export_locus_test
+cp $files/data/metagenomes/human_gut/IGD_SUBSET/*.db    $output_dir/export_locus_test
+cp $files/data/input_files/*.txt                        $output_dir/export_locus_test
+
+cd $output_dir/export_locus_test
+
+mkdir test
+
+INFO "Running anvi-export-locus in default-mode"
+anvi-export-locus -c CONTIGS.db --gene-caller-id 68 -n 7,7 -o test/ -O metagenome_68
+
+INFO "Running anvi-export-locus in flank-mode"
+anvi-export-locus -c CONTIGS.db --gene-caller-id 68,78 -o test/ -O metagenome_68_78 --flank-mode
