af6bf874889d0a8fec5e651a2580d708d6552c5c
Matthew Klein <mtt.l.kln@gmail.com>
2022-02-04 20:18:55 +0000

    refactor to single file to avoid ecm/import interop issues with mjs

--- anvio/data/interactive/lib/svg-crowbar/src/index.js
+++ anvio/data/interactive/lib/svg-crowbar/src/index.js
@@ -1,15 +1,159 @@
-import getSource from './inputProcessor'
-import download from './svg'
-import downloadPNG from './png'
-import {getFilename} from './util'
+const doctype =
+  '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
+const prefix = {
+  xmlns: 'http://www.w3.org/2000/xmlns/',
+  xlink: 'http://www.w3.org/1999/xlink',
+  svg: 'http://www.w3.org/2000/svg',
+}
+const REMOVE_TIMEOUT = 10
+const DEFAULT_FILENAME = 'untitled'
 
-const downloadSvg = (svgElement, filename, options) =>
-  download(getSource(svgElement, options), filename || getFilename(svgElement))
-export default downloadSvg
-const downloadPng = (svgElement, filename, options) =>
-  downloadPNG(
-    getSource(svgElement, options),
-    filename || getFilename(svgElement),
-    options?.downloadPNGOptions,
+const downloadSvg = (svgElement, filename, options) => download(getSource(svgElement, options), filename || getFilename(svgElement))
+
+function download(source, filename = DEFAULT_FILENAME) {
+  const url = URL.createObjectURL(new Blob([source.source], {type: 'text/xml'}))
+
+  commenceDownload(`${filename}.svg`, url, () => URL.revokeObjectURL(url))
+}
+
+function getFilename(source) {
+  if (!(source instanceof SVGElement)) {
+    throw new Error('SVG Element is required')
+  }
+
+  return (
+    source.getAttribute('id') ||
+    source.getAttribute('class') ||
+    document.title.replace(/[^a-z0-9]/gi, '-').toLowerCase() ||
+    DEFAULT_FILENAME
   )
-export {downloadSvg, downloadPng}
+}
+
+function commenceDownload(filename, imgdata, callback) {
+  const a = document.createElement('a')
+  document.body.appendChild(a)
+  a.setAttribute('class', 'svg-crowbar')
+  a.setAttribute('download', filename)
+  a.setAttribute('href', imgdata)
+  a.style.display = 'none'
+  a.click()
+
+  setTimeout(() => {
+    if (callback) {
+      callback()
+    }
+    document.body.removeChild(a)
+  }, REMOVE_TIMEOUT)
+}
+
+function getEmptySvgDeclarationComputed() {
+  let emptySvg = document.createElementNS(prefix.svg, 'svg')
+  document.body.appendChild(emptySvg)
+  emptySvg.style.all = 'initial'
+  const emptySvgDeclarationComputed = getComputedStyle(emptySvg)
+  document.body.removeChild(emptySvg)
+  emptySvg = null
+  return emptySvgDeclarationComputed
+}
+
+function getSource(svg, {css = 'inline'} = {}) {
+  if (!(svg instanceof SVGElement)) {
+    throw new Error('SVG element is required')
+  }
+
+  svg.setAttribute('version', '1.1')
+  // removing attributes so they aren't doubled up
+  svg.removeAttribute('xmlns')
+  svg.removeAttribute('xlink')
+
+  // These are needed for the svg
+  if (!svg.hasAttributeNS(prefix.xmlns, 'xmlns')) {
+    svg.setAttributeNS(prefix.xmlns, 'xmlns', prefix.svg)
+  }
+
+  if (!svg.hasAttributeNS(prefix.xmlns, 'xmlns:xlink')) {
+    svg.setAttributeNS(prefix.xmlns, 'xmlns:xlink', prefix.xlink)
+  }
+
+  // if (css === 'inline') {
+  //   setInlineStyles(svg, getEmptySvgDeclarationComputed())
+  // } else if (css === 'internal') {
+  //   setInternalStyles(svg)
+  // }
+
+  const source = new XMLSerializer().serializeToString(svg)
+  console.log(source);
+  const rect = svg.getBoundingClientRect()
+
+  const result = {
+    top: rect.top,
+    left: rect.left,
+    width: rect.width,
+    height: rect.height,
+    class: svg.getAttribute('class'),
+    id: svg.getAttribute('id'),
+    name: svg.getAttribute('name'),
+    childElementCount: svg.childElementCount,
+    source: doctype + source,
+  }
+
+  return result
+}
+
+function setInlineStyles(svg, emptySvgDeclarationComputed) {
+  function explicitlySetStyle(element) {
+    const cSSStyleDeclarationComputed = getComputedStyle(element)
+    let key
+    let value
+    let computedStyleStr = ''
+    for (let i = 0, len = cSSStyleDeclarationComputed.length; i < len; i++) {
+      key = cSSStyleDeclarationComputed[i]
+      value = cSSStyleDeclarationComputed.getPropertyValue(key)
+      if (value !== emptySvgDeclarationComputed.getPropertyValue(key)) {
+        computedStyleStr += `${key}:${value};`
+      }
+    }
+    element.setAttribute('style', computedStyleStr)
+  }
+  function traverse(obj) {
+    const tree = []
+    tree.push(obj)
+    visit(obj)
+    function visit(node) {
+      if (node && node.hasChildNodes()) {
+        let child = node.firstChild
+        while (child) {
+          if (child.nodeType === 1 && child.nodeName !== 'SCRIPT') {
+            tree.push(child)
+            visit(child)
+          }
+          child = child.nextSibling
+        }
+      }
+    }
+    return tree
+  }
+  // hardcode computed css styles inside svg
+  const allElements = traverse(svg)
+  let i = allElements.length
+  while (i--) {
+    explicitlySetStyle(allElements[i])
+  }
+}
+
+function setInternalStyles(svg) {
+  const style = document.createElement('style')
+  style.innerHTML = Array.from(document.styleSheets)
+    .filter(
+      (styleSheet) =>
+        // Prevent CORS errors
+        !styleSheet.href || styleSheet.href.startsWith(document.location.origin),
+    )
+    .map((styleSheet) =>
+      Array.from(styleSheet.cssRules)
+        .map((rule) => rule.cssText)
+        .join(' '),
+    )
+    .join(' ')
+  svg.prepend(style)
+}
--- anvio/data/interactive/lib/svg-crowbar/src/png.js
+++ anvio/data/interactive/lib/svg-crowbar/src/png.js
@@ -1,58 +1,58 @@
-import {DEFAULT_FILENAME} from './const'
-import {commenceDownload} from './util'
+// import {DEFAULT_FILENAME} from './const'
+// import {commenceDownload} from './util'
 
-export function _fixSource(source) {
-  return btoa(
-    unescape(
-      encodeURIComponent(source.replace(/[\u00A0-\u2666]/g, (c) => `&#${c.charCodeAt(0)};`)),
-    ),
-  )
-}
+// export function _fixSource(source) {
+//   return btoa(
+//     unescape(
+//       encodeURIComponent(source.replace(/[\u00A0-\u2666]/g, (c) => `&#${c.charCodeAt(0)};`)),
+//     ),
+//   )
+// }
 
-const DEFAULT_OPTIONS = {
-  debug: false,
-  fixSource: _fixSource,
-  scale: 1,
-}
+// const DEFAULT_OPTIONS = {
+//   debug: false,
+//   fixSource: _fixSource,
+//   scale: 1,
+// }
 
-function downloadPng(
-  source,
-  filename = DEFAULT_FILENAME,
-  {
-    debug = DEFAULT_OPTIONS.debug,
-    fixSource = DEFAULT_OPTIONS.fixSource,
-    scale = DEFAULT_OPTIONS.scale,
-  } = DEFAULT_OPTIONS,
-) {
-  const canvas = document.createElement('canvas')
-  const dpr = window.devicePixelRatio || 1
-  document.body.appendChild(canvas)
-  canvas.setAttribute('id', 'svg-image')
-  canvas.setAttribute('width', source.width * dpr * scale)
-  canvas.setAttribute('height', source.height * dpr * scale)
-  if (debug === false) {
-    canvas.style.display = 'none'
-  }
+// function downloadPng(
+//   source,
+//   filename = DEFAULT_FILENAME,
+//   {
+//     debug = DEFAULT_OPTIONS.debug,
+//     fixSource = DEFAULT_OPTIONS.fixSource,
+//     scale = DEFAULT_OPTIONS.scale,
+//   } = DEFAULT_OPTIONS,
+// ) {
+//   const canvas = document.createElement('canvas')
+//   const dpr = window.devicePixelRatio || 1
+//   document.body.appendChild(canvas)
+//   canvas.setAttribute('id', 'svg-image')
+//   canvas.setAttribute('width', source.width * dpr * scale)
+//   canvas.setAttribute('height', source.height * dpr * scale)
+//   if (debug === false) {
+//     canvas.style.display = 'none'
+//   }
 
-  const context = canvas.getContext('2d')
-  const imgsrc = `data:image/svg+xml;base64,${fixSource(source.source)}`
-  const image = new Image()
+//   const context = canvas.getContext('2d')
+//   const imgsrc = `data:image/svg+xml;base64,${fixSource(source.source)}`
+//   const image = new Image()
 
-  function onLoad() {
-    context.scale(dpr * scale, dpr * scale)
-    context.drawImage(image, 0, 0)
-    const canvasdata = canvas.toDataURL('image/png')
+//   function onLoad() {
+//     context.scale(dpr * scale, dpr * scale)
+//     context.drawImage(image, 0, 0)
+//     const canvasdata = canvas.toDataURL('image/png')
 
-    if (debug === false) {
-      commenceDownload(`${filename}.png`, canvasdata, () => document.body.removeChild(canvas))
-    }
-  }
+//     if (debug === false) {
+//       commenceDownload(`${filename}.png`, canvasdata, () => document.body.removeChild(canvas))
+//     }
+//   }
 
-  image.onload = onLoad
-  image.src = imgsrc
-  if (debug === true) {
-    document.body.appendChild(image)
-  }
-}
+//   image.onload = onLoad
+//   image.src = imgsrc
+//   if (debug === true) {
+//     document.body.appendChild(image)
+//   }
+// }
 
-export default downloadPng
+// export default downloadPng
