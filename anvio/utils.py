33dea531d3a955d2427d5cdba6b31abc83d56abb
Sam Miller <samuelmiller10@gmail.com>
2022-03-26 23:21:06 -0500

    function for pretty time formatting

--- anvio/utils.py
+++ anvio/utils.py
@@ -15,6 +15,7 @@ import shutil
 import smtplib
 import tarfile
 import hashlib
+import calendar
 import textwrap
 import linecache
 import webbrowser
@@ -1683,6 +1684,20 @@ def get_time_to_date(local_time, fmt='%Y-%m-%d %H:%M:%S'):
         raise ConfigError("utils::get_time_to_date is called with bad local_time.")
 
     return time.strftime(fmt, time.localtime(local_time))
+
+
+def parse_epoch_time(epoch_time):
+    """Convert the epoch time (returned by `time.time()`) into pretty strings."""
+    try:
+        epoch_time = float(epoch_time)
+    except ValueError:
+        raise ConfigError("utils::parse_epoch_time was called with a bad epoch_time.")
+
+    ti = time.gmtime(epoch_time)
+    hour_min_sec = f'{"%02d" % ti.tm_hour}:{"%02d" % ti.tm_min}:{"%02d" % ti.tm_sec}'
+    month_day_year = f'{calendar.month_name[ti.tm_mon]} {ti.tm_mday}, {ti.tm_year}'
+
+    return hour_min_sec, month_day_year
 
 
 def compare_times(calls, as_matrix=False, iterations_per_call=1):
