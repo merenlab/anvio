b4071f1232c54c3a50bd7f818127a10ea20ab223
A. Murat Eren <a.murat.eren@gmail.com>
2022-04-11 12:37:05 +0200

    testing db migrations. this will save lives

---
+++ anvio/tests/run_migration_tests_for_ancient_anvio_databases.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+source 00.sh
+set -e
+
+# Setup #############################
+SETUP_WITH_OUTPUT_DIR $1 $2
+#####################################
+
+cd $output_dir
+
+#########################################################################################
+
+#TEST="POUCHITIS_METAGENOMES_FROM_2015"
+#mkdir $TEST && cd $TEST
+#INFO "[$TEST] Downloading the data pack"
+#curl -L https://ndownloader.figshare.com/files/6035580 -o P214-MERGED.tar.gz
+#INFO "[$TEST] Unpacking"
+#tar -zxvf P214-MERGED.tar.gz && cd P214-MERGED/
+#INFO "[$TEST] Migrating databases"
+#anvi-migrate *db --migrate-dbs-safely
+#INFO "[$TEST] Downloading state"
+#curl -L http://merenlab.org/files/P-214-state.json -o P-214-state.json
+#INFO "[$TEST] Importing state"
+#anvi-import-state -p PROFILE.db -n default -s P-214-state.json
+#INFO "[$TEST] Running the interactive interface (--dry)"
+#anvi-interactive -p PROFILE.db -c CONTIGS.db --split-hmm-layers --dry
+#INFO "[$TEST] Setting up SCG taxonomy databases"
+#anvi-setup-scg-taxonomy --redo-databases --num-threads 4
+#INFO "[$TEST] Running SCG taxonomy"
+#anvi-run-scg-taxonomy -c CONTIGS.db --num-threads 4
+#INFO "[$TEST] Estimating SCG taxonomy"
+#anvi-estimate-scg-taxonomy -p PROFILE.db -c CONTIGS.db -C FINAL
+#INFO "[$TEST] Done!"
+#cd ..
+
+#########################################################################################
+
+TEST="A_TARA_MAG_FROM_2017"
+mkdir $TEST && cd $TEST
+INFO "[$TEST] Downloading the data pack"
+curl -L https://ndownloader.figshare.com/files/8248433 -o TARA_ANW_MAG_00006.tar.gz
+INFO "[$TEST] Unpacking"
+tar -zxvf TARA_ANW_MAG_00006.tar.gz && cd TARA_ANW_MAG_00006
+gzip -d AUXILIARY-DATA.h5.gz
+INFO "[$TEST] Migrating databases"
+anvi-migrate PROFILE.db CONTIGS.db --migrate-dbs-safely
+INFO "[$TEST] Running the interactive interface (--dry)"
+anvi-interactive -p PROFILE.db -c CONTIGS.db --dry
+INFO "[$TEST] Setting up SCG taxonomy databases"
+anvi-setup-scg-taxonomy --redo-databases --num-threads 4
+INFO "[$TEST] Running HMMs"
+anvi-run-hmms -c CONTIGS.db -T 4
+INFO "[$TEST] Estimating genome completeness"
+anvi-estimate-genome-completeness -c CONTIGS.db
+INFO "[$TEST] Running SCG taxonomy"
+anvi-run-scg-taxonomy -c CONTIGS.db --num-threads 4
+INFO "[$TEST] Estimating SCG taxonomy"
+anvi-estimate-scg-taxonomy -c CONTIGS.db
+INFO "[$TEST] Done!"
+cd ..
+
+#########################################################################################
