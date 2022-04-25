14fe07aa0c490fe3b54a2cc8f86c623cff87053e
Sam Miller <samuelmiller10@gmail.com>
2022-04-25 17:00:29 -0500

    add some metadata back for associated genomic contigs db, profile db, and bin collection

--- anvio/trnaseq.py
+++ anvio/trnaseq.py
@@ -6514,6 +6514,10 @@ class DatabaseMerger(object):
         set_meta_value('scg_taxonomy_database_version', None)
         set_meta_value('trna_taxonomy_was_run', False)
         set_meta_value('trna_taxonomy_database_version', None)
+        set_meta_value('genomic_contigs_db_project_name', None)
+        set_meta_value('genomic_contigs_db_hash', None)
+        set_meta_value('genomic_profile_db_hash', None)
+        set_meta_value('genomic_collection_name', None)
         set_meta_value('creation_date', time.time())
 
         insert_many = contigs_db.db.insert_many
