694c2b049bc00c9b2ce5dafe274375429b47fb72
A. Murat Eren <a.murat.eren@gmail.com>
2022-04-26 21:03:51 +0200

    --no-progress flag  for component tests.
    --no-progress is a community flag in anvi'o that supresses ephemeral
    messages telling the user what is going on.
    
    this addition will reduce the significant number of lines
    in files when test output is directed into a file. the current
    number of lines without `--no-progress` reaches over 30,000 lines :/

--- anvio/tests/run_component_tests_for_pangenomics.sh
+++ anvio/tests/run_component_tests_for_pangenomics.sh
@@ -17,23 +17,50 @@ cp $files/mock_data_for_pangenomics/group-information.txt     $output_dir/
 cd $output_dir/
 
 INFO "Generating an anvi'o genomes storage"
-anvi-gen-genomes-storage -e external-genomes.txt -o TEST-GENOMES.db
+anvi-gen-genomes-storage -e external-genomes.txt \
+                         -o TEST-GENOMES.db \
+                         --no-progress
 
 INFO "Running the pangenome anaysis with default parameters"
-anvi-pan-genome -g TEST-GENOMES.db -o TEST/ -n TEST --use-ncbi-blast --description example_description.md
+anvi-pan-genome -g TEST-GENOMES.db \
+                -o TEST/ \
+                -n TEST \
+                --use-ncbi-blast \
+                --description example_description.md \
+                --no-progress
 
 INFO "Running ANI on genomes and storing results in the PAN database"
-anvi-compute-genome-similarity -e external-genomes.txt --program pyANI -o ANI_TEST --log-file ANI_LOG.txt -p TEST/TEST-PAN.db
+anvi-compute-genome-similarity -e external-genomes.txt \
+                               --program pyANI \
+                               -o ANI_TEST \
+                               --log-file ANI_LOG.txt \
+                               -p TEST/TEST-PAN.db \
+                               --no-progress
 
 INFO "Running the pangenome analysis again utilizing previous search results"
-anvi-pan-genome -g TEST-GENOMES.db -o TEST/ -n ANOTHER_TEST --use-ncbi-blast --min-occurrence 2 --description example_description.md
+anvi-pan-genome -g TEST-GENOMES.db \
+                -o TEST/ \
+                -n ANOTHER_TEST \
+                --use-ncbi-blast \
+                --min-occurrence 2 \
+                --description example_description.md \
+                --no-progress
 
 INFO "Importing collections of gene clusters"
-anvi-import-collection -p TEST/TEST-PAN.db -C test_collection example-gene-clusters-collection.txt
-anvi-import-collection -p TEST/TEST-PAN.db -C collection_for_phylogenomics scg-gene-clusters-for-phylogenomics.txt
+anvi-import-collection -p TEST/TEST-PAN.db \
+                       -C test_collection example-gene-clusters-collection.txt \
+                       --no-progress
+
+anvi-import-collection -p TEST/TEST-PAN.db \
+                       -C collection_for_phylogenomics scg-gene-clusters-for-phylogenomics.txt \
+                       --no-progress
 
 INFO "Exporting the collection 'test_collection'"
-anvi-export-collection -p TEST/TEST-PAN.db -C test_collection -O exported_collection --include-unbinned
+anvi-export-collection -p TEST/TEST-PAN.db \
+                       -C test_collection \
+                       -O exported_collection \
+                       --include-unbinned \
+                       --no-progress
 
 INFO "List available aligners for aligning sequences in gene clusters"
 anvi-get-sequences-for-gene-clusters --list-aligners
@@ -43,7 +70,8 @@ anvi-get-sequences-for-gene-clusters -p TEST/TEST-PAN.db \
                                      -g TEST-GENOMES.db \
                                      -C test_collection \
                                      -b GENE_CLUSTER_BIN_1_CORE \
-                                     -o aligned_gene_sequences_in_GENE_CLUSTER_BIN_1_CORE_AA.fa
+                                     -o aligned_gene_sequences_in_GENE_CLUSTER_BIN_1_CORE_AA.fa \
+                                     --no-progress
 
 mkdir SPLIT_GENE_CLUSTER_FILES
 INFO "Exporting aligned amino acid sequences for some gene clusters as individual files"
@@ -52,7 +80,8 @@ anvi-get-sequences-for-gene-clusters -p TEST/TEST-PAN.db \
                                      -C test_collection \
                                      -b GENE_CLUSTER_BIN_1_CORE \
                                      --split-output-per-gene-cluster \
-                                     -O SPLIT_GENE_CLUSTER_FILES/MY_PROJECT
+                                     -O SPLIT_GENE_CLUSTER_FILES/MY_PROJECT \
+                                     --no-progress
 
 INFO "Exporting aligned DNA sequences for some gene clusters"
 anvi-get-sequences-for-gene-clusters -p TEST/TEST-PAN.db \
@@ -60,7 +89,8 @@ anvi-get-sequences-for-gene-clusters -p TEST/TEST-PAN.db \
                                      -C test_collection \
                                      -b GENE_CLUSTER_BIN_1_CORE \
                                      -o aligned_gene_sequences_in_GENE_CLUSTER_BIN_1_CORE_DNA.fa \
-                                     --report-DNA-sequences
+                                     --report-DNA-sequences \
+                                     --no-progress
 
 INFO "First five line from the AA output"
 head -n 5 aligned_gene_sequences_in_GENE_CLUSTER_BIN_1_CORE_AA.fa
@@ -71,7 +101,8 @@ head -n 5 aligned_gene_sequences_in_GENE_CLUSTER_BIN_1_CORE_DNA.fa
 INFO "Importing group information as misc data for layers"
 anvi-import-misc-data -p TEST/TEST-PAN.db \
                       -t layers \
-                      group-information.txt
+                      group-information.txt \
+                      --no-progress
 
 INFO "Estimating enriched functions per pan group"
 anvi-compute-functional-enrichment-in-pan -p TEST/TEST-PAN.db \
@@ -79,7 +110,8 @@ anvi-compute-functional-enrichment-in-pan -p TEST/TEST-PAN.db \
                                           --category group \
                                           --annotation-source COG20_FUNCTION \
                                           -o functions-enrichment.txt \
-                                          -F functional-occurence.txt
+                                          -F functional-occurence.txt \
+                                          --no-progress
 SHOW_FILE functions-enrichment.txt
 SHOW_FILE functional-occurence.txt
 
@@ -89,16 +121,29 @@ anvi-get-sequences-for-gene-clusters -p TEST/TEST-PAN.db \
                                      -C collection_for_phylogenomics \
                                      -b SCGs \
                                      -o SOME_VARIABLE_SCGs.fa \
-                                     --concatenate-gene-clusters
+                                     --concatenate-gene-clusters \
+                                     --no-progress
 
 INFO "Summarizing the pan, using the test collection (in quick mode)"
-anvi-summarize -p TEST/TEST-PAN.db -g TEST-GENOMES.db -C test_collection -o TEST_SUMMARY_QUICK --quick
+anvi-summarize -p TEST/TEST-PAN.db \
+               -g TEST-GENOMES.db \
+               -C test_collection \
+               -o TEST_SUMMARY_QUICK \
+               --quick \
+               --no-progress
 
 INFO "Summarizing the pan, using the test collection"
-anvi-summarize -p TEST/TEST-PAN.db -g TEST-GENOMES.db -C test_collection -o TEST_SUMMARY
+anvi-summarize -p TEST/TEST-PAN.db \
+               -g TEST-GENOMES.db \
+               -C test_collection \
+               -o TEST_SUMMARY \
+               --no-progress
 
 INFO "Splitting bins in the pan genome into smaller, self-contained pan databases"
-anvi-split -p TEST/TEST-PAN.db -g TEST-GENOMES.db -C test_collection -o TEST_SPLIT_PAN
+anvi-split -p TEST/TEST-PAN.db \
+           -g TEST-GENOMES.db \
+           -C test_collection \
+           -o TEST_SPLIT_PAN
 
 INFO "Resulting split pans"
 ls -l TEST_SPLIT_PAN/*/*db
@@ -107,13 +152,15 @@ INFO "Taking a look at the make up one of the split pans"
 anvi-db-info TEST_SPLIT_PAN/GENE_CLUSTER_BIN_1_CORE/PAN.db
 
 INFO "Listing collections available"
-anvi-show-collections-and-bins -p TEST/TEST-PAN.db
+anvi-show-collections-and-bins -p TEST/TEST-PAN.db \
+                               --no-progress
 
 INFO "Computing homogeneity for a single gene cluster"
 anvi-compute-gene-cluster-homogeneity -p TEST/TEST-PAN.db \
                                       -g TEST-GENOMES.db \
                                       --gene-cluster-id GC_00000001 \
-                                      -o gene_cluster_homogeneity_results.txt
+                                      -o gene_cluster_homogeneity_results.txt \
+                                      --no-progress
 SHOW_FILE gene_cluster_homogeneity_results.txt
 
 INFO "Computing homogeneity for a list of gene clusters"
@@ -121,7 +168,8 @@ echo -e "GC_00000001\nGC_00000003" > gene_clusters_for_homogeneity.txt
 anvi-compute-gene-cluster-homogeneity -p TEST/TEST-PAN.db \
                                       -g TEST-GENOMES.db \
                                       --gene-cluster-ids gene_clusters_for_homogeneity.txt \
-                                      -o gene_cluster_homogeneity_results.txt
+                                      -o gene_cluster_homogeneity_results.txt \
+                                      --no-progress
 SHOW_FILE gene_cluster_homogeneity_results.txt
 
 INFO "Computing homogeneity for gene clusters in a bin"
@@ -130,7 +178,8 @@ anvi-compute-gene-cluster-homogeneity -p TEST/TEST-PAN.db \
                                       -C test_collection \
                                       -b GENE_CLUSTER_BIN_2 \
                                       -o gene_cluster_homogeneity_results.txt \
-                                      --num-threads 2
+                                      --num-threads 2 \
+                                      --no-progress
 SHOW_FILE gene_cluster_homogeneity_results.txt
 
 INFO "Importing the default state for pretty outputs"
@@ -141,10 +190,12 @@ INFO "Displaying the initial pangenome analysis results"
 anvi-display-pan -p TEST/TEST-PAN.db \
                  -g TEST-GENOMES.db \
                  --title "A mock pangenome analysis" \
+                 --no-progress \
                  $dry_run_controller
 
 INFO "Displaying the second pangenome analysis results"
 anvi-display-pan -p TEST/ANOTHER_TEST-PAN.db \
                  -g TEST-GENOMES.db \
                  --title "A mock pangenome analysis (with --min-occurrence 2)" \
+                 --no-progress \
                  $dry_run_controller
