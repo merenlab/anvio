dd34390262a7cce0ade01bea126e7954a20ee6c6
A. Murat Eren <a.murat.eren@gmail.com>
2022-07-12 16:14:57 +0200

    removing `--sensitive` needs v+1 for JSON migrat.

--- anvio/tables/__init__.py
+++ anvio/tables/__init__.py
@@ -22,7 +22,7 @@ auxiliary_data_version = "2"
 structure_db_version = "2"
 genomes_storage_vesion = "7"
 trnaseq_db_version = "2"
-workflow_config_version = "2"
+workflow_config_version = "3"
 metabolic_modules_db_version = "4"
 
 versions_for_db_types = {'contigs': contigs_db_version,
