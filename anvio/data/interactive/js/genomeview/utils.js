ece4928182e9cfdcb7a79a726c7ca3b9757c6843
Matthew Klein <mtt.l.kln@gmail.com>
2022-07-22 19:58:13 +0000

    fix for fix

--- anvio/data/interactive/js/genomeview/utils.js
+++ anvio/data/interactive/js/genomeview/utils.js
@@ -168,7 +168,7 @@ function calcXBounds() {
  *  @returns array of functional annotation types from table in `genomeData`
  */
 function getFunctionalAnnotations() {
-  return Object.keys(genomeData?.genomes?.[0]?.[1]?.genes?.functions[0]);
+  return Object?.keys(genomeData?.genomes?.[0]?.[1]?.genes?.functions[0]);
 }
 
 /*
