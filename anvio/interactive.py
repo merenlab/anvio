151fe315a48d51bcf4f19f625a5c1a18dbf12dd7
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2022-09-13 10:12:05 +0200

    a check on missing functionality

--- anvio/interactive.py
+++ anvio/interactive.py
@@ -143,6 +143,14 @@ class Interactive(ProfileSuperclass, PanSuperclass, ContigsSuperclass):
                               "list of parameters and flags. Either of these would have made things much less confusing "
                               "here :(")
 
+        if self.annotation_source_for_per_split_summary and self.collection_name:
+            raise ConfigError("Unfortunately the flag `--annotation-source-for-per-split-summary` is not compatible with the collection "
+                              "mode :/ This is because while this function prepares all the function data for a split-level summary of "
+                              "functions, the splits of the collection mode are not actual splits, but bins. Being able to use this "
+                              "flag would require additional logic in collection mode code to consolidate all information in splits in "
+                              "a single bin to 're-summarize' data to the level of individual bins, which is currently not in place. "
+                              "Apologies for the inconvenience.")
+
         # make sure early on that both the distance and linkage is OK.
         clustering.is_distance_and_linkage_compatible(self.distance, self.linkage)
 
