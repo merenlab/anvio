b35a11e7e43ebdbbd6a6a1368492ba0ba75a2e1b
Iva Veseli <iva.veseli@gmail.com>
2022-10-10 16:47:09 +0200

    fix error output

--- anvio/kegg.py
+++ anvio/kegg.py
@@ -1237,8 +1237,7 @@ class KeggSetup(KeggContext):
         if not os.path.exists(self.kegg_modules_db_path):
             raise ConfigError(f"Wait a second. We understand that you are setting up user-defined metabolism data, but "
                               f"unfortunately you need to FIRST have KEGG data set up on your computer. Why, you ask? "
-                              f"Well, one reason is that we always use KEGG data first when estimating metabolism. And "
-                              f"the other reason is that we need to make sure none of your module names overlap with those "
+                              f"Well, we need to make sure none of your module names overlap with those "
                               f"in the KEGG MODULES database. Long story short, we looked for KEGG data at "
                               f"{self.kegg_modules_db_path} but we couldn't find it. If this is the wrong place for us to be "
                               f"looking, please run this program again and use the --kegg-data-dir parameter to tell us where "
