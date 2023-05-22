5ed1c9ab8f32052158bf07ccfd15504092bd138d
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2023-05-22 11:08:55 +0200

    typos :/

--- anvio/__init__.py
+++ anvio/__init__.py
@@ -511,7 +511,7 @@ D = {
              'type': int,
              'metavar': 'NUM NUCLEOTIDES',
              'choices': range(0, 51),
-             'help': "By default, anvi'o will report variable nucletodies and their alelle frequencies from any nucleotide ."
+             'help': "By default, anvi'o will report variable nucleotides and their allele frequencies from any nucleotide ."
                      "position in a given short read found in the BAM file. Although, there are some applications where "
                      "the observed variation in short reads depend on the location of the nucleotide positions in the read. "
                      "For instance, in ancient DNA sequencing, the start and the end of short reads are often suffer from "
@@ -519,7 +519,7 @@ D = {
                      "reference. This parameter enables the user to ask anvi'o to ignore a few number of nucleotides from "
                      "the beginning and the end of any given short read. For instance, if you provide `5` as a parameter, "
                      "the mismatches of a given read to the reference at the first and the last 5 nucleotides will not be "
-                     "reported in the varaibility table. The coverage will not be impacted by the use of this parameter -- "
+                     "reported in the variability table. The coverage will not be impacted by the use of this parameter -- "
                      "only what is reported as variants, decreasing the impact of noise in specific applications."}
                 ),
     'drop-previous-annotations': (
