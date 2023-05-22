6b4a6a3f8923301d5c0f125254ee2e7e08337f5a
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2023-05-22 11:04:56 +0200

    'num_nts_to_ignore_from_short_read_ends'
    a new parameter that enables anvi'o to ignore a number of nucleotides from the extremities of short reads from calculations of single-nucleotide variants.

--- anvio/contigops.py
+++ anvio/contigops.py
@@ -177,7 +177,8 @@ class Split:
 
 class Auxiliary:
     def __init__(self, split, min_coverage_for_variability=10, report_variability_full=False,
-                 profile_SCVs=False, skip_INDEL_profiling=False, skip_SNV_profiling=False, min_percent_identity=None):
+                 profile_SCVs=False, skip_INDEL_profiling=False, skip_SNV_profiling=False,
+                 min_percent_identity=None, num_nts_to_ignore_from_short_read_ends=0):
 
         if anvio.DEBUG:
             self.run = terminal.Run()
@@ -190,6 +191,7 @@ class Auxiliary:
         self.profile_SCVs = profile_SCVs
         self.skip_INDEL_profiling = skip_INDEL_profiling
         self.report_variability_full = report_variability_full
+        self.num_nts_to_ignore_from_short_read_ends = num_nts_to_ignore_from_short_read_ends
 
         # used during array processing
         self.nt_to_array_index = {nt: i for i, nt in enumerate(constants.nucleotides)}
@@ -482,6 +484,14 @@ class Auxiliary:
         read_count = 0
         for read in read_iterator(self.split.parent, self.split.start, self.split.end, **kwargs):
             aligned_sequence_as_ord, reference_positions = read.get_aligned_sequence_and_reference_positions()
+
+            # if the user is asking some nucleotides to be excluded from the calculation of
+            # single-nucleotide variants due to DNA damage or other reasons, don't take them
+            # into consideration:
+            if self.num_nts_to_ignore_from_short_read_ends > 0:
+                aligned_sequence_as_ord = aligned_sequence_as_ord[self.num_nts_to_ignore_from_short_read_ends:-self.num_nts_to_ignore_from_short_read_ends]
+                reference_positions = reference_positions[self.num_nts_to_ignore_from_short_read_ends:-self.num_nts_to_ignore_from_short_read_ends]
+
             aligned_sequence_as_index = utils.nt_seq_to_nt_num_array(aligned_sequence_as_ord, is_ord=True)
             reference_positions_in_split = reference_positions - self.split.start
 
