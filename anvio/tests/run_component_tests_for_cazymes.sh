d88bc417eacce36a060c28904bb6ff85ee812c09
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2023-06-07 12:12:13 +0200

    much better

--- anvio/tests/run_component_tests_for_cazymes.sh
+++ anvio/tests/run_component_tests_for_cazymes.sh
@@ -6,16 +6,13 @@ SETUP_WITH_OUTPUT_DIR $1 $2
 #####################################
 
 INFO "Setting up the anvi-export-locus test directory"
-mkdir $output_dir/cazyme_test
-cp $files/data/genomes/bacteria/*.db $output_dir/cazyme_test
-
-cd $output_dir/cazyme_test
+cp $files/data/genomes/bacteria/*.db $output_dir
 
 INFO "Running anvi-run-cazymes help menu"
 anvi-run-cazymes -h
 
 INFO "Running anvi-run-cazymes"
-anvi-run-cazymes -c B_thetaiotamicron_VPI-5482.db
+anvi-run-cazymes -c $output_dir/B_thetaiotamicron_VPI-5482.db
 
 INFO "Running anvi-run-cazymes with --noise-cutoff-terms"
-anvi-run-cazymes -c B_thetaiotamicron_VPI-5482.db --noise-cutoff-terms "-E 1e-12"
+anvi-run-cazymes -c $output_dir/B_thetaiotamicron_VPI-5482.db --noise-cutoff-terms "-E 1e-12"
