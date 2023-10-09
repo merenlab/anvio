0db1fe5c1854b129b1148d34643374c5c664a4e3
Sam Miller <samuelmiller10@gmail.com>
2023-10-09 12:38:30 -0500

    metavars expected loading pan db

--- anvio/dbops.py
+++ anvio/dbops.py
@@ -3983,15 +3983,17 @@ class PanDatabase:
             return
 
         self.meta = dbi(self.db_path, expecting=self.db_type).get_self_table()
-
-        for key in ['num_genomes', 'gene_cluster_min_occurrence', 'use_ncbi_blast', 'exclude_partial_gene_calls', \
-                    'num_gene_clusters', 'num_genes_in_gene_clusters', 'gene_alignments_computed', 'items_ordered']:
+        for key in ['num_genomes', 'gene_cluster_min_occurrence', 'use_ncbi_blast', 'exclude_partial_gene_calls',
+                    'num_gene_clusters', 'num_genes_in_gene_clusters', 'gene_alignments_computed', 'items_ordered',
+                    'reaction_network_ko_annotations_hash', 'reaction_network_kegg_database_release',
+                    'reaction_network_modelseed_database_sha']:
             try:
                 self.meta[key] = int(self.meta[key])
             except:
                 pass
 
-        for key in ['min_percent_identity', 'minbit', 'mcl_inflation']:
+        for key in ['min_percent_identity', 'minbit', 'mcl_inflation',
+                    'reaction_network_consensus_threshold', 'reaction_network_discard_ties']:
             try:
                 self.meta[key] = float(self.meta[key])
             except:
