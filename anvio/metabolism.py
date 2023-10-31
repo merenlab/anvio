8d7e8771be673fa2202576453fd8ad7c1acc6c2b
Iva Veseli <iva.veseli@gmail.com>
2023-10-31 14:55:32 +0100

    initial sanity checks for pathway data

--- anvio/metabolism.py
+++ anvio/metabolism.py
@@ -21,6 +21,14 @@ progress = terminal.Progress()
 run_quiet = terminal.Run(log_file_path=None, verbose=False)
 progress_quiet = terminal.Progress(verbose=False)
 
+## REQUIRED FIELDS FOR A PATHWAY FILE
+## AND THEIR ACCEPTABLE VALUES (IF APPLICABLE)
+REQ_FIELDS = {'type': ['pathway', 'enzyme set'],
+              'source': ['KEGG', 'user'],
+              'functional_definition': None,
+              'functions': None
+            }
+
 class PathwayYAML:
     """A class to store definitions of metabolic pathways from YAML-formatted data. 
 
@@ -49,9 +57,24 @@ class PathwayYAML:
         elif path_dict:
             self.file_path = None
             pathway_dict = path_dict
+        
+        num_pathways_in_file = len(pathway_dict.keys())
+        if anvio.DEBUG:
+            run.info("Number of pathways found", num_pathways_in_file)
+
+        if num_pathways_in_file > 1:
+            # check if user forgot to nest the pathway info within a dictionary keyed by pathway ID
+            for key in pathway_dict.keys():
+                if key in list(REQ_FIELDS.keys()):
+                    raise ConfigError(f"We noticed that the top-level of the data dictionary contains pathway information, like '{key}'. "
+                                      f"The first level of this dictionary should be the pathway ID (such as 'M00001'). Please nest the "
+                                      f"pathway info one level lower in your input data.")
+            raise ConfigError("We found more than one pathway in the input data, but we are not yet equipped to handle that :/ ")
+        
         self.id = list(pathway_dict.keys())[0]
         self.dict = pathway_dict[self.id]
 
+        self.check_required_fields()
     #### ACCESSOR FUNCTIONS ####
     def get_pathway_dict(self):
         """Returns the pathway dictionary."""
@@ -64,3 +87,13 @@ class PathwayYAML:
         print(self.dict)
 
     #### SANITY CHECKING FUNCTIONS ####
+    def check_required_fields(self):
+        """Ensure that all required fields are defined in the pathway file."""
+
+        for field, accepted_vals in REQ_FIELDS.items():
+            if field not in self.dict:
+                raise ConfigError(f"The required field '{field}' was not found in the input data.")
+            if accepted_vals and (self.dict[field] not in accepted_vals):
+                raise ConfigError(f"There is an issue with the definition of {field} in the input pathway data. "
+                                  f"This field has the value '{self.dict[field]}', but only the following values are accepted: "
+                                  f"{', '.join(accepted_vals)}")
