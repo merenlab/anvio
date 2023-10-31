e9e6b85cfba6376ce602a49b06850944448c6795
Iva Veseli <iva.veseli@gmail.com>
2023-10-31 14:51:28 +0100

    enable dictionary input as well as file input to new class

--- anvio/metabolism.py
+++ anvio/metabolism.py
@@ -22,17 +22,32 @@ run_quiet = terminal.Run(log_file_path=None, verbose=False)
 progress_quiet = terminal.Progress(verbose=False)
 
 class PathwayYAML:
-    """A YAML-formatted file to store definitions of metabolic pathways.
+    """A class to store definitions of metabolic pathways from YAML-formatted data. 
+
+    The data must be provided either in the form of a YAML file, or as a dictionary.
     
     PARAMETERS
     ==========
     file_path : string
-        path to input YAML file (required)
+        path to input YAML file
+    path_dict : dictionary
+        dictionary containing the pathway info, ie from an already-parsed YAML file
     """
 
-    def __init__(self, file_path):
-        filesnpaths.is_file_exists(file_path)
-        pathway_dict = utils.get_yaml_as_dict(file_path)
+    def __init__(self, file_path=None, path_dict=None):
 
-        run.info_single(f"The pathway file {file_path} has been successfully loaded.")
+        if not file_path and not path_dict:
+            raise ConfigError("The PathwayYAML class requires an input. You should provide either a path to a YAML file or a "
+                              "dictionary of pathway information.")
 
+        if file_path:
+            self.file_path = file_path
+            filesnpaths.is_file_exists(self.file_path)
+            pathway_dict = utils.get_yaml_as_dict(self.file_path)
+            if anvio.DEBUG:
+                run.info_single(f"The pathway file {self.file_path} has been successfully loaded.")
+        elif path_dict:
+            self.file_path = None
+            pathway_dict = path_dict
+        self.id = list(pathway_dict.keys())[0]
+        self.dict = pathway_dict[self.id]
