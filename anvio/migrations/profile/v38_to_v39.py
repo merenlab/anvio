f9c7ba31762af51274a27537c64f88d5a3dc9e30
A. Murat Eren <a.murat.eren@gmail.com>
2023-11-20 09:25:25 +0100

    UPDATE THE DB VERSION FR

--- anvio/migrations/profile/v38_to_v39.py
+++ anvio/migrations/profile/v38_to_v39.py
@@ -25,15 +25,11 @@ def migrate(db_path):
     profile_db = db.DB(db_path, None, ignore_version = True)
 
     is_blank = profile_db.get_meta_value('blank')
-    is_merged = profile_db.get_meta_value('merged')
 
     progress.new("Harr Harr")
     progress.update('...')
 
-    msg = ""
-
     if is_blank:
-        msg = "But this was a blank profile database, so anvi'o did nothing."
         pass
     else:
         try:
@@ -43,17 +39,25 @@ def migrate(db_path):
 
         profile_db.set_meta_value('skip_edges_for_variant_profiling', 0)
 
+    # uppity
+    profile_db.set_version(next_version)
+
     #              خدا حافظ
     profile_db.disconnect()
 
     progress.end()
 
-    run.info_single(f"The profile database is now {next_version}. We recently added a parameter for "
-                    f"ancient DNA-friendly profiling of SNVs, SCVs, and SAAVs. If you are interested "
-                    f"to learn more, see https://github.com/merenlab/anvio/pull/2081. Version bump was "
-                    f"necessary to make sure all anvi'o profile-dbs have a means to track whether "
-                    f"an additional parameter was used by the user to skip the edges of short reads "
-                    f"during profiling.", nl_after=1, nl_before=1, mc='green')
+    if is_blank:
+        run.info_single(f"Your blank profile-db is immune to this update, so anvi'o simply bumped its "
+                        f"version to {next_version} without really changing anything else. BYE.",
+                        nl_after=1, nl_before=1, mc='green')
+    else:
+        run.info_single(f"The profile database is now {next_version}. We recently added a parameter for "
+                        f"ancient DNA-friendly profiling of SNVs, SCVs, and SAAVs. If you are interested "
+                        f"to learn more, see https://github.com/merenlab/anvio/pull/2081. Version bump was "
+                        f"necessary to make sure all anvi'o profile-dbs have a means to track whether "
+                        f"an additional parameter was used by the user to skip the edges of short reads "
+                        f"during profiling.", nl_after=1, nl_before=1, mc='green')
 
 
 if __name__ == '__main__':
