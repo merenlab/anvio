5020d5b8c4c1d440085dc2c4bc635e97284a39e0
Katy Lambert-Slosarska <klambertslosarska@gmail.com>
2024-01-09 14:44:32 +0100

    Load parameters and sanity check

--- anvio/dgrs.py
+++ anvio/dgrs.py
@@ -13,6 +13,8 @@ from Bio.SeqRecord import SeqRecord
 
 import anvio
 import anvio.terminal as terminal
+import anvio.filesnpaths as filesnpaths
+from anvio.errors import ConfigError 
 
 __author__ = "Developers of anvi'o (see AUTHORS.txt)"
 __copyright__ = "Copyleft 2015-2024, the Meren Lab (http://merenlab.org/)"
@@ -29,3 +31,20 @@ class DGR_Finder:
         self.run = run
         self.progress = progress
 
+        A = lambda x: args.__dict__[x] if x in args.__dict__ else None
+        self.input_path = A('input_file')
+        self.step = A('step')
+        self.word_size = A('word_size')
+        self.skip_Ns = A('skip_Ns')
+        self.skip_dashes = A('skip_dashes')
+
+        filesnpaths.is_file_fasta_formatted(self.input_path)
+        if self.step < 0 or self.word_size < 0:
+            raise ConfigError('The step value and/or word size value you are trying to input should be positive.')
+        
+        self.run.info('Input FASTA file', self.input_path)
+        self.run.info('Step size', self.step)
+        self.run.info('BLASTn word size', self.word_size)
+        self.run.info('Skip "N" characters', self.skip_Ns)
+        self.run.info('Skip "-" characters', self.skip_dashes)
+        
