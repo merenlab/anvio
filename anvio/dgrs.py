f16c613dd0034dfb601537c0363c275cb5452384
Katy Lambert-Slosarska <klambertslosarska@gmail.com>
2024-01-24 14:58:45 +0100

    attempt at creating high SNV windows

--- anvio/dgrs.py
+++ anvio/dgrs.py
@@ -54,6 +54,7 @@ class DGR_Finder:
         self.temp_dir = A('temp_dir') or filesnpaths.get_temp_directory_path()
         self.min_dist_bw_snvs = A('distance_between_snv')
         self.variable_buffer_length = A('variable_buffer_length')
+        self.departure_from_reference_percentage = A('departure_from_reference_percentage')
 
         self.sanity_check()
         
@@ -71,7 +72,7 @@ class DGR_Finder:
         if self.profile_db_path and self.contigs_db_path:
             self.run.info('Minimum distance between SNVs', self.min_dist_bw_snvs)
             self.run.info('Variable buffer length', self.variable_buffer_length)
-
+            self.run.info('Departure from reference percentage', self.departure_from_reference_percentage)
     def sanity_check(self):
         if self.contigs_db_path and self.fasta_file_path:
             raise ConfigError("You should either choose a FASTA file or a contigs db to send to this "
@@ -84,7 +85,9 @@ class DGR_Finder:
             raise ConfigError('The step value and/or word size value you are trying to input should be positive.')
         
         if self.variable_buffer_length < 0:
-            raise ConfigError('The variable buffer length value you are trying to input should be positive.')
+
+        if self.departure_from_reference_percentage < 0:
+            raise ConfigError('The departure from reference percentage value you are trying to input should be a positive decimal number.')
 
     def get_blast_results(self):
         """
@@ -126,18 +129,194 @@ class DGR_Finder:
             blast.blast(outputfmt = '5', word_size = self.word_size)
 
         elif self.contigs_db_path and self.profile_db_path:
-            #contigs_db = dbops.ContigsDatabase(self.contigs_db_path, run=run_quiet, progress=progress_quiet)
-                #self.splits_basic_info = contigs_db.db.smart_get(t.splits_info_table_name, column='split', data=split_names)
-                #self.contig_sequences = contigs_db.db.get_table_as_dict(t.contig_sequences_table_name)
-                #self.genes_are_called_in_contigs_db = contigs_db.meta['genes_are_called']
-                #self.genes_annotated_with_functions_in_contigs_db = contigs_db.meta['gene_function_sources'] is not None and len(contigs_db.meta['gene_function_sources']) > 0
-                #contigs_db.disconnect()
-            #NEED SNV WINDOW MAKER func here that takes SNV info from .db using tables? or tableops? 
-            #then take in these windows outputs nd blast to give blast_output in xaml file. but need to original sequence to comapre the SNV windows to?
-            pass
+            contigs_db = dbops.ContigsDatabase(self.contigs_db_path, run=run_quiet, progress=progress_quiet)
+            #self.splits_basic_info = contigs_db.db.smart_get(t.splits_info_table_name, column = 'split')
+            #self.splits_of_interest = contigs_db.db.smart_get(t.splits_info_table_name, column='split')
+            self.contig_sequences = contigs_db.db.get_table_as_dict(t.contig_sequences_table_name)
+            #self.splits_of_interest = list(self.splits_basic_info.keys())
+            #self.splits_of_interest = ', '.join(map(str, self.splits_basic_info.keys()))
+            #print(self.splits_of_interest)
+            #split_path = 'split_of_interest_file.txt
+
+            contigs_db.disconnect()
+            
+            #open merged profile-db and get the variable nucleotide table as a dictionary then acess the split names as a list to use in get_snvs
+            profile_db = dbops.ProfileDatabase(self.profile_db_path)
+            self.variable_nucleotides_dict = profile_db.db.get_table_as_dict(t.variable_nts_table_name)
+       
+
+            # Use a list comprehension to extract the values associated with the target key
+            split_names = [self.variable_nucleotides_dict[key]['split_name'] for key in self.variable_nucleotides_dict if 'split_name' in self.variable_nucleotides_dict[key]]
+            self.split_names_unique = list(dict.fromkeys(split_names))
+            sample_id_list = [self.variable_nucleotides_dict[key]['sample_id'] for key in self.variable_nucleotides_dict if 'sample_id' in self.variable_nucleotides_dict[key]]
+            sample_id_list = list(set(sample_id_list))
+            departure_from_reference = [self.variable_nucleotides_dict[key]['departure_from_reference'] for key in self.variable_nucleotides_dict if 'departure_from_reference' in self.variable_nucleotides_dict[key]]
+            
+            profile_db.disconnect()
+           
+            self.snv_panda = self.get_snvs().sort_values(by=['contig_name', 'pos_in_contig'])
+
+            self.snv_panda['departure_from_reference'] = self.snv_panda.apply(lambda row: self.variable_nucleotides_dict.get(row.name, {}).get('departure_from_reference', None), axis=1)
+
+            #Sort  pandas frame by 'split_name' and then by 'pos'
+            #self.snvs_sorted = self.snv_panda
+            #self.snvs_sorted_dict = self.snvs_sorted.to_dict()
+            #in split subset extract contig_name and pos_in_contig 
+            self.all_possible_windows = []
+            
+            for split in self.split_names_unique:
+                for sample in sample_id_list:
+                    split_subset = self.snv_panda.loc[(self.snv_panda.split_name==split)&
+                                                      (self.snv_panda.sample_id==sample)&
+                                                      (self.snv_panda.departure_from_reference>=self.departure_from_reference_percentage)]
+                    if split_subset.shape[0] == 0:
+                        continue
+                    contig_name = split_subset.contig_name.unique()[0]
+                    pos_list = split_subset.pos_in_contig.to_list()
+
+                    if contig_name not in self.all_possible_windows:
+                        # If not, initialize it with an empty dictionary
+                        self.all_possible_windows[contig_name] = []
+                        # subset pandas df with split name
+
+                    #get list of pos within that split
+                    for i in range(len(pos_list) - 1):
+                        current_pos = pos_list[i]
+                        next_pos = pos_list[i + 1]
+                        distance = next_pos - current_pos
+                        range_start = current_pos
+                        
+                        while i + 1 < len(pos_list) and distance <= self.min_dist_bw_snvs:
+                            i += 1
+                            current_pos = pos_list[i]
+                            if i + 1 < len(pos_list):
+                                next_pos = pos_list[i + 1]
+                                distance = next_pos - current_pos
+                                range_end = current_pos
+                        if distance <= self.min_dist_bw_snvs:
+                            range_end = next_pos
+
+                        if range_end > range_start:
+                            window_start = range_start - self.variable_buffer_length
+                            window_end = range_end + self.variable_buffer_length
+                        
+                        contig_len = len(self.contig_sequences[contig_name]['sequence'])
+
+                        if window_start <0:
+                            window_start = 0
+                        if window_end > contig_len:
+                            window_end = contig_len
+                        
+                        # Generate a unique key based on the index of the start and end positions
+                        key = f'{contig_name}_window_{len(self.all_possible_windows[contig_name]) + 1}'
+
+                        # Create a sub-dictionary for the window
+                        window_dict = {
+                            'contig_name': contig_name,
+                            'start_position': window_start,
+                            'end_position': window_end
+                        }
+
+                        # Add the sub-dictionary to the contig's dictionary using the generated key
+                        self.all_possible_windows[contig_name][key] = window_dict
+          
+            # Initialize an empty list for unique overlapping sequences
+            all_entries = []
+        
+            for contig_name, window_dict in self.all_possible_windows.items():
+                for window_name, window_values in window_dict.items():
+                    all_entries.append((contig_name, window_name, window_values['start_position'], window_values['end_position']))
+
+            # now it is time to identify clusters. the following state
+            clusters = []
+            while 1:
+                if not len(all_entries):
+                    break
+
+                entry = all_entries.pop(0)
+                cluster = [entry]
+                contig_name, window_number, start, end = entry
+                matching_entries = []
+
+                for i in range(0, len(all_entries)):
+                    contig_name, n_window_number, n_start, n_end = all_entries[i]
+                    if self.range_overlapping(start, end, n_start, n_end):
+                        matching_entries.append(i)
+                        start = min(start, n_start)
+                        end = max(end, n_end)
+                #for i in range(0, len(all_entries)):
+                    #contig_name, n_window_number, n_start, n_end = all_entries[i]
+                    #if self.range_overlapping(start, end, n_start, n_end):
+                        #matching_entries.append(i)
+
+                # add all matching entries
+                for i in sorted(matching_entries, reverse=True):
+                    cluster.append(all_entries.pop(i))
+
+                # combine ranges of the cluster from entries and then add the combined lsit to the final clusters
+                combined_result = self.combine_ranges(cluster)
+                clusters.append(combined_result)
+
+                for i in range(len(clusters)):
+                    for j in range(i, len(clusters)):
+                        if i != j:
+                            if self.range_overlapping(clusters[i][2], clusters[i][3], clusters[j][2], clusters[j][3]):
+                                print(f"overlapping at indices {i} and {j}:\n{clusters[i]}\n{clusters[j]}")
 
         return(blast_output)
-                
+    
+    def combine_ranges(self, entries):
+        """
+        This function takes a list of (contig_name, key, start, end) tuples and takes the longest sequence possible - the smallest start and largest end.
+        
+        Returns 
+        =======
+        a tuple containing (contig_name, 'combined', combined_start, combined_end) where the variables are the following:
+        contig_name : str
+            header of the contig sequence
+        combined_start, combined_end : integers
+            A new start and end position for a contig sequence, to get the longest possible string. 
+        """
+        
+        #extract all starts and stops
+        all_start = []
+        all_end = []
+        contig_name = None
+
+        for contig, key, start, end in entries:
+            contig_name = contig # these should all be the same so it doesn't matter that we overwrite it every iteration of the loop
+            all_start.append(start)
+            all_end.append(end)
+        # do le math
+        combined_start = min(all_start) # may need to load numpy package to use the min() function
+        combined_end = max(all_end) # same deal with numpy and max()
+                                
+        return (contig_name, 'combined', combined_start, combined_end)
+    
+    def range_overlapping(self, start1, end1, n_start, n_end):
+        """
+        This function checks if the sections of sequences overlap based on the start and end positions.
+        
+        Returns 
+        =======
+        ??
+        """
+        #self.start1, self.end1 = self.start_end_dict1['start_position'], self.start_end_dict1['end_position']
+        #self.start2, self.end2 = self.start_end_dict2['start_position'], self.start_end_dict2['end_position']
+        
+        return (n_start >= start1 and n_start <= end1) or (n_end >= start1 and n_end <= end1)
+     
+    
+    def check_overlap(window1, window2):
+                        contig_name_1, start_position_1, end_position_1 = window1[0][1], window1[1][1], window1[2][1]
+                        contig_name_2, start_position_2, end_position_2 = window2[0][1], window2[1][1], window2[2][1]
+
+                        return (
+                            contig_name_1 == contig_name_2
+                            and start_position_1 <= end_position_2
+                            and end_position_1 >= start_position_2
+                        )
+       
     def get_snvs(self):
         args = argparse.Namespace(contigs_db=self.contigs_db_path,
                                 profile_db=self.profile_db_path,
@@ -174,52 +353,50 @@ class DGR_Finder:
                     print(sequence.seq)
         return section_sequences
     
-    def run_blastn(self):
-        """
-        This function runs the BLASTn search of the split sequences against the original input FASTA to find regions of matching nucleotides.
+    #def run_blastn(self):
+        #"""
+        #This function runs the BLASTn search of the split sequences against the original input FASTA to find regions of matching nucleotides.
         
-        Running the BLASTn generates an xml file of results.
+        #Running the BLASTn generates an xml file of results.
 
-        Returns
-        root : xml file
-            An xml of BLASTn results
-        =======
-        """
-        #initialise temporary dictionary
-        tmp_directory_path = filesnpaths.get_temp_directory_path()
-        # split sequences output file name 
-        shredded_sequence_file = os.path.join(tmp_directory_path,f"shredded_sequences_step_{self.step}_wordsize_{self.word_size}.fasta")
+        #Returns
+        #blast_output : xml file 
+            #BLASTn results
+        #=======
+        #"""
         #blast output file name
-        blast_output = os.path.join(tmp_directory_path,f"blast_output_step_{self.step}_wordsize_{self.word_size}.xml")
-        target_file_path = os.path.join(tmp_directory_path,f"input_file.fasta")
-        print(f"cp {self.input_path} {target_file_path}")
-        os.system(f"cp {self.input_path} {target_file_path}")
-        self.run.info('temporary input for blast', target_file_path) 
+        #blast_output = os.path.join(tmp_directory_path,f"blast_output_step_{self.step}_wordsize_{self.word_size}.xml")
+        #self.target_file_path = os.path.join(tmp_directory_path,f"input_file.fasta")
+        #print(f"cp {self.fasta_file_path} {self.target_file_path}")
+        #os.system(f"cp {self.fasta_file_path} {self.target_file_path}")
+        #self.run.info('temporary input for blast', self.target_file_path) 
 
         # Start at half the step size of the output file
-        overlap_start = self.step // 2
-        first_sequences = self.split_sequences()
-        second_sequences = self.split_sequences(overlap_start)
+        #overlap_start = self.step // 2
+        #first_sequences = self.split_sequences()
+        #second_sequences = self.split_sequences(overlap_start)
 
-        all_sequences = first_sequences + second_sequences
+        #all_sequences = first_sequences + second_sequences
 
         # Write combined sequences to output file
-        with open(shredded_sequence_file, "w") as output_handle:
-            SeqIO.write(all_sequences, output_handle, "fasta")
+        #with open(shredded_sequence_file, "w") as output_handle:
+            #SeqIO.write(all_sequences, output_handle, "fasta")
         #need a temporary directory where intermediate files are written, to call on them. 
         
-        blast = BLAST(shredded_sequence_file, target_fasta =target_file_path, search_program = 'blastn', output_file=blast_output, additional_params = '-dust no')
-        blast.evalue = 10 #set Evalue to be same as blastn default
-        blast.makedb(dbtype = 'nucl')
-        blast.blast(outputfmt = '5', word_size = self.word_size)
+        #blast = BLAST(shredded_sequence_file, target_fasta =self.target_file_path, search_program = 'blastn', output_file=blast_output, additional_params = '-dust no')
+        #blast.evalue = 10 #set Evalue to be same as blastn default
+        #blast.makedb(dbtype = 'nucl')
+        #blast.blast(outputfmt = '5', word_size = self.word_size)
 
-        #blast_command = ["blastn", "-query", output_file, "-subject", self.input_path, "-out", blast_output, 
+        #blast_command = ["blastn", "-query", output_file, "-subject", self.fasta_file_path, "-out", blast_output, 
                          #"-word_size", str(self.word_size), "-dust", "no", "-outfmt", "5"]
         #subprocess.run(blast_command)
-        return blast_output
-    #need to return blast_output which is a path so next function calls the path. 
-        
-    #then have new function that takes root as an argument to filter for hits - filter blast hit, with self and root as param. copy in code. 
+        #return blast_output
+     
+     #def find_SNV_window(self, profile.db)
+        #if SNV:
+            #for row()
+
     def filter_blastn_for_none_identical(self, blast_output):
         """
         This function takes the BLASTn xml output and refines the results to those with less than 100% identity.
