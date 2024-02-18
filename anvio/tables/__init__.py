1923ba608109e95f4452d8ab1bd77b125cdd1978
Sam Miller <samuelmiller10@gmail.com>
2024-02-17 21:51:15 -0600

    add structure of new pan db reaction network table for ko info

--- anvio/tables/__init__.py
+++ anvio/tables/__init__.py
@@ -47,14 +47,17 @@ pan_gene_clusters_table_name           = 'gene_clusters'
 pan_gene_clusters_table_structure      = ['gene_caller_id', 'gene_cluster_id', 'genome_name', 'alignment_summary']
 pan_gene_clusters_table_types          = [    'numeric'   ,      'str'       ,     'str'    ,        'str'       ]
 
-pan_gene_cluster_function_reactions_table_name        = 'gene_cluster_function_reactions'
-pan_gene_cluster_function_reactions_table_structure   = ['modelseed_reaction_id', 'modelseed_reaction_name', 'ko_kegg_reaction_source', 'ko_ec_number_source', 'other_kegg_reaction_ids', 'other_ec_numbers', 'metabolite_modelseed_ids', 'stoichiometry', 'compartments', 'reversibility']
-pan_gene_cluster_function_reactions_table_types       = [         'text'        ,            'text'        ,          'text'          ,         'text'       ,         'text'           ,       'text'      ,           'text'          ,      'text'    ,     'text'    ,      'bool'    ]
+pan_reaction_network_reactions_table_name        = 'reaction_network_reactions'
+pan_reaction_network_reactions_table_structure   = ['modelseed_reaction_id', 'modelseed_reaction_name', 'ko_kegg_reaction_source', 'ko_ec_number_source', 'other_kegg_reaction_ids', 'other_ec_numbers', 'metabolite_modelseed_ids', 'stoichiometry', 'compartments', 'reversibility']
+pan_reaction_network_reactions_table_types       = [         'text'        ,            'text'        ,          'text'          ,         'text'       ,         'text'           ,       'text'      ,           'text'          ,      'text'    ,     'text'    ,      'bool'    ]
 
-pan_gene_cluster_function_metabolites_table_name      = 'gene_cluster_function_metabolites'
-pan_gene_cluster_function_metabolites_table_structure = ['modelseed_compound_id', 'modelseed_compound_name', 'kegg_aliases', 'formula', 'charge']
-pan_gene_cluster_function_metabolites_table_types     = [         'text'        ,           'text'         ,     'text'    ,   'text' , 'numeric']
+pan_reaction_network_metabolites_table_name      = 'reaction_network_metabolites'
+pan_reaction_network_metabolites_table_structure = ['modelseed_compound_id', 'modelseed_compound_name', 'kegg_aliases', 'formula', 'charge']
+pan_reaction_network_metabolites_table_types     = [         'text'        ,           'text'         ,     'text'    ,   'text' , 'numeric']
 
+pan_reaction_network_kegg_table_name             = 'reaction_network_kegg'
+pan_reaction_network_kegg_table_structure        = ['kegg_id', 'name', 'modules', 'pathways', 'brite_categorization']
+pan_reaction_network_kegg_table_types            = [ 'text'  , 'text',  'text'  ,   'text'  ,         'text'        ]
 
 ####################################################################################################
 #
@@ -405,8 +408,9 @@ table_requires_unique_entry_id = {'self': False,
                                   'max_normalized_ratio_splits': False,
                                   'relative_abundance_splits': False,
                                   pan_gene_clusters_table_name: True,
-                                  pan_gene_cluster_function_reactions_table_name: False,
-                                  pan_gene_cluster_function_metabolites_table_name: False,
+                                  pan_reaction_network_reactions_table_name: False,
+                                  pan_reaction_network_metabolites_table_name: False,
+                                  pan_reaction_network_kegg_table_name: False,
                                   genes_in_splits_table_name: True,
                                   gene_function_calls_table_name: True,
                                   reaction_network_reactions_table_name: False,
