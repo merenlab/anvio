6072e47e23ce99202263b46b2e4ac2bf9a4d3c99
Sam Miller <samuelmiller10@gmail.com>
2024-02-17 21:53:45 -0600

    add contigs db version increment

--- anvio/tables/__init__.py
+++ anvio/tables/__init__.py
@@ -14,7 +14,7 @@ __maintainer__ = "A. Murat Eren"
 __email__ = "a.murat.eren@gmail.com"
 
 
-contigs_db_version = "22"
+contigs_db_version = "23"
 profile_db_version = "40"
 genes_db_version = "6"
 pan_db_version = "17"
