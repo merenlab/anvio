9505c6b692893350827eeca7cfae4f25c091b910
Metehan Sever <metehaansever@gmail.com>
2025-01-15 11:04:39 +0100

    Remove num_genes_in_psgc

--- anvio/panops.py
+++ anvio/panops.py
@@ -620,7 +620,6 @@ class Pangenome(object):
         self.add_layers_to_view(psgc_gc_counts, psgc_gene_counts, psgc_gc_types)
         item_additional_data_keys.extend([
             'num_gene_clusters_in_psgc',
-            'num_genes_in_psgc',
             'psgc_composition!core',
             'psgc_composition!singleton',
             'psgc_composition!accessory',
@@ -722,7 +721,7 @@ class Pangenome(object):
     def add_layers_to_view(self, psgc_gc_counts, psgc_gene_counts, psgc_gc_types):
         """Add all PSGC layers to the view data."""
         for gene_cluster in self.view_data:
-            self.additional_view_data[gene_cluster].update({ 'num_gene_clusters_in_psgc': psgc_gc_counts.get(gene_cluster, 0), 'num_genes_in_psgc': psgc_gene_counts.get(gene_cluster, 0) })
+            self.additional_view_data[gene_cluster].update({ 'num_gene_clusters_in_psgc': psgc_gc_counts.get(gene_cluster, 0) })
 
 
     def gen_synteny_based_ordering_of_gene_clusters(self, gene_clusters_dict):
