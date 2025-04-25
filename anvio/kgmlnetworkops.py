e929f26fa921114b9ff9ace8c2a98e6eda63367b
Samuel E. Miller <samuelmiller10@gmail.com>
2025-04-25 14:38:44 -0500

    add KGML walker static method to find branch index of partly cyclic chain

--- anvio/kgmlnetworkops.py
+++ anvio/kgmlnetworkops.py
@@ -1281,6 +1281,61 @@ class KGMLNetworkWalker:
 
         return True
 
+    @staticmethod
+    def get_cyclic_branch_index(chain: Chain) -> int:
+        """
+        To identify a "partly cyclic" chain looping a cycle via the entry or exit of an uncycled
+        compound through a reaction in the cycle, check if the last reaction in the chain is
+        traversed earler in the chain.
+
+        This method avoids cyclic chains that do not branch off and only include cycled compounds.
+
+        Stringent criteria are imposed in the identification of a partly cyclic chain beyond the
+        last KGML reaction occurring a second time in the chain, although that might be sufficient
+        given experience. The branch KGML reaction must occur in the same direction and produce or
+        consume the same KGML compound, which is the cycle-closing compound last in the chain.
+
+        Parameters
+        ==========
+        chain : Chain
+            Chain to be checked.
+
+        Returns
+        =======
+        int
+            If the chain is partly cyclic, a non-negative int representing the index of the first
+            occurrence of the branching KGML reaction that also ends the chain. -1 if the chain is
+            not partly cyclic.
+        """
+        last_kgml_reaction_id = chain.kgml_reactions[-1].id
+        last_direction = chain.kgml_reaction_directions[-1]
+        candidate_cycled_kgml_compound_id = chain.kgml_compound_entries[-1].id
+
+        for i, (kgml_reaction, direction) in enumerate(zip(
+            chain.kgml_reactions[: -1], chain.kgml_reaction_directions[: -1]
+        )):
+            if kgml_reaction.id == last_kgml_reaction_id and direction == last_direction:
+                break
+        else:
+            return -1
+
+        if (
+            chain.is_consumed and
+            candidate_cycled_kgml_compound_id == chain.kgml_compound_entries[i + 1].id
+        ):
+            # The partly cyclic consumption chain traversed the same KGML reaction in the same
+            # direction producing the same compound as before.
+            return i
+        elif (
+            not chain.is_consumed and
+            candidate_cycled_kgml_compound_id == chain.kgml_compound_entries[i].id
+        ):
+            # The partly cyclic production chain traversed the same KGML reaction in the same
+            # direction consuming the same reactant as before.
+            return i
+
+        return -1
+
     def remove_intermediate_chains(
         self,
         compound_id_chains: dict[str, list[Chain]]
