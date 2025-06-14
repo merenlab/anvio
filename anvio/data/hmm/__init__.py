387e6e13da4272bca4505f964cdd95724570362c
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2025-06-14 09:31:05 +0200

    pfft meren

--- anvio/data/hmm/__init__.py
+++ anvio/data/hmm/__init__.py
@@ -8,18 +8,10 @@ run = terminal.Run()
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 sources = u.get_HMM_sources_dictionary([s for s in glob.glob(os.path.join(dir_path, '*')) if not s.endswith('.py') and s.find('.txt') < 0 and not os.path.basename(s).startswith('__')])
-scg_domain_to_source = dict([(sources[s]['domain'], s) for s in sources if sources[s]['kind'] == 'singlecopy']) 
+scg_domain_to_source = dict([(sources[s]['domain'], s) for s in sources if sources[s]['kind'] == 'singlecopy'])
 
 if len(sources):
-    if len(missing_programs):
-        run.warning("Anvi'o found one or more databases to perform single-copy gene analysis "
-                    "that may be useful for downstream analyses. However, this process require "
-                    "certain programs to be present in your system. Here is the command(s) anvio "
-                    "tried to access and failed: %s. Please see the documentation for system "
-                    "requirements." % ', '.join(missing_programs))
-        sources = {}
-    else:
-        run.info('HMM profiles',
-                 '%d source%s been loaded: %s' % (len(sources),
-                                                  's have' if len(sources) > 1 else ' has',
-                                                  ', '.join(['%s (%d genes, domain: %s)' % (s, len(sources[s]['genes']), sources[s]['domain']) for s in sources])))
+   run.info('HMM profiles',
+            '%d source%s been loaded: %s' % (len(sources),
+                                             's have' if len(sources) > 1 else ' has',
+                                             ', '.join(['%s (%d genes, domain: %s)' % (s, len(sources[s]['genes']), sources[s]['domain']) for s in sources])))
