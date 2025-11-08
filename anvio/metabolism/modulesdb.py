68cfddfb9c22799cb285550dce8780d96c066429
Iva Veseli <iva.veseli@gmail.com>
2025-11-08 09:28:34 +0100

    ignore the COMPLETE entries!

--- anvio/metabolism/modulesdb.py
+++ anvio/metabolism/modulesdb.py
@@ -405,7 +405,9 @@ class ModulesDatabase(KeggContext):
                     for name, val, definition, line in entries_tuple_list:
                         # there is one situation in which we want to ignore the entry, and that is Modules appearing in the ORTHOLOGY category, like so:
                         # (M00531  Assimilatory nitrate reduction, nitrate => ammonia)
-                        if not (name == "ORTHOLOGY" and val[0] == '('):
+                        # as of September 2025, we also want to ignore COMPLETE entries, which are really long lists of organisms that have the module
+                        # complete and we just don't care about that info to justify the additional space it takes in the database
+                        if not (name == "ORTHOLOGY" and val[0] == '(') and not (name == 'COMPLETE'):
                             # append_and_store will collect db entries and store every 10000 at a time
                             mod_table.append_and_store(self.db, mnum, name, val, definition, line)
                         else:
