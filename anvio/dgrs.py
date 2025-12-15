3db877a3a45917b575a373398417123445d226b0
Florian Trigodet <trigodet.florian@gmail.com>
2025-12-15 16:00:20 +0100

    Remove debug message about SNV density

--- anvio/dgrs.py
+++ anvio/dgrs.py
@@ -448,9 +448,6 @@ class DGR_Finder:
                     snv_density = num_snvs_in_cluster/snv_cluster_length
 
                     if snv_density > self.minimum_snv_density:
-                        if anvio.DEBUG:
-                            self.run.warning(f"Removing putative VR because the SNV density is over the threshold, this putative VR is at a density of {snv_density} over the SNV high density region. Please feel free to adjust parameters as you see fit, we hope this means something to you.")
-
                         #add buffer length
                         window_start = range_start - self.variable_buffer_length
                         window_end = range_end + self.variable_buffer_length
