239fc2b8790ac346b8510b2c126f2282cea81df1
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2025-12-21 19:33:56 +0100

    add some visualization options

--- anvio/cli/reorient_genomes.py
+++ anvio/cli/reorient_genomes.py
@@ -58,6 +58,21 @@ def get_args():
                                "option is useful for bacterial genomes but may not work well for plasmids or viral genomes "
                                "without DnaA.")
 
+    groupViz = parser.add_argument_group('VISUALIZATION',
+                                         "By default, the program will generate synteny ribbon plots showing "
+                                         "alignment patterns between the reference and each query genome before "
+                                         "and after reorientation/scaffolding. These visualizations help assess "
+                                         "the quality of the reorientation process.")
+    groupViz.add_argument('--skip-visualizing-alignments', action='store_true',
+                          help="Skip generating alignment visualizations. This can speed up processing when "
+                               "you only need the reoriented FASTA files without visual inspection.")
+    groupViz.add_argument('--plot-width', type=int,
+                          help="Width of alignment plots in characters. By default, plots automatically scale "
+                               "to use the full terminal width. Use this to override with a specific width. "
+                               "Note: widths below 100 characters may not display properly.")
+    groupViz.add_argument('--plot-height', type=int, default=20,
+                          help="Height of alignment plots in characters. Default: %(default)s")
+
     groupB = parser.add_argument_group('ADVANCED')
     groupB.add_argument('--threads', type=int, default=1,
                         help="Number of threads for minimap2.")
