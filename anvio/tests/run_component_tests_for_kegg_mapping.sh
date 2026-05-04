f9ec19d81aa2f0aa802a766be8eb189ce9464239
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2026-05-04 14:37:09 +0200

    update component tests & see why this is cleaner

--- anvio/tests/run_component_tests_for_kegg_mapping.sh
+++ anvio/tests/run_component_tests_for_kegg_mapping.sh
@@ -32,7 +32,6 @@ anvi-gen-genomes-storage -e external-genomes.txt -o TEST-GENOMES.db --no-progres
 
 INFO "Running the pangenome analysis with default parameters"
 anvi-pan-genome -g TEST-GENOMES.db \
-                -o TEST/ \
                 -n TEST \
                 --use-ncbi-blast \
                 --description example_description.md \
@@ -54,7 +53,7 @@ fi
 
 INFO "Generating a pangenomic reaction network"
 args=()
-args+=( "--pan-db" "TEST/TEST-PAN.db" )
+args+=( "--pan-db" "TEST-PAN.db" )
 args+=( "--genomes-storage" "TEST-GENOMES.db" )
 if [ ${use_default_modelseed_db} == "False" ]
 then
@@ -148,7 +147,7 @@ anvi-draw-kegg-pathways "${args[@]}"
 INFO "Testing mapping KOs from a pangenomic database, displaying genome counts emphasizing shared \
 reactions"
 args=()
-args+=( "--pan-db" "TEST/TEST-PAN.db" )
+args+=( "--pan-db" "TEST-PAN.db" )
 args+=( "--genomes-storage" "TEST-GENOMES.db" )
 args+=( "--output-dir" ${output_dir}/pan_db_kos_genome_count_emphasize_shared )
 args+=( "--ko" )
@@ -159,7 +158,7 @@ anvi-draw-kegg-pathways "${args[@]}"
 INFO "Testing mapping KOs from a pangenomic database, displaying genome counts emphasizing \
 unshared reactions, drawing map grids and map files for each genome"
 args=()
-args+=( "--pan-db" "TEST/TEST-PAN.db" )
+args+=( "--pan-db" "TEST-PAN.db" )
 args+=( "--genomes-storage" "TEST-GENOMES.db" )
 args+=( "--output-dir" ${output_dir}/pan_db_kos_genome_count_emphasize_unshared )
 args+=( "--categorize-files" )
@@ -174,7 +173,7 @@ anvi-draw-kegg-pathways "${args[@]}"
 
 INFO "Testing mapping KOs from a pangenomic database, displaying presence/absence"
 args=()
-args+=( "--pan-db" "TEST/TEST-PAN.db" )
+args+=( "--pan-db" "TEST-PAN.db" )
 args+=( "--genomes-storage" "TEST-GENOMES.db" )
 args+=( "--output-dir" ${output_dir}/pan_db_kos_presence_absence )
 args+=( "--ko" )
@@ -187,7 +186,7 @@ INFO "Testing mapping KOs from a pangenomic database, displaying group count whe
 any genomes in the group, emphasizing shared reactions, drawing map grids and map files for a \
 subset of groups"
 args=()
-args+=( "--pan-db" "TEST/TEST-PAN.db" )
+args+=( "--pan-db" "TEST-PAN.db" )
 args+=( "--genomes-storage" "TEST-GENOMES.db" )
 args+=( "--groups-txt" "pan-group-information.txt" )
 args+=( "--group-threshold" "0" )
@@ -203,7 +202,7 @@ anvi-draw-kegg-pathways "${args[@]}"
 INFO "Testing mapping KOs from a pangenomic database, displaying group membership where KOs are \
 in any genomes in the group, emphasizing unshared reactions, drawing map grids for each group"
 args=()
-args+=( "--pan-db" "TEST/TEST-PAN.db" )
+args+=( "--pan-db" "TEST-PAN.db" )
 args+=( "--genomes-storage" "TEST-GENOMES.db" )
 args+=( "--groups-txt" "pan-group-information.txt" )
 args+=( "--group-threshold" "0" )
